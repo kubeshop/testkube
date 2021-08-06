f06231ce0c305e7ce51f610b9d0d302fb2addcd4
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-06 11:16:49 +0200

    added config for executor client

--- pkg/executor/client/http_test.go
+++ pkg/executor/client/http_test.go
@@ -12,7 +12,7 @@ func TestHTTPClient(t *testing.T) {
 	t.Run("client execute script", func(t *testing.T) {
 		t.Skip("Implement valid script") // TODO  implement me
 
-		client := NewHTTPExecutorClient(DefaultURI)
+		client := NewHTTPExecutorClient(Config{DefaultURI})
 		e, err := client.Execute("", make(map[string]string)) // To be fixed with the proper types call.
 
 		assert.NoError(t, err)
