09090484317a4cd8bc157f6554420f11a5a82f7d
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-06 13:51:06 +0200

    added version bump in scripts

--- scripts/bump.go
+++ scripts/bump.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"strings"
 
+	"github.com/kubeshop/kubetest/pkg/process"
+	"github.com/kubeshop/kubetest/pkg/ui"
 	"github.com/kubeshop/kubetest/pkg/version"
 )
 
+var kind = flag.String("kind", "patch", "version kind to bump can be one of: major|minor|patch (patch is default one)")
+
 func main() {
-	fmt.Println(version.Next("0.0.2", version.Major))
-	fmt.Println(version.Next("0.0.2", version.Minor))
-	fmt.Println(version.Next("0.0.2", version.Patch))
+
+	out, err := process.Execute("git", "tag")
+	ui.ExitOnError("getting tags", err)
+
+	versions := strings.Split(string(out), "\n")
+	currentVersion := version.GetNewest(versions)
+	nextVersion, err := version.Next(currentVersion, *kind)
+	ui.ExitOnError("getting next version for "+*kind, err)
+
+	ui.Info("Generated new version", nextVersion)
+
+	_, err = process.Execute("git", "tag", nextVersion)
+	ui.ExitOnError("tagging new version", err)
+
+	_, err = process.Execute("git", "push", "--tags")
+	ui.ExitOnError("pushing new version to repository", err)
 }
