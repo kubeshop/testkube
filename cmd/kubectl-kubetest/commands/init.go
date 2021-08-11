c4dfe42e7221d4c2943aa09d720808403d7826bd
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-11 07:06:36 +0200

    removed connect function

--- cmd/kubectl-kubetest/commands/init.go
+++ cmd/kubectl-kubetest/commands/init.go
@@ -6,28 +6,17 @@ import (
 )
 
 func init() {
-	installCmd.Flags().String("namespace", "default", "namespace where kubetest should be installed to")
 
 	RootCmd.AddCommand(installCmd)
 
+	installCmd.Flags().String("namespace", "default", "namespace where kubetest should be installed to")
 	installCmd.Flags().String("port", ":8080", "kubetest api server port")
-	RootCmd.AddCommand(connectCmd)
 }
 
 var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Install Helm chart registry in current kubectl context",
 	Long:  `Install can be configured with use of particular `,
-	Run: func(cmd *cobra.Command, args []string) {
-		ui.Errf("NOT IMPLEMENTED")
-	},
-}
-
-// TODO - remove after migrating API as API-server/delegator
-var connectCmd = &cobra.Command{
-	Use:   "connect",
-	Short: "Creates new connection with use of port-forward to local machine",
-	Long:  `temporary command - will be removed in incoming versions`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ui.Errf("NOT IMPLEMENTED")
 	},
