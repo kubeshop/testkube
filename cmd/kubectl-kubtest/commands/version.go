9f2ba405ea9a8e66a3e1dadc1ef79e748c6e52d5
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-19 08:30:03 +0200

    fixed installation

---
+++ cmd/kubectl-kubtest/commands/version.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"github.com/kubeshop/kubtest/pkg/ui"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows version and build info",
+	Long:  `Shows version and build info`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		ui.Logo()
+		ui.Info("Version", Version)
+		ui.Info("Commit", Commit)
+		ui.Info("Built by", BuiltBy)
+		ui.Info("Build date", Date)
+
+	},
+}
