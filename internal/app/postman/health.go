7e8ebf743f8a13f64b7f53d59d83293e7082cdd9
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-26 08:39:27 +0200

    removing postman executor

--- internal/app/postman/health.go
+++
@@ -1,9 +0,0 @@
-package postman
-
-import "github.com/gofiber/fiber/v2"
-
-func (p PostmanExecutor) HealthEndpoint() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		return c.SendString("OK 👋!")
-	}
-}
