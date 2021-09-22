f6ce3d788db08bb664cbd66836c03d5f47db555c
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-09-22 06:37:24 +0200

    fixed abort command

--- cmd/kubectl-kubtest/commands/scripts/abort_execution.go
+++ cmd/kubectl-kubtest/commands/scripts/abort_execution.go
@@ -10,20 +10,20 @@ import (
 func NewAbortExecutionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "abort",
-		Short: "(NOT IMPLEMENTED) Aborts execution of the script",
+		Short: "Aborts execution of the script",
 		Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			var scriptID string
+			var executionID string
 			if len(args) == 0 {
-				scriptID = "-"
-			} else if len(args) > 0 {
-				scriptID = args[0]
+				ui.Failf("Please pass execution ID as argument")
 			}
 
+			executionID = args[0]
+
 			client, _ := GetClient(cmd)
-			fmt.Println("....")
-			err := client.AbortExecution("script", scriptID)
-			ui.ExitOnError(fmt.Sprintf("aborting execution %s", scriptID), err)
+
+			err := client.AbortExecution("script", executionID)
+			ui.ExitOnError(fmt.Sprintf("aborting execution %s", executionID), err)
 		},
 	}
 }
