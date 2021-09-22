cbd1249b34c981956c16a2d6d3b1eb7e5ec1ed82
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-09-22 06:38:22 +0200

    removed printfs

--- pkg/api/client/direct_test.go
+++ pkg/api/client/direct_test.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,8 +13,6 @@ func TestScriptsAPI(t *testing.T) {
 		client := NewDefaultDirectScriptsAPI()
 		execution, err := client.GetExecution("test", "60f807e2cd42fbe1286ecfa3")
 
-		fmt.Printf("%+v\n", execution)
-
 		assert.NotNil(t, execution)
 		assert.NoError(t, err)
 
@@ -26,8 +23,6 @@ func TestScriptsAPI(t *testing.T) {
 		t.Skip("Implement real test") // TODO Implement me
 		client := NewDefaultDirectScriptsAPI()
 		execution, err := client.ExecuteScript("test", "default", "some name", map[string]string{})
-
-		fmt.Printf("%+v\n", execution)
 
 		assert.NotNil(t, execution)
 		assert.NoError(t, err)
