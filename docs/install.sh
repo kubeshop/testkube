2bb480c6807df7549ab7e53b6db24badc2b597b3
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-09-28 09:50:52 +0200

    Updating install script to version v0.5.36

--- docs/install.sh
+++ docs/install.sh
@@ -1,6 +1,6 @@
 echo "Getting kubectl-kubtest plugin"
 #!/bin/sh 
-KUBTEST_VERSION=${KUBTEST_VERSION:-"v0.5.35"} 
+KUBTEST_VERSION=${KUBTEST_VERSION:-"v0.5.36"} 
 
 if [ ! -z "${DEBUG}" ]; 
 then set -x 
