e82ee97217a2399d4d53f81516b01fd3d66b1f91
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-10-04 13:25:49 +0200

    skipping get proxy client

--- pkg/api/v1/client/factory_test.go
+++ pkg/api/v1/client/factory_test.go
@@ -10,6 +10,8 @@ import (
 func TestGetClient(t *testing.T) {
 
 	t.Run("get proxy client", func(t *testing.T) {
+		t.Skip("This one needs kubernetes config to work")
+
 		client, err := GetClient(ClientProxy, "default")
 		assert.NoError(t, err)
 		assert.Equal(t, "client.ProxyScriptsAPI", fmt.Sprintf("%T", client))
