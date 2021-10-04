0adaf5adffce6229bb7b6058c33b3576803e52d8
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-10-04 13:02:39 +0200

    unit tests

---
+++ pkg/api/v1/client/factory_test.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetClient(t *testing.T) {
+
+	t.Run("get proxy client", func(t *testing.T) {
+		client, err := GetClient(ClientProxy, "default")
+		assert.NoError(t, err)
+		assert.Equal(t, "client.ProxyScriptsAPI", fmt.Sprintf("%T", client))
+	})
+
+	t.Run("get direct client", func(t *testing.T) {
+		client, err := GetClient(ClientDirect, "default")
+		assert.NoError(t, err)
+		assert.Equal(t, "client.DirectScriptsAPI", fmt.Sprintf("%T", client))
+	})
+}
