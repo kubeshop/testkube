59e4195fbb3b754f51d6454072c407ed82929785
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-10-25 08:27:04 +0200

    checking version of server and warn if greater than client

--- cmd/kubectl-testkube/commands/root.go
+++ cmd/kubectl-testkube/commands/root.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Masterminds/semver"
+	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/scripts"
 	"github.com/kubeshop/testkube/pkg/ui"
 	"github.com/spf13/cobra"
 )
@@ -32,6 +34,32 @@ var RootCmd = &cobra.Command{
 		ui.Logo()
 		cmd.Usage()
 		cmd.DisableAutoGenTag = true
+	},
+
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		ui.Verbose = verbose
+
+		client, _ := scripts.GetClient(cmd)
+		info, err := client.GetServerInfo(namespace)
+		ui.ExitOnError("getting server info in namespace"+namespace, err)
+		ui.Info("server version", info.Version)
+		ui.Info("client version", Version)
+
+		serverVersion, err := semver.NewVersion(info.Version)
+		if err != nil {
+			ui.PrintOnError("parsing server version: "+info.Version, err)
+			return
+		}
+
+		clientVersion, err := semver.NewVersion(Version)
+		if err != nil {
+			ui.PrintOnError("parsing client version: "+Version, err)
+			return
+		}
+
+		if clientVersion.LessThan(serverVersion) {
+			ui.Warn(fmt.Sprintf("You're using old version of kubectl testkube plugin (%s) - please upgrade to %s", clientVersion.String(), serverVersion.String()))
+		}
 	},
 }
 
