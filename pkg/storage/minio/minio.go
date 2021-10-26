09b00ec3089138db5a4f635c36dacfb0345dc975
Jasmin Gacic <jasmin.gacic@gmail.com>
2021-10-26 15:19:07 +0200

    fixed incorrect import (#449)
    * fixed incorrect import
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>
    
    * one more fix
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>

--- pkg/storage/minio/minio.go
+++ pkg/storage/minio/minio.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
-	"github.com/kubeshop/kubtest/pkg/storage"
+	"github.com/kubeshop/testkube/pkg/storage"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
@@ -106,7 +107,9 @@ func (c *Client) SaveFile(bucket, filePath string) error {
 		fileName = objectStat.Name()
 	}
 
-	n, err := c.minioclient.PutObject(context.Background(), bucket, fileName, object, objectStat.Size(), minio.PutObjectOptions{ContentType: "application/octet-stream"})	if err != nil {
+	n, err := c.minioclient.PutObject(context.Background(), bucket, fileName, object, objectStat.Size(), minio.PutObjectOptions{ContentType: "application/octet-stream"})
+
+	if err != nil {
 		return err
 	}
 
