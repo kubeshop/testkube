43934a358a91918b1a6d43400104d6492e7c5056
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-11-15 09:10:17 +0100

    watch based on logs

--- cmd/kubectl-testkube/commands/scripts/start_script.go
+++ cmd/kubectl-testkube/commands/scripts/start_script.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/kubeshop/testkube/pkg/api/v1/client"
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/runner/output"
 	"github.com/kubeshop/testkube/pkg/ui"
 	"github.com/spf13/cobra"
 )
@@ -37,67 +40,11 @@ func NewStartScriptCmd() *cobra.Command {
 
 			PrintExecutionDetails(execution)
 
-			result := execution.ExecutionResult
-
-			switch true {
-
-			case result.IsQueued():
-				ui.Warn("Script queued for execution")
-
-			case result.IsPending():
-				ui.Warn("Script execution started")
-
-			case result.IsSuccesful():
-				fmt.Println(result.Output)
-				duration := execution.EndTime.Sub(execution.StartTime)
-				ui.Success("Script execution completed with sucess in " + duration.String())
-
-			case result.IsFailed():
-				fmt.Println(result.ErrorMessage)
-				ui.Errf("Script execution failed")
-			}
-
+			uiPrintStatus(execution)
 			uiShellCommandBlock(execution.Id)
 
 			if watch {
-				ui.Info("Watching for changes")
-
-				logs, err := client.Logs(execution.Id)
-				ui.ExitOnError("getting logs from exxcutor", err)
-
-				for l := range logs {
-					ui.LogLine(l.String())
-				}
-
-				for range time.Tick(time.Second) {
-
-					execution, err := client.GetExecution("-", execution.Id)
-					ui.ExitOnError("get script execution details", err)
-
-					render := GetExecutionRenderer(cmd)
-					err = render.Watch(execution, os.Stdout)
-					ui.ExitOnError("watching for changes", err)
-
-					if execution.ExecutionResult.IsCompleted() {
-						ui.Info("\nGetting results")
-						render.Render(execution, os.Stdout)
-						ui.ShellCommand(
-							"Use following command to get script execution details",
-							"kubectl testkube scripts execution "+execution.Id,
-						)
-						ui.Warn("Script execution completed in", execution.Duration().String())
-
-						if execution.ExecutionResult.IsFailed() {
-							os.Exit(1)
-						}
-						return
-					}
-				}
-
-			}
-
-			if execution.ExecutionResult.IsFailed() {
-				os.Exit(1)
+				watchLogs(execution.Id, client)
 			}
 		},
 	}
@@ -107,6 +54,46 @@ func NewStartScriptCmd() *cobra.Command {
 	cmd.Flags().BoolVarP(&watch, "watch", "f", false, "watch for changes after start")
 
 	return cmd
+}
+
+func watchLogs(id string, client client.Client) {
+	ui.Info("Watching for changes")
+
+	logs, err := client.Logs(id)
+	ui.ExitOnError("getting logs from exxcutor", err)
+
+	for l := range logs {
+		switch l.Type {
+		case output.TypeResult:
+			ui.Info("Execution completed", l.Result.Output)
+		default:
+			ui.LogLine(l.String())
+		}
+	}
+
+	uiShellCommandBlock(id)
+}
+
+func uiPrintStatus(execution testkube.Execution) {
+	result := execution.ExecutionResult
+
+	switch true {
+	case result.IsQueued():
+		ui.Warn("Script queued for execution")
+
+	case result.IsPending():
+		ui.Warn("Script execution started")
+
+	case result.IsSuccesful():
+		fmt.Println(result.Output)
+		duration := execution.EndTime.Sub(execution.StartTime)
+		ui.Success("Script execution completed with sucess in " + duration.String())
+
+	case result.IsFailed():
+		fmt.Println(result.ErrorMessage)
+		ui.Errf("Script execution failed")
+		os.Exit(1)
+	}
 }
 
 func uiShellCommandBlock(id string) {
