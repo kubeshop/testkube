2bae284502e33257338a8ecaf49e13775f00ef3a
nicufk <furtuna.nicolae@protonmail.com>
2021-12-20 16:05:48 +0200

    fix: use dashboard url for dashboard cmd

--- cmd/kubectl-testkube/commands/constants.go
+++ cmd/kubectl-testkube/commands/constants.go
@@ -8,6 +8,6 @@ const (
 	ApiServerName     string = "testkube-api-server"
 	ApiServerPort     int    = 8088
 	ApiVersion        string = "v1"
-	DashboardURI      string = "http://demo.testkube.io/apiEndpoint?apiEndpoint="
+	DashboardURI      string = "http://dashboard.testkube.io/apiEndpoint?apiEndpoint="
 	CurrentApiVersion string = apiclient.Version
 )
