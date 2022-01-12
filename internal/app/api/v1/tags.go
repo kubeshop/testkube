7794fe4be04a06e97e39185678ddba0f5e097599
nicufk <furtuna.nicolae@protonmail.com>
2022-01-12 16:20:02 +0200

    fix: improve list all tags handler

--- internal/app/api/v1/tags.go
+++ internal/app/api/v1/tags.go
@@ -1,19 +1,29 @@
 package v1
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/kubeshop/testkube/pkg/utils"
 )
 
 func (s TestKubeAPI) ListTagsHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		ctx := context.Background()
-		tags, err := s.ExecutionResults.GetTags(ctx)
+		namespace := c.Query("namespace", "testkube")
+		testsTags, err := s.TestsClient.ListTags(namespace)
 		if err != nil {
 			return s.Error(c, http.StatusBadRequest, err)
 		}
+
+		scriptsTags, err := s.ScriptsClient.ListTags(namespace)
+		if err != nil {
+			return s.Error(c, http.StatusBadRequest, err)
+		}
+
+		tags := append(testsTags, scriptsTags...)
+
+		tags = utils.RemoveDuplicates(tags)
 
 		return c.JSON(tags)
 	}
