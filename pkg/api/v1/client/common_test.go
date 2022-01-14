2e4d74b8b23bd231ee5c24136c9deb425ee2cb02
Vladislav Sukhin <vsukhin@envisionworld.co>
2022-01-14 12:47:58 +0300

    refactor: add unit tests for output parsing

--- pkg/api/v1/client/common_test.go
+++ pkg/api/v1/client/common_test.go
@@ -1,8 +1,13 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/kubeshop/testkube/pkg/runner/output"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,4 +17,27 @@ func TestTrimSSEChunk(t *testing.T) {
 	out := trimDataChunk(in)
 
 	assert.Equal(t, `{"type": "error","message": "some message"}`, string(out))
+}
+
+// TestStreamToLogsChannelOldFormat parses old output error format and return type field
+func TestStreamToLogsChannelOldErrorFormat(t *testing.T) {
+	log := make(chan output.Output)
+	in := []byte(`data: {"type": "error", "message": "some message"}\n\n`)
+	buf := bytes.NewBuffer(in)
+
+	go StreamToLogsChannel(buf, log)
+	result := <-log
+	assert.Equal(t, output.Output{Type_: "error", Content: ""}, result)
+}
+
+// TestStreamToLogsChannelNewFormat parses new output error format and return type and content fields
+func TestStreamToLogsChannelNewErrorFormat(t *testing.T) {
+	log := make(chan output.Output)
+	out, _ := json.Marshal(output.NewOutputError(errors.New("some message")))
+	in := []byte(fmt.Sprintf("%s\n", out))
+	buf := bytes.NewBuffer(in)
+
+	go StreamToLogsChannel(buf, log)
+	result := <-log
+	assert.Equal(t, output.Output{Type_: "error", Content: "some message"}, result)
 }
