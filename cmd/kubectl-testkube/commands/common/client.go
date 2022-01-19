6eb22fe7317b7851e434847450f06cecc4a31e9c
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-01-19 09:30:58 +0100

    fix: testkube installation namespace

--- cmd/kubectl-testkube/commands/common/client.go
+++ cmd/kubectl-testkube/commands/common/client.go
@@ -8,7 +8,10 @@ import (
 
 func GetClient(cmd *cobra.Command) (client.Client, string) {
 	clientType := cmd.Flag("client").Value.String()
-	namespace := cmd.Flag("namespace").Value.String()
+	// TODO allow to install testkube in different namespace
+	// we need to use some config for plugin then to save testkube installation namespace after first call
+	// testkube installation namespace
+	namespace := "testkube"
 
 	client, err := client.GetClient(client.ClientType(clientType), namespace)
 	ui.ExitOnError("setting up client type", err)
