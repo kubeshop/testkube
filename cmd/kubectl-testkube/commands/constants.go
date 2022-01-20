62296b77678d4d8ef0b10b9567ce3a2aa74b43fd
Vladislav Sukhin <vsukhin@kubeshop.io>
2022-01-20 20:12:55 +0300

    feat: allow to run local dashboard

--- cmd/kubectl-testkube/commands/constants.go
+++ cmd/kubectl-testkube/commands/constants.go
@@ -5,9 +5,12 @@ import (
 )
 
 const (
-	ApiServerName     string = "testkube-api-server"
-	ApiServerPort     int    = 8088
-	ApiVersion        string = "v1"
-	DashboardURI      string = "http://dashboard.testkube.io/apiEndpoint?apiEndpoint="
-	CurrentApiVersion string = apiclient.Version
+	ApiServerName      string = "testkube-api-server"
+	ApiServerPort      int    = 8088
+	ApiVersion         string = "v1"
+	DashboardURI       string = "http://dashboard.testkube.io"
+	CurrentApiVersion  string = apiclient.Version
+	DashboardName      string = "testkube-dashboard"
+	DashboardPort      int    = 80
+	DashboardLocalPort int    = 8080
 )
--- cmd/kubectl-testkube/commands/dashboard.go
+++ cmd/kubectl-testkube/commands/dashboard.go
@@ -2,15 +2,22 @@ package commands
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
+	"sync"
 
 	"github.com/kubeshop/testkube/pkg/process"
 	"github.com/kubeshop/testkube/pkg/ui"
 	"github.com/spf13/cobra"
 )
 
+// NewDashboardCmd is a method to create new dashboard command
 func NewDashboardCmd() *cobra.Command {
-	var namespace string
+	var (
+		namespace          string
+		useGlobalDashboard bool
+	)
+
 	cmd := &cobra.Command{
 		Use:   "dashboard",
 		Short: "Open testkube dashboard",
@@ -20,23 +27,52 @@ func NewDashboardCmd() *cobra.Command {
 			ui.Verbose = true
 			ui.Logo()
 
-			dashboardAddress := fmt.Sprintf("%slocalhost:%d/%s", DashboardURI, ApiServerPort, ApiVersion)
+			uri := fmt.Sprintf("http://localhost:%d", DashboardLocalPort)
+			if useGlobalDashboard {
+				uri = DashboardURI
+			}
+
+			dashboardAddress := fmt.Sprintf("%s/apiEndpoint?apiEndpoint=localhost:%d/%s", uri, ApiServerPort, ApiVersion)
 			ui.Success("The dashboard is accessible here:", dashboardAddress)
 			ui.Success("Port forwarding is started for the test results endpoint, hit Ctrl+C to stop")
+
+			var (
+				wg      sync.WaitGroup
+				command *exec.Cmd
+				err     error
+			)
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				command, err = process.ExecuteAsync("kubectl", "port-forward",
+					"--namespace", namespace,
+					fmt.Sprintf("deployment/%s", DashboardName),
+					fmt.Sprintf("%d:%d", DashboardPort, DashboardLocalPort))
+			}()
+
+			wg.Wait()
+			ui.ExitOnError("port forwarding dashboard endpoint", err)
+
 			openCmd, err := getOpenCommand()
 			if err == nil {
 				_, err = process.Execute(openCmd, dashboardAddress)
 				ui.PrintOnError("openning dashboard", err)
 			}
+
 			_, err = process.Execute("kubectl", "port-forward",
 				"--namespace", namespace,
 				fmt.Sprintf("deployment/%s", ApiServerName),
 				fmt.Sprintf("%d:%d", ApiServerPort, ApiServerPort))
 			ui.PrintOnError("port forwarding results endpoint", err)
+
+			err = command.Process.Kill()
+			ui.ExitOnError("process killing", err)
 		},
 	}
 
 	cmd.Flags().StringVarP(&namespace, "namespace", "s", "testkube", "namespace where the testkube is installed")
+	cmd.Flags().BoolVar(&useGlobalDashboard, "use-global-dashboard", false, "use global dashboard for viewing testkube results")
 	return cmd
 }
 
