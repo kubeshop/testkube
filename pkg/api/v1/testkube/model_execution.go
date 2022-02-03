31862130f10d3fbe3df4ea5674618a96d659be38
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-02-03 10:16:19 +0100

    feat: added paramsFile to Executions resource

--- pkg/api/v1/testkube/model_execution.go
+++ pkg/api/v1/testkube/model_execution.go
@@ -30,8 +30,10 @@ type Execution struct {
 	// additional arguments/flags passed to executor binary
 	Args []string `json:"args,omitempty"`
 	// execution params passed to executor converted to vars for usage in tests
-	Params  map[string]string `json:"params,omitempty"`
-	Content *ScriptContent    `json:"content,omitempty"`
+	Params map[string]string `json:"params,omitempty"`
+	// params file content - need to be in format for particular executor (e.g. postman envs file)
+	ParamsFile string         `json:"paramsFile,omitempty"`
+	Content    *ScriptContent `json:"content,omitempty"`
 	// test start time
 	StartTime time.Time `json:"startTime,omitempty"`
 	// test end time
