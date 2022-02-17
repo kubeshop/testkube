00b86731dbcf167cbaf12a5b5371e1b7252aad21
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-02-17 10:25:03 +0100

    Update cmd/kubectl-testkube/commands/testsuites/delete.go
    Co-authored-by: nicufk <89570185+nicufk@users.noreply.github.com>

--- cmd/kubectl-testkube/commands/testsuites/delete.go
+++ cmd/kubectl-testkube/commands/testsuites/delete.go
@@ -11,7 +11,7 @@ func NewDeleteTestSuiteCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "delete <testSuiteName>",
 		Short: "Delete tests",
-		Long:  `Delete tests by name`,
+		Long:  `Delete test suite by name`,
 		Args:  validator.TestSuiteName,
 		Run: func(cmd *cobra.Command, args []string) {
 			ui.Logo()
