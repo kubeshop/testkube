7d12f9219d4a9310435532328ccbd6a79ed1a711
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-03-03 10:55:58 +0100

    fix: status code

--- internal/app/api/v1/webhook.go
+++ internal/app/api/v1/webhook.go
@@ -69,7 +69,7 @@ func (s TestkubeAPI) DeleteWebhookHandler() fiber.Handler {
 			return s.Error(c, http.StatusBadRequest, err)
 		}
 
-		c.Context().SetStatusCode(204)
+		c.Status(204)
 		return nil
 	}
 }
