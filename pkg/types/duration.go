3d5bc6b0eef55eae8c69dba0a6b306ec2d60e01e
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-03-18 09:28:07 +0100

    feat: added duration to executons and ts executions (#1114)
    * feat: added duration to executons and ts executions
    
    * formatted tests
    
    * fixed test

---
+++ pkg/types/duration.go
@@ -0,0 +1,30 @@
+package types
+
+import "time"
+
+const DefaultDurationFormat = "15:04:05"
+const InvalidDurationString = "invalid"
+
+func FormatDuration(in string) string {
+	if in == "" {
+		return ""
+	}
+	duration, err := time.ParseDuration(in)
+	if err != nil {
+		return InvalidDurationString + " " + err.Error()
+	}
+
+	return FormattedDuration(duration).Format()
+}
+
+type FormattedDuration time.Duration
+
+func (t FormattedDuration) Format(formats ...string) string {
+	format := DefaultDurationFormat
+	if len(formats) > 0 {
+		format = formats[0]
+	}
+
+	z := time.Unix(0, 0).UTC()
+	return z.Add(time.Duration(t)).Format(format)
+}
