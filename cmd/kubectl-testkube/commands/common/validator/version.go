b0449a5827e7c7f7c345184fa6bbfff30f0d4805
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-04-28 06:03:26 +0200

    feat: minor version warning only (#1404)

--- cmd/kubectl-testkube/commands/common/validator/version.go
+++ cmd/kubectl-testkube/commands/common/validator/version.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/Masterminds/semver"
 	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/common"
@@ -10,6 +11,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// PersistentPreRunVersionCheck will check versions based on commands client
 func PersistentPreRunVersionCheck(cmd *cobra.Command, version string) {
 	// version validation
 	// if client version is less than server version show warning
@@ -21,6 +23,7 @@ func PersistentPreRunVersionCheck(cmd *cobra.Command, version string) {
 	}
 }
 
+// ValidateVersions will check if kubectl plugins MINOR version is greater or equal Testkube API version
 func ValidateVersions(c apiclient.Client, version string) error {
 	info, err := c.GetServerInfo()
 	if err != nil {
@@ -31,7 +34,8 @@ func ValidateVersions(c apiclient.Client, version string) error {
 		return fmt.Errorf("server version not set")
 	}
 
-	serverVersion, err := semver.NewVersion(info.Version)
+	apiMinorVersion := TrimPatchVersion(info.Version)
+	apiVersion, err := semver.NewVersion(apiMinorVersion)
 	if err != nil {
 		return fmt.Errorf("parsing server version '%s': %w", info.Version, err)
 	}
@@ -40,18 +44,30 @@ func ValidateVersions(c apiclient.Client, version string) error {
 		return fmt.Errorf("client version not set")
 	}
 
-	clientVersion, err := semver.NewVersion(version)
+	minorVersion := TrimPatchVersion(version)
+	clientVersion, err := semver.NewVersion(minorVersion)
 	if err != nil {
 		return fmt.Errorf("parsing client version %s: %w", version, err)
 	}
 
-	if clientVersion.LessThan(serverVersion) {
+	if clientVersion.LessThan(apiVersion) {
 		ui.Warn("Your Testkube API version is newer than your `kubectl testkube` plugin")
-		ui.Info("Testkube API version", serverVersion.String())
+		ui.Info("Testkube API version", apiVersion.String())
 		ui.Info("Testkube kubectl plugin client", clientVersion.String())
 		ui.Info("It's recommended to upgrade client to version close to API server version")
 		ui.NL()
 	}
 
 	return nil
+}
+
+// TrimPatchVersion will trim
+func TrimPatchVersion(version string) string {
+	re := regexp.MustCompile("([0-9]+).([0-9]+).([0-9]+)(.*)")
+	parts := re.FindStringSubmatch(version)
+	if len(parts) == 5 {
+		return fmt.Sprintf("%s.%s.%s", parts[1], parts[2], "0")
+	}
+
+	return version
 }
