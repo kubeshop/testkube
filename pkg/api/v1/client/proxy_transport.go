2f10312c36aa6730c72690030fe8fdaff3761898
Vladislav Sukhin <vladislav@kubeshop.io>
2022-05-18 15:01:07 +0300

    fix: missed import

--- pkg/api/v1/client/proxy_transport.go
+++ pkg/api/v1/client/proxy_transport.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"	
 
 	"github.com/kubeshop/testkube/pkg/executor/output"
 	"github.com/kubeshop/testkube/pkg/problem"
