d6391bddcbaacd8e768e7f9003615ba67c89c989
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-05-27 11:50:55 +0200

    k6 fix

--- docs/demo-video/resources/Video-chuck-k6.js
+++ docs/demo-video/resources/Video-chuck-k6.js
@@ -1,9 +1,8 @@
-
 import http from 'k6/http';
-import { sleep,check } from 'k6';
+import { check } from 'k6';
 
 export default function () {
-  const baseURI = `${__ENV.API_URI || 'http://testkube-api-server:8881'}`;
+  const baseURI = `${__ENV.API_URI || 'http://testkube-example-service:8881'}`;
 
   check(http.get(`${baseURI}/joke`), {
     'joke should be about Chuck': r => r.body.includes("Chuck")
