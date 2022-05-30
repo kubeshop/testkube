909da0c9b375815838798596e3d7e079a76ccd28
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-05-30 07:19:50 +0200

    fixes

--- docs/demo-video/resources/chuck-jokes.k6.js
+++ docs/demo-video/resources/chuck-jokes.k6.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export default function () {
-  const baseURI = `${__ENV.API_URI || 'http://testkube-example-service:8881'}`;
+  const baseURI = `${__ENV.API_URI || 'http://chuck-jokes:8881'}`;
 
   check(http.get(`${baseURI}/joke`), {
     'joke should be about Chuck': r => r.body.includes("Chuck")
---
+++ docs/demo-video/resources/commands.sh
@@ -0,0 +1,20 @@
+brew install testkube
+
+kubectl testkube version
+
+kubectl testkube install
+
+kubectl testkube version
+
+kubectl get pods -ntestkube
+
+kubectl testkube dashboard
+
+docker build  --platform linux/x86_64 -t kubeshop/chuck-jokes .
+docker push kubeshop/chuck-jokes
+
+kubectl apply -f manifests.yaml
+
+kubectl testkube create test --file chuck-jokes.postman_collection.json --name chuck-jokes-postman
+
+# upload with UI
