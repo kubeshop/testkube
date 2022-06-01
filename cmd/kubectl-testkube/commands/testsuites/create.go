f1a17120639838f02090fb8cfe367869895da7b5
Vladislav Sukhin <vladislav@kubeshop.io>
2022-06-01 15:05:05 +0300

    feat: test suite creation dry run

--- cmd/kubectl-testkube/commands/testsuites/create.go
+++ cmd/kubectl-testkube/commands/testsuites/create.go
@@ -2,12 +2,14 @@ package testsuites
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 
 	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/common"
 	apiClient "github.com/kubeshop/testkube/pkg/api/v1/client"
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/crd"
 	"github.com/kubeshop/testkube/pkg/ui"
 	"github.com/robfig/cron"
 	"github.com/spf13/cobra"
@@ -22,6 +24,7 @@ func NewCreateTestSuitesCmd() *cobra.Command {
 		variables       map[string]string
 		secretVariables map[string]string
 		schedule        string
+		crdOnly         bool
 	)
 
 	cmd := &cobra.Command{
@@ -74,9 +77,21 @@ func NewCreateTestSuitesCmd() *cobra.Command {
 			err = validateSchedule(options.Schedule)
 			ui.ExitOnError("validating schedule", err)
 
-			test, err = client.CreateTestSuite((apiClient.UpsertTestSuiteOptions(options)))
-			ui.ExitOnError("creating TestSuite "+options.Name+" in namespace "+options.Namespace, err)
-			ui.Success("TestSuite created", options.Name)
+			if !crdOnly {
+				test, err = client.CreateTestSuite((apiClient.UpsertTestSuiteOptions(options)))
+				ui.ExitOnError("creating test suite "+options.Name+" in namespace "+options.Namespace, err)
+
+				ui.Success("Test suite created", options.Name)
+			} else {
+				if options.Description != "" {
+					options.Description = fmt.Sprintf("%q", options.Description)
+				}
+
+				data, err := crd.ExecuteTemplate(crd.TemplateTestSuite, options)
+				ui.ExitOnError("executing crd template", err)
+
+				ui.Info(data)
+			}
 		},
 	}
 
@@ -86,6 +101,7 @@ func NewCreateTestSuitesCmd() *cobra.Command {
 	cmd.Flags().StringToStringVarP(&variables, "variable", "v", nil, "param key value pair: --variable key1=value1")
 	cmd.Flags().StringToStringVarP(&secretVariables, "secret-variable", "s", nil, "secret variable key value pair: --secret-variable key1=value1")
 	cmd.Flags().StringVarP(&schedule, "schedule", "", "", "test suite schedule in a cronjob form: * * * * *")
+	cmd.Flags().BoolVar(&crdOnly, "crd-only", false, "generate only test suite crd")
 
 	return cmd
 }
