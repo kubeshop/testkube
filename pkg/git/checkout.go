1946e75513140750b7dc98f3e8604d7992696b26
Vladislav Sukhin <vladislav@kubeshop.io>
2022-07-19 21:11:41 +0300

    feat: commit checkout method

--- pkg/git/checkout.go
+++ pkg/git/checkout.go
@@ -9,6 +9,66 @@ import (
 	"github.com/kubeshop/testkube/pkg/process"
 )
 
+func CheckoutCommit(uri, path, commit, dir string) error {
+	_, err := process.ExecuteInDir(
+		dir,
+		"git",
+		"init",
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = process.ExecuteInDir(
+		dir,
+		"git",
+		"remote",
+		"add",
+		"origin",
+		uri,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = process.ExecuteInDir(
+		dir,
+		"git",
+		"--depth",
+		"1",
+		"origin",
+		commit,
+	)
+	if err != nil {
+		return err
+	}
+
+	if path != "" {
+		_, err = process.ExecuteInDir(
+			dir,
+			"git",
+			"sparse-checkout",
+			"set",
+			path,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = process.ExecuteInDir(
+		dir,
+		"git",
+		"checkout",
+		"FETCH_HEAD",
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Checkout will checkout directory from Git repository
 func Checkout(uri, branch, commit, dir string) (outputDir string, err error) {
 	tmpDir := dir
@@ -19,32 +79,23 @@ func Checkout(uri, branch, commit, dir string) (outputDir string, err error) {
 		}
 	}
 
-	args := []string{"clone"}
-	if branch != "" {
-		args = append(args, "-b", branch)
-	}
+	if commit == "" {
+		args := []string{"clone"}
+		if branch != "" {
+			args = append(args, "-b", branch)
+		}
 
-	args = append(args, "--depth", "1", uri, "repo")
-	_, err = process.ExecuteInDir(
-		tmpDir,
-		"git",
-		args...,
-	)
-	if err != nil {
-		return "", err
-	}
-
-	if commit != "" {
+		args = append(args, "--depth", "1", uri, "repo")
 		_, err = process.ExecuteInDir(
-			tmpDir+"/repo",
+			tmpDir,
 			"git",
-			"reset",
-			"--hard",
-			commit,
+			args...,
 		)
 		if err != nil {
 			return "", err
 		}
+	} else {
+
 	}
 
 	return tmpDir + "/repo/", nil
@@ -60,43 +111,34 @@ func PartialCheckout(uri, path, branch, commit, dir string) (outputDir string, e
 		}
 	}
 
-	args := []string{"clone"}
-	if branch != "" {
-		args = append(args, "-b", branch)
-	}
+	if commit == "" {
+		args := []string{"clone"}
+		if branch != "" {
+			args = append(args, "-b", branch)
+		}
 
-	args = append(args, "--depth", "1", "--filter", "blob:none", "--sparse", uri, "repo")
-	_, err = process.ExecuteInDir(
-		tmpDir,
-		"git",
-		args...,
-	)
-	if err != nil {
-		return "", err
-	}
-
-	if commit != "" {
+		args = append(args, "--depth", "1", "--filter", "blob:none", "--sparse", uri, "repo")
 		_, err = process.ExecuteInDir(
-			tmpDir+"/repo",
+			tmpDir,
 			"git",
-			"reset",
-			"--hard",
-			commit,
+			args...,
 		)
 		if err != nil {
 			return "", err
 		}
-	}
 
-	_, err = process.ExecuteInDir(
-		tmpDir+"/repo",
-		"git",
-		"sparse-checkout",
-		"set",
-		path,
-	)
-	if err != nil {
-		return "", err
+		_, err = process.ExecuteInDir(
+			tmpDir+"/repo",
+			"git",
+			"sparse-checkout",
+			"set",
+			path,
+		)
+		if err != nil {
+			return "", err
+		}
+	} else {
+
 	}
 
 	return tmpDir + "/repo/" + path, nil
