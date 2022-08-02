13b6fc8fdd7fe5c8c6a4d85f4945c2da6ef82830
nicufk <nicufk@users.noreply.github.com>
2022-08-02 11:19:45 +0000

    [create-pull-request] automated change

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'testkube_1.4.2_Windows_i386.msi'
+$fileLocation = Join-Path $toolsDir 'testkube_1.4.3_Windows_i386.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'testkube'
 
-  checksum      = 'E2122392B2AD5314A08AD6B34C4E55E75088DAC75162A46186050B6A8240F6F7'
+  checksum      = 'A189185DC05BD877FEFEACEF483CA61A4ADB2F347E7A1070446473886928603E'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
