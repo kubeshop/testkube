185ca0f22669ce44c7a84ccd6ab0769fa1d3eb37
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-08-09 14:01:45 +0200

    fix: better time example

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -11,19 +11,19 @@ func TestRoundDuration(t *testing.T) {
 	t.Run("round duration should round to default 100ms", func(t *testing.T) {
 
 		//given
-		d := time.Duration(99999999999)
+		d := time.Duration(99111111111)
 
 		// when
 		rounded := RoundDuration(d)
 
 		// then
-		assert.Equal(t, "1m40s", rounded.String())
+		assert.Equal(t, "1m39.11s", rounded.String())
 	})
 
 	t.Run("round duration should round to given value", func(t *testing.T) {
 
 		//given
-		d := time.Duration(99999999999)
+		d := time.Duration(99111111111)
 
 		// when
 		rounded := RoundDuration(d, time.Minute)
