79fd80c6d245d80fc933396393b85456c1023eee
nicufk <nicolae@kubeshop.io>
2022-09-05 06:51:04 -0300

    fix: obfuscate secret refs as well

--- pkg/api/v1/testkube/model_variables_extended.go
+++ pkg/api/v1/testkube/model_variables_extended.go
@@ -1,7 +1,10 @@
 package testkube
 
 import (
+	"os"
 	"strings"
+
+	"github.com/kubeshop/testkube/pkg/secret"
 )
 
 type Variables map[string]Variable
@@ -16,9 +19,27 @@ func VariablesToMap(v Variables) map[string]string {
 	return vars
 }
 
-func ObfuscateSecrets(output string, variables Variables) string {
+func ObfuscateSecrets(output string, variables Variables, testName string) string {
+	// TODO: this is ugly, does anybody have a better idea?
+	namespace := "testkube"
+	if ns, ok := os.LookupEnv("TESTKUBE_NAMESPACE"); ok {
+		namespace = ns
+	}
+	secretClient, err := secret.NewClient(namespace)
+	var secretKeyValues map[string]string
+	if err == nil {
+		secretKeyValues, _ = secretClient.Get(secret.GetMetadataName(testName))
+	}
+
 	for _, v := range variables {
-		if v.Type_ == VariableTypeSecret {
+		secretValue := ""
+		if *v.Type_ == SECRET_VariableType {
+			secretValue = v.Value
+		} else if *v.Type_ == SECRET_VariableType && v.SecretRef != nil {
+			secretValue = secretKeyValues[v.Value]
+		}
+
+		if secretValue != "" {
 			output = strings.ReplaceAll(output, `'`+v.Value+`'`, "********")
 			output = strings.ReplaceAll(output, `"`+v.Value+`"`, "********")
 		}
