11af3160914ef9d0c5fe1c8f6e37408884974ebd
nicufk <nicolae@kubeshop.io>
2022-09-05 17:00:46 -0300

    fix: secrets key value getting

--- pkg/api/v1/testkube/model_variables_extended.go
+++ pkg/api/v1/testkube/model_variables_extended.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"strings"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/kubeshop/testkube/pkg/log"
 	"github.com/kubeshop/testkube/pkg/secret"
 )
 
@@ -28,15 +31,19 @@ func ObfuscateSecrets(output string, variables Variables, testName string) strin
 	secretClient, err := secret.NewClient(namespace)
 	var secretKeyValues map[string]string
 	if err == nil {
-		secretKeyValues, _ = secretClient.Get(secret.GetMetadataName(testName))
+		secretKeyValues, err = secretClient.Get(secret.GetMetadataName(testName))
+		if err != nil && !errors.IsNotFound(err) {
+			log.DefaultLogger.Warnw("error getting secret", "error", err)
+		}
 	}
 
 	for _, v := range variables {
 		secretValue := ""
 		if *v.Type_ == SECRET_VariableType {
 			secretValue = v.Value
-		} else if *v.Type_ == SECRET_VariableType && v.SecretRef != nil {
-			secretValue = secretKeyValues[v.Value]
+			if *v.Type_ == SECRET_VariableType && v.SecretRef != nil {
+				secretValue = secretKeyValues[v.SecretRef.Key]
+			}
 		}
 
 		if secretValue != "" {
