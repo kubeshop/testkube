28db2b89fa09669b38c2b706a578fe6d6b213c68
Vladislav Sukhin <vladislav@kubeshop.io>
2022-09-14 20:40:34 +0300

    feat: additional parameters for test source commands

---
+++ cmd/kubectl-testkube/commands/testsources/common.go
@@ -0,0 +1,104 @@
+package testsources
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+)
+
+func newSourceFromFlags(cmd *cobra.Command) (source *testkube.TestSource, err error) {
+	var fileContent []byte
+
+	sourceType := cmd.Flag("source-type").Value.String()
+	file := cmd.Flag("file").Value.String()
+	uri := cmd.Flag("uri").Value.String()
+	gitUri := cmd.Flag("git-uri").Value.String()
+	gitBranch := cmd.Flag("git-branch").Value.String()
+	gitCommit := cmd.Flag("git-commit").Value.String()
+	gitPath := cmd.Flag("git-path").Value.String()
+	gitUsername := cmd.Flag("git-username").Value.String()
+	gitToken := cmd.Flag("git-token").Value.String()
+	gitUsernameSecret, err := cmd.Flags().GetStringToString("git-username-secret")
+	if err != nil {
+		return source, err
+	}
+
+	gitTokenSecret, err := cmd.Flags().GetStringToString("git-token-secret")
+	if err != nil {
+		return source, err
+	}
+
+	// get file content
+	if file != "" {
+		fileContent, err = os.ReadFile(file)
+		if err != nil {
+			return source, fmt.Errorf("reading file "+file+" error: %w", err)
+		}
+	} else if stat, _ := os.Stdin.Stat(); (stat.Mode() & os.ModeCharDevice) == 0 {
+		fileContent, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			return source, fmt.Errorf("reading stdin error: %w", err)
+		}
+	}
+
+	// content is correct when is passed from file, by uri, ur by git repo
+	if len(fileContent) == 0 && uri == "" && gitUri == "" {
+		return source, fmt.Errorf("empty test source, please pass some data to create test source")
+	}
+
+	if gitUri != "" && sourceType == "" {
+		sourceType = string(testkube.TestContentTypeGitDir)
+	}
+
+	if uri != "" && sourceType == "" {
+		sourceType = string(testkube.TestContentTypeFileURI)
+	}
+
+	if len(fileContent) > 0 {
+		sourceType = string(testkube.TestContentTypeString)
+	}
+
+	var repository *testkube.Repository
+	if gitUri != "" {
+		if sourceType == "" {
+			sourceType = "git-dir"
+		}
+
+		repository = &testkube.Repository{
+			Type_:    "git",
+			Uri:      gitUri,
+			Branch:   gitBranch,
+			Commit:   gitCommit,
+			Path:     gitPath,
+			Username: gitUsername,
+			Token:    gitToken,
+		}
+
+		for key, val := range gitUsernameSecret {
+			repository.UsernameSecret = &testkube.SecretRef{
+				Name: key,
+				Key:  val,
+			}
+		}
+
+		for key, val := range gitTokenSecret {
+			repository.TokenSecret = &testkube.SecretRef{
+				Name: key,
+				Key:  val,
+			}
+		}
+	}
+
+	source = &testkube.TestSource{
+		Type_:      sourceType,
+		Data:       string(fileContent),
+		Repository: repository,
+		Uri:        uri,
+	}
+
+	return source, nil
+}
--- cmd/kubectl-testkube/commands/testsources/create.go
+++ cmd/kubectl-testkube/commands/testsources/create.go
@@ -12,8 +12,18 @@ import (
 
 func NewCreateTestSourceCmd() *cobra.Command {
 	var (
-		name, uri string
-		labels    map[string]string
+		name, uri         string
+		sourceType        string
+		file              string
+		gitUri            string
+		gitBranch         string
+		gitCommit         string
+		gitPath           string
+		gitUsername       string
+		gitToken          string
+		labels            map[string]string
+		gitUsernameSecret map[string]string
+		gitTokenSecret    map[string]string
 	)
 
 	cmd := &cobra.Command{
@@ -61,8 +71,18 @@ func NewCreateTestSourceCmd() *cobra.Command {
 	}
 
 	cmd.Flags().StringVarP(&name, "name", "n", "", "unique test source name - mandatory")
-	cmd.Flags().StringVarP(&uri, "uri", "u", "", "URI which should be called when given event occurs")
 	cmd.Flags().StringToStringVarP(&labels, "label", "l", nil, "label key value pair: --label key1=value1")
+	cmd.Flags().StringVarP(&sourceType, "source-type", "", "", "source type of test one of string|file-uri|git-file|git-dir")
+	cmd.Flags().StringVarP(&file, "file", "f", "", "source file - will be read from stdin if not specified")
+	cmd.Flags().StringVarP(&uri, "uri", "u", "", "URI which should be called when given event occurs")
+	cmd.Flags().StringVarP(&gitUri, "git-uri", "", "", "Git repository uri")
+	cmd.Flags().StringVarP(&gitBranch, "git-branch", "", "", "if uri is git repository we can set additional branch parameter")
+	cmd.Flags().StringVarP(&gitCommit, "git-commit", "", "", "if uri is git repository we can use commit id (sha) parameter")
+	cmd.Flags().StringVarP(&gitPath, "git-path", "", "", "if repository is big we need to define additional path to directory/file to checkout partially")
+	cmd.Flags().StringVarP(&gitUsername, "git-username", "", "", "if git repository is private we can use username as an auth parameter")
+	cmd.Flags().StringVarP(&gitToken, "git-token", "", "", "if git repository is private we can use token as an auth parameter")
+	cmd.Flags().StringToStringVarP(&gitUsernameSecret, "git-username-secret", "", map[string]string{}, "git username secret in a form of secret_name1=secret_key1 for private repository")
+	cmd.Flags().StringToStringVarP(&gitTokenSecret, "git-token-secret", "", map[string]string{}, "git token secret in a form of secret_name1=secret_key1 for private repository")
 
 	return cmd
 }
