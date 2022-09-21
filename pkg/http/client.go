9d9e598e088c4798f63d5500664f6e80fe90f834
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-09-21 13:28:51 +0200

    fix: comment

--- pkg/http/client.go
+++ pkg/http/client.go
@@ -25,6 +25,7 @@ func NewClient() *http.Client {
 	}
 }
 
+// NewSSEClient is HTTP client with long timeout to be able to read SSE endpoints
 func NewSSEClient() *http.Client {
 	return &http.Client{
 		Timeout: time.Hour,
