13bf4b540e151b01fb748fa64f6d55561621f734
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2022-09-30 14:49:58 +0200

    Cypress - ENV validation added

--- test/cypress/executor-smoke/cypress-10/cypress/e2e/dummy_test.cy.js
+++ test/cypress/executor-smoke/cypress-10/cypress/e2e/dummy_test.cy.js
@@ -2,4 +2,12 @@ describe('Testkube website', () => {
   it('Open Testkube website', () => {
     cy.visit('/')
   })
+  it(`Print CYPRESS_CUSTOM_ENV ENV: ${Cypress.env('CUSTOM_ENV')}`, () => {
+  })
+  it(`Print NON_CYPRESS_ENV ENV: ${Cypress.env('NON_CYPRESS_ENV')}`, () => {
+  })
+  it('Validate ENVs', () => {
+    expect('CYPRESS_CUSTOM_ENV_value').to.equal(Cypress.env('CUSTOM_ENV')) //CYPRESS_CUSTOM_ENV - "cypress" prefix - auto-loaded from global ENVs
+    expect('NON_CYPRESS_ENV_value').to.equal(Cypress.env('NON_CYPRESS_ENV')) //NON_CYPRESS_ENV - need to be loaded with --env parameter
+  })
 })
