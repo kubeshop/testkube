192e00e10fa9ae710c2d1f72ad2c328acc64faf2
Povilas Versockas <p.versockas@gmail.com>
2022-09-30 10:58:46 +0300

    feat: add support for ImagePullSecret in job executors (#2423)

--- pkg/scheduler/test_scheduler_test.go
+++ pkg/scheduler/test_scheduler_test.go
@@ -1,9 +1,18 @@
 package scheduler
 
 import (
-	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	v1 "github.com/kubeshop/testkube-operator/apis/executor/v1"
+	testsv3 "github.com/kubeshop/testkube-operator/apis/tests/v3"
+	executorsclientv1 "github.com/kubeshop/testkube-operator/client/executors/v1"
+	testsclientv3 "github.com/kubeshop/testkube-operator/client/tests/v3"
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/executor/client"
+	"github.com/stretchr/testify/assert"
+	k8sv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestParamsNilAssign(t *testing.T) {
@@ -40,4 +49,90 @@ func TestParamsNilAssign(t *testing.T) {
 		assert.Equal(t, "2", out["p2"].Value)
 	})
 
+}
+
+func TestGetExecuteOptions(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockTestsClient := testsclientv3.NewMockInterface(mockCtrl)
+	mockExecutorsClient := executorsclientv1.NewMockInterface(mockCtrl)
+
+	sc := Scheduler{testsClient: mockTestsClient, executorsClient: mockExecutorsClient}
+
+	mockTest := testsv3.Test{
+		ObjectMeta: metav1.ObjectMeta{Namespace: "testkube", Name: "some-test"},
+		Spec: testsv3.TestSpec{
+			Type_: "cypress",
+			ExecutionRequest: &testsv3.ExecutionRequest{
+				Name:   "some-custom-execution",
+				Number: 1,
+				Image:  "test-image",
+			},
+		},
+	}
+	mockExecutorTypes := "cypress"
+	mockExecutor := v1.Executor{
+		TypeMeta:   metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{Namespace: "testkube", Name: "cypress"},
+		Spec: v1.ExecutorSpec{
+			Types:            []string{mockExecutorTypes},
+			ExecutorType:     "job",
+			URI:              "",
+			Image:            "cypress",
+			Args:             nil,
+			Command:          []string{"run"},
+			ImagePullSecrets: []k8sv1.LocalObjectReference{{Name: "secret-name1"}, {Name: "secret-name2"}},
+			Features:         nil,
+			ContentTypes:     nil,
+			JobTemplate:      "",
+		},
+	}
+
+	mockTestsClient.EXPECT().Get("id").Return(&mockTest, nil).Times(1)
+	mockExecutorsClient.EXPECT().GetByType(mockExecutorTypes).Return(&mockExecutor, nil)
+
+	req := testkube.ExecutionRequest{
+		Name:             "id-1",
+		Number:           1,
+		ExecutionLabels:  map[string]string{"label": "value"},
+		Namespace:        "namespace",
+		VariablesFile:    "",
+		Variables:        map[string]testkube.Variable{"var": testkube.Variable{Name: "one"}},
+		Command:          []string{},
+		Args:             []string{},
+		Image:            "executor-image",
+		ImagePullSecrets: []testkube.LocalObjectReference{},
+		Envs: map[string]string{
+			"env": "var",
+		},
+		SecretEnvs: map[string]string{
+			"secretEnv": "secretVar",
+		},
+		Sync:       false,
+		HttpProxy:  "",
+		HttpsProxy: "",
+		CopyFiles: map[string][]byte{
+			"": {},
+		},
+	}
+
+	got, err := sc.getExecuteOptions("namespace", "id", req)
+	assert.NoError(t, err)
+
+	want := client.ExecuteOptions{
+		ID:                   "",
+		TestName:             "id",
+		Namespace:            "namespace",
+		TestSpec:             mockTest.Spec,
+		ExecutorName:         "cypress",
+		ExecutorSpec:         mockExecutor.Spec,
+		Request:              req,
+		Sync:                 false,
+		Labels:               map[string]string(nil),
+		ImageOverride:        "executor-image",
+		ImagePullSecretNames: []string{"secret-name1", "secret-name2"},
+	}
+
+	assert.Equal(t, want, got)
 }
