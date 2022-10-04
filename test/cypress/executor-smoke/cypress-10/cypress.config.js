af89625e1831a7f85ad0b1aeb6a09c3c5caba483
Vladislav Sukhin <vladislav@kubeshop.io>
2022-10-04 14:08:55 +0300

    fix: test steps

--- test/cypress/executor-smoke/cypress-10/cypress.config.js
+++ test/cypress/executor-smoke/cypress-10/cypress.config.js
@@ -1,17 +1,7 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
-    setupNodeEvents(on, config) {
-      on('task', {
-        log(message) {
-          console.log(message)
-      
-          return null
-        },
-      })
-    }
-  }
-})
+    baseUrl: 'https://testkube.kubeshop.io/'
+  },
+});
--- test/cypress/executor-smoke/cypress-10/cypress/e2e/dummy_test.cy.js
+++ test/cypress/executor-smoke/cypress-10/cypress/e2e/dummy_test.cy.js
@@ -1,15 +1,11 @@
 describe('Testkube website', () => {
   it('Open Testkube website', () => {
     cy.visit('/')
-    cy.task("log", Cypress.env())
   })
-  it(`Validate CYPRESS_CUSTOM_ENV ENV (${Cypress.env('CUSTOM_ENV')})`, () => {
-    expect('CYPRESS_CUSTOM_ENV_value').to.equal(Cypress.env('CUSTOM_ENV')) //CYPRESS_CUSTOM_ENV - "cypress" prefix - auto-loaded from global ENVs
+  it(`Validate CYPRESS_CUSTOM_ENV ENV (${Cypress.env('CYPRESS_CUSTOM_ENV')})`, () => {
+    expect('CYPRESS_CUSTOM_ENV_value').to.equal(Cypress.env('CYPRESS_CUSTOM_ENV')) //CYPRESS_CUSTOM_ENV - "cypress" prefix - auto-loaded from global ENVs
   })
   it(`Validate NON_CYPRESS_ENV ENV (${Cypress.env('NON_CYPRESS_ENV')})`, () => {
     expect('NON_CYPRESS_ENV_value').to.equal(Cypress.env('NON_CYPRESS_ENV')) //NON_CYPRESS_ENV - need to be loaded with --env parameter
-  })
-  it(`Validate cypress env  (${Cypress.env()})`, () => {
-    expect('').to.equal(Cypress.env())
   })
 })
