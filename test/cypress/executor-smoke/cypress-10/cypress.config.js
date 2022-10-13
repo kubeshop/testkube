08cdd19f86731f52f8196fe9b9056cb46f69f4bd
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2022-10-13 16:41:54 +0200

    Cypress - video recording disabled

--- test/cypress/executor-smoke/cypress-10/cypress.config.js
+++ test/cypress/executor-smoke/cypress-10/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://testkube.kubeshop.io/'
+    baseUrl: 'https://testkube.kubeshop.io/',
+    video: false
   },
 });
