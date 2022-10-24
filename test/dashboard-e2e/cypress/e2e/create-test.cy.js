f9b3f75395f6050d22c45a9cf3a7c90b25f1cbb1
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2022-10-24 17:01:04 +0200

    cleanup

--- test/dashboard-e2e/cypress/e2e/create-test.cy.js
+++ test/dashboard-e2e/cypress/e2e/create-test.cy.js
@@ -4,11 +4,8 @@ import TestDataHandler from '../support/data-handlers/test-data-handlers';
 const testDataHandler=new TestDataHandler();
 import ApiHelpers from '../support/api/api-helpers';
 const apiHelpers=new ApiHelpers();
-
 import CommonHelpers from '../support/common-helpers';
 const commonHelpers=new CommonHelpers();
-
-
 import MainPage from '../support/pages/MainPage';
 const mainPage=new MainPage();
 import CreateTestPage from '../support/pages/CreateTestPage';
@@ -23,15 +20,18 @@ describe('Create test with Dashboard', () => {
     const testName = "k6-git-file"
     const testData = testDataHandler.getTest(testName)
 
+    //prerequisites
     const assureTestNotCreated = apiHelpers.assureTestNotCreated(testData.name)
     cy.wrap(assureTestNotCreated)
     .then(() => {
+      //actions
       mainPage.visitMainPage()
       mainPage.openCreateTestDialog()
       createTestPage.createTest("k6-git-file")
-      cy.url().should('eq', 'http://localhost:8080/tests/executions/internal-dashboard-e2e-k6-git-file') //TODO baseurl
+      cy.url().should('eq', `http://localhost:8080/tests/executions/${testData.name}`) //TODO baseurl
     })
     .then(() => {
+      //validation
       const getTestData = apiHelpers.getTestData(testData.name)
       cy.wrap(getTestData).then((createdTestData) => {
         commonHelpers.validateTest(testData, createdTestData)
