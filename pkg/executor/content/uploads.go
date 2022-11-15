dc5fcf4e1b285883159f7d361e8960aefa53b9fc
Lilla Vass <lilla@kubeshop.io>
2022-11-15 14:19:15 +0100

    fix: update file placement

--- pkg/executor/content/uploads.go
+++ pkg/executor/content/uploads.go
@@ -19,7 +19,16 @@ func NewCopyFilesPlacer(endpoint, accessKeyID, secretAccessKey, location, token 
 }
 
 // PlaceFiles downloads the files from minio and places them into the /data/uploads directory
-func (p CopyFilesPlacer) PlaceFiles(buckets []string) error {
+func (p CopyFilesPlacer) PlaceFiles(testName, executionName string) error {
 	prefix := "/data/uploads/"
+
+	buckets := []string{}
+	if testName != "" {
+		buckets = append(buckets, p.client.GetValidBucketName("test", testName))
+	}
+	if executionName != "" {
+		buckets = append(buckets, p.client.GetValidBucketName("execution", executionName))
+	}
+
 	return p.client.PlaceFiles(buckets, prefix)
 }
