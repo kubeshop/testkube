8540d9ff52f268b85733acacf109be427c5cf4cb
nicufk <nicolae@kubeshop.io>
2022-11-18 01:52:20 +0200

    fix: use execution name as ID

--- internal/app/api/v1/executions.go
+++ internal/app/api/v1/executions.go
@@ -226,10 +226,7 @@ func (s TestkubeAPI) GetExecutionHandler() fiber.Handler {
 		if id == "" {
 			execution, err = s.ExecutionResults.Get(ctx, executionID)
 			if err == mongo.ErrNoDocuments {
-				execution, err = s.ExecutionResults.GetByName(ctx, executionID)
-				if err == mongo.ErrNoDocuments {
-					return s.Error(c, http.StatusNotFound, fmt.Errorf("test with execution id/name %s not found", executionID))
-				}
+				return s.Error(c, http.StatusNotFound, fmt.Errorf("test with execution id/name %s not found", executionID))
 			}
 			if err != nil {
 				return s.Error(c, http.StatusInternalServerError, err)
--- internal/app/api/v1/testsuites.go
+++ internal/app/api/v1/testsuites.go
@@ -573,10 +573,8 @@ func (s TestkubeAPI) GetTestSuiteExecutionHandler() fiber.Handler {
 		id := c.Params("executionID")
 		execution, err := s.TestExecutionResults.Get(ctx, id)
 		if err == mongo.ErrNoDocuments {
-			execution, err = s.TestExecutionResults.GetByName(ctx, id)
-			if err == mongo.ErrNoDocuments {
-				return s.Error(c, http.StatusNotFound, fmt.Errorf("test suite with execution id/name %s not found", id))
-			}
+			return s.Error(c, http.StatusNotFound, fmt.Errorf("test suite with execution id/name %s not found", id))
+
 		}
 		if err != nil {
 			return s.Error(c, http.StatusBadRequest, err)
@@ -611,10 +609,7 @@ func (s TestkubeAPI) AbortTestSuiteExecutionHandler() fiber.Handler {
 		id := c.Params("executionID")
 		execution, err := s.TestExecutionResults.Get(ctx, id)
 		if err == mongo.ErrNoDocuments {
-			execution, err = s.TestExecutionResults.GetByName(ctx, id)
-			if err == mongo.ErrNoDocuments {
-				return s.Error(c, http.StatusNotFound, fmt.Errorf("test suite with execution id/name %s not found", id))
-			}
+			return s.Error(c, http.StatusNotFound, fmt.Errorf("test suite with execution id/name %s not found", id))
 		}
 		if err != nil {
 			return s.Error(c, http.StatusBadRequest, err)
