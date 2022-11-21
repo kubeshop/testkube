5608df855cd4ca61d8ca71b5aa588b061be7c65e
Vladislav Sukhin <vladislav@kubeshop.io>
2022-11-21 15:57:05 +0300

    fix: go fmt

--- pkg/mapper/executions/summary.go
+++ pkg/mapper/executions/summary.go
@@ -2,7 +2,7 @@ package executions
 
 import "github.com/kubeshop/testkube/pkg/api/v1/testkube"
 
-// MapToSummary maps testkube.Execution to testkube.ExecutionSummary for lists without so many details
+// MapToSummary maps testkube.Execution to testkube.ExecutionSummary for lists without so many details.
 func MapToSummary(execution *testkube.Execution) *testkube.ExecutionSummary {
 	var status *testkube.ExecutionStatus
 	if execution.ExecutionResult != nil {
