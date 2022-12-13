051d2907188f6aa3b1c8f5cb8e2a9b72a8dd9031
nicufk <89570185+nicufk@users.noreply.github.com>
2022-12-13 19:19:54 +0200

    fix missing colon after name (#2900)

--- cmd/kubectl-testkube/commands/tests/renderer/execution_obj.go
+++ cmd/kubectl-testkube/commands/tests/renderer/execution_obj.go
@@ -16,7 +16,7 @@ func ExecutionRenderer(ui *ui.UI, obj interface{}) error {
 	}
 
 	ui.Warn("ID:        ", execution.Id)
-	ui.Warn("Name       ", execution.Name)
+	ui.Warn("Name:      ", execution.Name)
 	if execution.Number != 0 {
 		ui.Warn("Number:           ", fmt.Sprintf("%d", execution.Number))
 	}
