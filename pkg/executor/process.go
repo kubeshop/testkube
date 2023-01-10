163af8dcc954bcdf070902a5373bfbb48b9f3b03
Lilla Vass <lilla@kubeshop.io>
2023-01-10 15:42:33 +0100

    fix: don't print out secret vars in arguments

--- pkg/executor/process.go
+++ pkg/executor/process.go
@@ -17,7 +17,8 @@ import (
 // LoggedExecuteInDir will put wrapped JSON output to stdout AND get RAW output into out var
 // json logs can be processed later on watch of pod logs
 func Run(dir string, command string, envMngr secret.Manager, arguments ...string) (out []byte, err error) {
-	output.PrintLog(fmt.Sprintf("%s Executing in directory %s: \n $ %s %s", ui.IconMicroscope, dir, command, strings.Join(arguments, " ")))
+	obfuscatedArgs := envMngr.Obfuscate([]byte(strings.Join(arguments, " ")))
+	output.PrintLog(fmt.Sprintf("%s Executing in directory %s: \n $ %s %s", ui.IconMicroscope, dir, command, obfuscatedArgs))
 	out, err = process.LoggedExecuteInDir(dir, output.NewJSONWrapWriter(os.Stdout, envMngr), command, arguments...)
 	if err != nil {
 		output.PrintLog(fmt.Sprintf("%s Execution failed: %s", ui.IconCross, err.Error()))
