e8241051435c196c0aed5c5c7903c59d5bdcbbe5
Lilla Vass <lilla@kubeshop.io>
2023-01-11 14:40:12 +0100

    fix: use PrintLog instead of PrintEvent

--- pkg/executor/scraper/scraper.go
+++ pkg/executor/scraper/scraper.go
@@ -36,20 +36,20 @@ type MinioScraper struct {
 
 // Scrape gets artifacts from pod based on execution ID and directories list
 func (s MinioScraper) Scrape(id string, directories []string) error {
-	output.PrintEvent(fmt.Sprintf("%s Scraping artifacts %s", ui.IconCabinet, directories))
+	output.PrintLog(fmt.Sprintf("%s Scraping artifacts %s", ui.IconCabinet, directories))
 	client := minio.NewClient(s.Endpoint, s.AccessKeyID, s.SecretAccessKey, s.Location, s.Token, s.Ssl) // create storage client
 	err := client.Connect()
 	if err != nil {
-		output.PrintEvent(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
+		output.PrintLog(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
 		return fmt.Errorf("error occured creating minio client: %w", err)
 	}
 
 	err = client.ScrapeArtefacts(id, directories...)
 	if err != nil {
-		output.PrintEvent(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
+		output.PrintLog(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
 		return err
 	}
 
-	output.PrintEvent(fmt.Sprintf("%s Successfully scraped artifacts", ui.IconCheckMark))
+	output.PrintLog(fmt.Sprintf("%s Successfully scraped artifacts", ui.IconCheckMark))
 	return nil
 }
