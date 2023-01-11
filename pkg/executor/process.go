498c418777c426be624bf78f2207f7ff429239b1
Lilla Vass <vLia@users.noreply.github.com>
2023-01-11 17:34:50 +0100

    feat: add logs to common executor parts (#3012)

--- pkg/executor/process.go
+++ pkg/executor/process.go
@@ -1,11 +1,14 @@
 package executor
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/kubeshop/testkube/pkg/executor/output"
 	"github.com/kubeshop/testkube/pkg/executor/secret"
 	"github.com/kubeshop/testkube/pkg/process"
+	"github.com/kubeshop/testkube/pkg/ui"
 )
 
 // Run runs executor process wrapped in json line output
@@ -14,5 +17,13 @@ import (
 // LoggedExecuteInDir will put wrapped JSON output to stdout AND get RAW output into out var
 // json logs can be processed later on watch of pod logs
 func Run(dir string, command string, envMngr secret.Manager, arguments ...string) (out []byte, err error) {
-	return process.LoggedExecuteInDir(dir, output.NewJSONWrapWriter(os.Stdout, envMngr), command, arguments...)
+	obfuscatedArgs := envMngr.Obfuscate([]byte(strings.Join(arguments, " ")))
+	output.PrintLog(fmt.Sprintf("%s Executing in directory %s: \n $ %s %s", ui.IconMicroscope, dir, command, obfuscatedArgs))
+	out, err = process.LoggedExecuteInDir(dir, output.NewJSONWrapWriter(os.Stdout, envMngr), command, arguments...)
+	if err != nil {
+		output.PrintLog(fmt.Sprintf("%s Execution failed: %s", ui.IconCross, err.Error()))
+		return out, err
+	}
+	output.PrintLog(fmt.Sprintf("%s Execution succeeded", ui.IconCheckMark))
+	return out, nil
 }
