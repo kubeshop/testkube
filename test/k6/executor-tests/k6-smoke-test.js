7b9969f11143bbfcb5245538b34e6338fdf4f5df
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2023-01-23 12:32:40 +0100

    K6 smoke test - fail on ENVs - fixed

--- test/k6/executor-tests/k6-smoke-test.js
+++ test/k6/executor-tests/k6-smoke-test.js
@@ -1,23 +1,14 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+if (__ENV.K6_ENV_FROM_PARAM != "K6_ENV_FROM_PARAM_value") {
+  throw new Error("Incorrect K6_ENV_FROM_PARAM ENV value");
+}
+
+if (__ENV.K6_SYSTEM_ENV != "K6_SYSTEM_ENV_value") {
+  throw new Error("Incorrect K6_SYSTEM_ENV ENV value");
+}
+
 export default function () {
   http.get('https://testkube.kubeshop.io/');
-
-
-  if (
-    !check(__ENV.K6_ENV_FROM_PARAM, {
-      'Correct ENV value is set with -e param (K6_ENV_FROM_PARAM)': (value) => value == "K6_ENV_FROM_PARAM_value",
-    })
-  ) {
-    fail('Incorrect ENV value for K6_ENV_FROM_PARAM');
-  }
-
-  if (
-    !check(__ENV.K6_SYSTEM_ENV, {
-      'Correct ENV value is set from system ENV (K6_SYSTEM_ENV)': (value) => value == "K6_SYSTEM_ENV_value",
-    })
-  ) {
-    fail('Incorrect ENV value for K6_SYSTEM_ENV');
-  }
 }
