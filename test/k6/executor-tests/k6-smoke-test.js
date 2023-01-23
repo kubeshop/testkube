537e94450fb5938458fa216a642901f386ccf1b7
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2023-01-23 12:12:30 +0100

    K6 smoke test - fail on checks

--- test/k6/executor-tests/k6-smoke-test.js
+++ test/k6/executor-tests/k6-smoke-test.js
@@ -4,11 +4,20 @@ import { check } from 'k6';
 export default function () {
   http.get('https://testkube.kubeshop.io/');
 
-  check(__ENV.K6_ENV_FROM_PARAM, {
-    'Correct ENV value is set with -e param (K6_ENV_FROM_PARAM)': (value) => value == "K6_ENV_FROM_PARAM_value",
-  });
-  check(__ENV.K6_SYSTEM_ENV, {
-    'Correct ENV value is set from system ENV (K6_SYSTEM_ENV)': (value) => value == "K6_SYSTEM_ENV_value",
-  });
-  // Check results are visible in K6 summary, but non-zero exit codes aren't returned on failed checks
+
+  if (
+    !check(__ENV.K6_ENV_FROM_PARAM, {
+      'Correct ENV value is set with -e param (K6_ENV_FROM_PARAM)': (value) => value == "K6_ENV_FROM_PARAM_value",
+    })
+  ) {
+    fail('Incorrect ENV value for K6_ENV_FROM_PARAM');
+  }
+
+  if (
+    !check(__ENV.K6_SYSTEM_ENV, {
+      'Correct ENV value is set from system ENV (K6_SYSTEM_ENV)': (value) => value == "K6_SYSTEM_ENV_value",
+    })
+  ) {
+    fail('Incorrect ENV value for K6_SYSTEM_ENV');
+  }
 }
