f84a39d99fc389daa81a21a54cf39e21b6cc072a
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2023-02-20 20:13:41 +0100

    ENV/param checks reanabled for Cypress tests

--- test/cypress/executor-tests/cypress-11/cypress/e2e/smoke.cy.js
+++ test/cypress/executor-tests/cypress-11/cypress/e2e/smoke.cy.js
@@ -1,11 +1,11 @@
-describe('Testkube website', () => { //TODO: disabled for now - reenable after: https://github.com/kubeshop/testkube/issues/2540
+describe('Testkube website', () => {
   it('Open Testkube website', () => {
     cy.visit('/')
   })
-  it.skip(`Validate CYPRESS_CUSTOM_ENV ENV (${Cypress.env('CUSTOM_ENV')})`, () => {
+  it(`Validate CYPRESS_CUSTOM_ENV ENV (${Cypress.env('CUSTOM_ENV')})`, () => {
     expect('CYPRESS_CUSTOM_ENV_value').to.equal(Cypress.env('CUSTOM_ENV')) //CYPRESS_CUSTOM_ENV - "cypress" prefix - auto-loaded from global ENVs
   })
-  it.skip(`Validate NON_CYPRESS_ENV ENV (${Cypress.env('NON_CYPRESS_ENV')})`, () => {
+  it(`Validate NON_CYPRESS_ENV ENV (${Cypress.env('NON_CYPRESS_ENV')})`, () => {
     expect('NON_CYPRESS_ENV_value').to.equal(Cypress.env('NON_CYPRESS_ENV')) //NON_CYPRESS_ENV - need to be loaded with --env parameter
   })
 })
