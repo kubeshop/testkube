5050dfeeee21a4491a91453692dbeab413f181bf
Vladislav Sukhin <vladislav@kubeshop.io>
2023-03-01 20:16:30 +0300

    feat: artillery executor

---
+++ contrib/executor/artillery/build/agent/docker-bake.hcl
@@ -0,0 +1,12 @@
+// docker-bake.hcl
+target "docker-metadata-action" {}
+
+target "build" {
+  inherits = ["docker-metadata-action"]
+  context = "./"
+  dockerfile = "build/agent/Dockerfile"
+  platforms = [
+    "linux/amd64",
+    "linux/arm64",
+  ]
+}
---
+++ contrib/executor/artillery/pkg/runner/artillery_test.go
@@ -0,0 +1,44 @@
+//go:build integration
+
+package runner
+
+import (
+	"testing"
+
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/executor"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+
+	t.Run("runner should run test based on execution data", func(t *testing.T) {
+		// given
+		// install artillery before running test
+		_, err := executor.Run("", "npm", "install", "-g", "artillery@2.0.0-27")
+		if err != nil {
+			t.Errorf("npm install artillery error: %v", err)
+		}
+		runner := NewArtilleryRunner()
+		repoURI := "https://github.com/kubeshop/testkube-executor-artillery.git"
+		result, err := runner.Run(testkube.Execution{
+			Content: &testkube.TestContent{
+				Type_: string(testkube.TestContentTypeGitFile),
+				Repository: &testkube.Repository{
+					Type_:  "git",
+					Uri:    repoURI,
+					Branch: "main",
+					Path:   "examples/test.yaml",
+				},
+			},
+		})
+		if err != nil {
+			t.Errorf("Artillery Test Failed: ResultErr: %v, Err: %v ", result.ErrorMessage, err)
+		}
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, *result.Status, testkube.PASSED_ExecutionStatus)
+
+	})
+
+}
