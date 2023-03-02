8284bfee1e1da998d7f6d7b33abaad22fdec8d20
Vladislav Sukhin <vladislav@kubeshop.io>
2023-03-02 17:05:00 +0300

    feat: soapui and template executor

---
+++ contrib/executor/soapui/pkg/runner/runner.go
@@ -0,0 +1,130 @@
+package runner
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/envs"
+	"github.com/kubeshop/testkube/pkg/executor"
+	"github.com/kubeshop/testkube/pkg/executor/content"
+	"github.com/kubeshop/testkube/pkg/executor/env"
+	"github.com/kubeshop/testkube/pkg/executor/output"
+	"github.com/kubeshop/testkube/pkg/executor/runner"
+	"github.com/kubeshop/testkube/pkg/executor/scraper"
+	"github.com/kubeshop/testkube/pkg/ui"
+)
+
+const FailureMessage string = "finished with status [FAILED]"
+
+// NewRunner creates a new SoapUIRunner
+func NewRunner() (*SoapUIRunner, error) {
+	output.PrintLog(fmt.Sprintf("%s Preparing test runner", ui.IconTruck))
+	params, err := envs.LoadTestkubeVariables()
+	if err != nil {
+		return nil, fmt.Errorf("could not initialize Artillery runner variables: %w", err)
+	}
+
+	return &SoapUIRunner{
+		SoapUIExecPath: "/usr/local/SmartBear/EntryPoint.sh",
+		SoapUILogsPath: "/home/soapui/.soapuios/logs",
+		Fetcher:        content.NewFetcher(""),
+		Scraper: scraper.NewMinioScraper(
+			params.Endpoint,
+			params.AccessKeyID,
+			params.SecretAccessKey,
+			params.Location,
+			params.Token,
+			params.Bucket,
+			params.Ssl,
+		),
+		DataDir: params.DataDir,
+	}, nil
+}
+
+// SoapUIRunner runs SoapUI tests
+type SoapUIRunner struct {
+	SoapUIExecPath string
+	SoapUILogsPath string
+	Fetcher        content.ContentFetcher
+	Scraper        scraper.Scraper
+	DataDir        string
+}
+
+// Run executes the test and returns the test results
+func (r *SoapUIRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
+	output.PrintLog(fmt.Sprintf("%s Preparing for test run", ui.IconTruck))
+
+	testFile, err := r.Fetcher.Fetch(execution.Content)
+	if err != nil {
+		return result, err
+	}
+
+	setUpEnvironment(execution.Args, testFile)
+
+	fileInfo, err := os.Stat(testFile)
+	if err != nil {
+		return result, err
+	}
+
+	if fileInfo.IsDir() {
+		return testkube.ExecutionResult{}, errors.New("SoapUI executor only tests one project per execution, a directory of projects was given")
+	}
+
+	output.PrintLog(fmt.Sprintf("%s Running SoapUI tests", ui.IconMicroscope))
+	result = r.runSoapUI(&execution)
+
+	if err = r.Scraper.Scrape(execution.Id, []string{r.SoapUILogsPath}); err != nil {
+		return result, fmt.Errorf("failed getting artifacts: %w", err)
+	}
+
+	return result, nil
+}
+
+// setUpEnvironment sets up the COMMAND_LINE environment variable to
+// contain the incoming arguments and to point to the test file path
+func setUpEnvironment(args []string, testFilePath string) {
+	args = append(args, testFilePath)
+	os.Setenv("COMMAND_LINE", strings.Join(args, " "))
+}
+
+// runSoapUI runs the SoapUI executable and returns the output
+func (r *SoapUIRunner) runSoapUI(execution *testkube.Execution) testkube.ExecutionResult {
+
+	envManager := env.NewManagerWithVars(execution.Variables)
+	envManager.GetReferenceVars(envManager.Variables)
+
+	runPath := ""
+	if execution.Content.Repository != nil && execution.Content.Repository.WorkingDir != "" {
+		runPath = filepath.Join(r.DataDir, "repo", execution.Content.Repository.WorkingDir)
+	}
+
+	output, err := executor.Run(runPath, "/bin/sh", envManager, r.SoapUIExecPath)
+	output = envManager.ObfuscateSecrets(output)
+	if err != nil {
+		return testkube.ExecutionResult{
+			Status:       testkube.ExecutionStatusFailed,
+			ErrorMessage: err.Error(),
+		}
+	}
+	if strings.Contains(string(output), FailureMessage) {
+		return testkube.ExecutionResult{
+			Status:       testkube.ExecutionStatusFailed,
+			ErrorMessage: FailureMessage,
+			Output:       string(output),
+		}
+	}
+
+	return testkube.ExecutionResult{
+		Status: testkube.ExecutionStatusPassed,
+		Output: string(output),
+	}
+}
+
+// GetType returns runner type
+func (r *SoapUIRunner) GetType() runner.Type {
+	return runner.TypeMain
+}
---
+++ contrib/executor/soapui/pkg/runner/runner_test.go
@@ -0,0 +1,122 @@
+package runner
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/kubeshop/testkube/contrib/executor/soapui/pkg/mock"
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+	testXML := "./example/REST-Project-1-soapui-project.xml"
+	f := mock.Fetcher{}
+	f.FetchFn = func(content *testkube.TestContent) (path string, err error) {
+		return testXML, nil
+	}
+
+	e := testkube.Execution{
+		Id:            "get_petstore",
+		TestName:      "Get Petstore",
+		TestNamespace: "petstore",
+		TestType:      "soapui/xml",
+		Name:          "Testing GET",
+		Args:          []string{"-c 'TestCase 1'"},
+		Content:       &testkube.TestContent{},
+	}
+
+	tests := []struct {
+		name            string
+		scraper         func(id string, directories []string) error
+		testFileCreator func() (*os.File, error)
+		execution       testkube.Execution
+		expectedError   string
+		expectedStatus  testkube.ExecutionStatus
+	}{
+		{
+			name:            "Successful test, successful scraper",
+			scraper:         func(id string, directories []string) error { return nil },
+			testFileCreator: createSuccessfulScript,
+			execution:       e,
+			expectedError:   "",
+			expectedStatus:  *testkube.ExecutionStatusPassed,
+		},
+		{
+			name:            "Failing test, successful scraper",
+			scraper:         func(id string, directories []string) error { return nil },
+			testFileCreator: createFailingScript,
+			execution:       e,
+			expectedError:   "",
+			expectedStatus:  *testkube.ExecutionStatusFailed,
+		},
+		{
+			name:            "Successful test, failing scraper",
+			scraper:         func(id string, directories []string) error { return errors.New("Scraping failed") },
+			testFileCreator: createSuccessfulScript,
+			execution:       e,
+			expectedError:   "failed getting artifacts: Scraping failed",
+			expectedStatus:  *testkube.ExecutionStatusPassed,
+		},
+		{
+			name:            "Failing test, failing scraper",
+			scraper:         func(id string, directories []string) error { return errors.New("Scraping failed") },
+			testFileCreator: createFailingScript,
+			execution:       e,
+			expectedError:   "failed getting artifacts: Scraping failed",
+			expectedStatus:  *testkube.ExecutionStatusFailed,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := mock.Scraper{}
+			s.ScrapeFn = test.scraper
+
+			file, err := test.testFileCreator()
+			assert.NoError(t, err)
+			defer file.Close()
+
+			runner := SoapUIRunner{
+				Fetcher:        f,
+				SoapUIExecPath: file.Name(),
+				Scraper:        s,
+			}
+
+			res, err := runner.Run(test.execution)
+			assert.EqualError(t, err, test.expectedError)
+			assert.Equal(t, test.expectedStatus, *res.Status)
+		})
+	}
+}
+
+func createSuccessfulScript() (*os.File, error) {
+	file, err := os.CreateTemp("", "successful_script")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = file.WriteString("#!/bin/sh\nexit 0\n")
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func createFailingScript() (*os.File, error) {
+	file, err := os.CreateTemp("", "failing_script")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = file.WriteString("#!/bin/sh\nexit 1\n")
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
