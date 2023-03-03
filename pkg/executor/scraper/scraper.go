0540cb980a63c23f474f3a89a60ea72f95ae14d4
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2023-03-03 14:20:06 +0100

    Revert "feat: add support for cloud artifacts; massive scraper refactor (#3309)" (#3324)
    This reverts commit a9feb2618f7e24ebcb81fa5d8a6e6f29d124dc67.

--- pkg/executor/scraper/scraper.go
+++ pkg/executor/scraper/scraper.go
@@ -1,7 +1,11 @@
 package scraper
 
 import (
-	"context"
+	"fmt"
+
+	"github.com/kubeshop/testkube/pkg/executor/output"
+	"github.com/kubeshop/testkube/pkg/storage/minio"
+	"github.com/kubeshop/testkube/pkg/ui"
 )
 
 // Scraper is responsible for collecting and persisting the necessary artifacts
@@ -10,22 +14,43 @@ type Scraper interface {
 	Scrape(executionID string, directories []string) error
 }
 
-type ScraperV2 struct {
-	extractor Extractor
-	loader    Uploader
-}
+// NewMinioScraper returns a Minio implementation of the Scraper
+func NewMinioScraper(endpoint, accessKeyID, secretAccessKey, location, token, bucket string, ssl bool) *MinioScraper {
 
-func NewScraperV2(extractor Extractor, loader Uploader) *ScraperV2 {
-	return &ScraperV2{
-		extractor: extractor,
-		loader:    loader,
+	return &MinioScraper{
+		Endpoint:        endpoint,
+		AccessKeyID:     accessKeyID,
+		SecretAccessKey: secretAccessKey,
+		Location:        location,
+		Token:           token,
+		Bucket:          bucket,
+		Ssl:             ssl,
 	}
+
 }
 
-func (s *ScraperV2) Scrape(ctx context.Context, meta map[string]any) error {
-	return s.
-		extractor.
-		Extract(ctx, func(ctx context.Context, object *Object) error {
-			return s.loader.Upload(ctx, object, meta)
-		})
+// MinioScraper manages getting artifacts from job pods
+type MinioScraper struct {
+	Endpoint, AccessKeyID, SecretAccessKey, Location, Token, Bucket string
+	Ssl                                                             bool
+}
+
+// Scrape gets artifacts from pod based on execution ID and directories list
+func (s MinioScraper) Scrape(id string, directories []string) error {
+	output.PrintLog(fmt.Sprintf("%s Scraping artifacts %s", ui.IconCabinet, directories))
+	client := minio.NewClient(s.Endpoint, s.AccessKeyID, s.SecretAccessKey, s.Location, s.Token, s.Bucket, s.Ssl) // create storage client
+	err := client.Connect()
+	if err != nil {
+		output.PrintLog(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
+		return fmt.Errorf("error occured creating minio client: %w", err)
+	}
+
+	err = client.ScrapeArtefacts(id, directories...)
+	if err != nil {
+		output.PrintLog(fmt.Sprintf("%s Failed to scrape artifacts: %s", ui.IconCross, err.Error()))
+		return err
+	}
+
+	output.PrintLog(fmt.Sprintf("%s Successfully scraped artifacts", ui.IconCheckMark))
+	return nil
 }
