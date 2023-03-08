d56b40fc05f21e8b7350c0f7fa6b53d434659965
nicufk <89570185+nicufk@users.noreply.github.com>
2023-03-08 16:58:55 +0200

    feat: add cluster type to the telemetry events (#3365)
    * feat: add cluster type to the telemetry events
    
    * fix: move get cluster type to the functions
    
    * fix: add unidentified type

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -1,15 +1,18 @@
 package telemetry
 
 import (
+	"context"
 	"net/http"
 	"sync"
 
 	"strings"
 
 	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/kubeshop/testkube/cmd/kubectl-testkube/config"
 	httpclient "github.com/kubeshop/testkube/pkg/http"
+	"github.com/kubeshop/testkube/pkg/k8sclient"
 	"github.com/kubeshop/testkube/pkg/log"
 )
 
@@ -26,7 +29,7 @@ type Sender func(client *http.Client, payload Payload) (out string, err error)
 
 // SendServerStartEvent will send event to GA
 func SendServerStartEvent(clusterId, version string) (string, error) {
-	payload := NewAPIPayload(clusterId, "testkube_api_start", version, "localhost")
+	payload := NewAPIPayload(clusterId, "testkube_api_start", version, "localhost", GetClusterType())
 	return sendData(senders, payload)
 }
 
@@ -38,31 +41,31 @@ func SendCmdEvent(cmd *cobra.Command, version string) (string, error) {
 		command = "root"
 	}
 
-	payload := NewCLIPayload(getCurrentContext(), GetMachineID(), command, version, "cli_command_execution")
+	payload := NewCLIPayload(getCurrentContext(), GetMachineID(), command, version, "cli_command_execution", GetClusterType())
 	return sendData(senders, payload)
 }
 
 // SendCmdInitEvent will send CLI event to GA
 func SendCmdInitEvent(cmd *cobra.Command, version string) (string, error) {
-	payload := NewCLIPayload(getCurrentContext(), GetMachineID(), "init", version, "cli_command_execution")
+	payload := NewCLIPayload(getCurrentContext(), GetMachineID(), "init", version, "cli_command_execution", GetClusterType())
 	return sendData(senders, payload)
 }
 
 // SendHeartbeatEvent will send CLI event to GA
 func SendHeartbeatEvent(host, version, clusterId string) (string, error) {
-	payload := NewAPIPayload(clusterId, "testkube_api_heartbeat", version, host)
+	payload := NewAPIPayload(clusterId, "testkube_api_heartbeat", version, host, GetClusterType())
 	return sendData(senders, payload)
 }
 
 // SendCreateEvent will send API create event for Test or Test suite to GA
 func SendCreateEvent(event string, params CreateParams) (string, error) {
-	payload := NewCreatePayload(event, params)
+	payload := NewCreatePayload(event, GetClusterType(), params)
 	return sendData(senders, payload)
 }
 
 // SendCreateEvent will send API run event for Test or Test suite to GA
 func SendRunEvent(event string, params RunParams) (string, error) {
-	payload := NewRunPayload(event, params)
+	payload := NewRunPayload(event, GetClusterType(), params)
 	return sendData(senders, payload)
 }
 
@@ -99,4 +102,49 @@ func getCurrentContext() RunContext {
 		OrganizationId: data.CloudContext.Organization,
 		EnvironmentId:  data.CloudContext.Environment,
 	}
+}
+
+func GetClusterType() string {
+
+	wasThereError := false
+	clientset, err := k8sclient.ConnectToK8s()
+	if err != nil {
+		log.DefaultLogger.Debugw("Creating k8s clientset", err)
+		wasThereError = true
+	}
+
+	pods, err := clientset.CoreV1().Pods("kube-system").List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		log.DefaultLogger.Debugw("Getting pods from kube-system namespace", err)
+		wasThereError = true
+	}
+
+	// Loop through the pods and check if their name contains the search string.
+	for _, pod := range pods.Items {
+		if strings.Contains(pod.Name, "-kind-") || strings.Contains(pod.Name, "kindnet") {
+			return "kind"
+		}
+		if strings.Contains(pod.Name, "-minikube") {
+			return "minikube"
+		}
+		if strings.Contains(pod.Name, "docker-desktop") {
+			return "docker-desktop"
+		}
+		if strings.Contains(pod.Name, "gke-") || strings.Contains(pod.Name, "-gke-") {
+			return "gke"
+		}
+		if strings.Contains(pod.Name, "aws-") || strings.Contains(pod.Name, "-aws-") {
+			return "eks"
+		}
+		if strings.Contains(pod.Name, "azure-") || strings.Contains(pod.Name, "-azuredisk-") || strings.Contains(pod.Name, "-azurefile-") {
+			return "aks"
+		}
+
+	}
+
+	if wasThereError {
+		return "unidentified"
+	}
+
+	return "others"
 }
