f6379d20d36c8b5d3b00161d6b5675cd68e89307
Vladislav Sukhin <vladislav@kubeshop.io>
2023-03-28 13:46:29 +0300

    fix: clean triggers only in installation namespace

--- internal/app/api/v1/testtriggers.go
+++ internal/app/api/v1/testtriggers.go
@@ -120,27 +120,20 @@ func (s *TestkubeAPI) BulkUpdateTestTriggersHandler() fiber.Handler {
 			return s.Error(c, http.StatusBadRequest, err)
 		}
 
-		namespaces, err := s.Clientset.CoreV1().Namespaces().List(c.UserContext(), v1.ListOptions{})
-		if err != nil {
-			return errors.Wrap(err, "error fetching list of all namespaces")
-		}
-		for _, ns := range namespaces.Items {
-			err = s.TestKubeClientset.
-				TestsV1().
-				TestTriggers(ns.Name).
-				DeleteCollection(c.UserContext(), v1.DeleteOptions{}, v1.ListOptions{})
-
-			if err != nil && !k8serrors.IsNotFound(err) {
-				return s.Error(c, http.StatusBadGateway, errors.Wrap(err, "error cleaning triggers before reapply"))
-			}
+		err = s.TestKubeClientset.
+			TestsV1().
+			TestTriggers(s.Namespace).
+			DeleteCollection(c.UserContext(), v1.DeleteOptions{}, v1.ListOptions{})
+		if err != nil && !k8serrors.IsNotFound(err) {
+			return s.Error(c, http.StatusBadGateway, errors.Wrap(err, "error cleaning triggers before reapply"))
 		}
 
 		s.Metrics.IncBulkDeleteTestTrigger(nil)
 
 		testTriggers := make([]testkube.TestTrigger, 0, len(request))
 
-		namespace := s.Namespace
 		for _, upsertRequest := range request {
+			namespace := s.Namespace
 			if upsertRequest.Namespace != "" {
 				namespace = upsertRequest.Namespace
 			}
