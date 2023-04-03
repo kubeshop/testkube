68d9e8e95226f0c8c31853b6923da98a045926ab
Lilla Vass <lilla@kubeshop.io>
2023-04-03 18:24:16 +0200

    feat: update config endpoint errors

--- internal/app/api/v1/configs.go
+++ internal/app/api/v1/configs.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 )
@@ -15,7 +16,10 @@ func (s TestkubeAPI) GetConfigsHandler() fiber.Handler {
 		ctx := c.Context()
 		config, err := s.ConfigMap.Get(ctx)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, fmt.Errorf("unable to get config: %w", err))
+			if err == mongo.ErrNoDocuments {
+				return s.Error(c, http.StatusNotFound, fmt.Errorf("failed to get config: db found no config: %w", err))
+			}
+			return s.Error(c, http.StatusInternalServerError, fmt.Errorf("failed to get config: %w", err))
 		}
 		return c.JSON(config)
 	}
@@ -25,23 +29,27 @@ func (s TestkubeAPI) GetConfigsHandler() fiber.Handler {
 func (s TestkubeAPI) UpdateConfigsHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		ctx := c.Context()
+		errPrefix := "failed to update config"
 
 		config, err := s.ConfigMap.Get(ctx)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, fmt.Errorf("unable to get config: %w", err))
+			if err == mongo.ErrNoDocuments {
+				return s.Error(c, http.StatusNotFound, fmt.Errorf("%s: db found no config: %w", errPrefix, err))
+			}
+			return s.Error(c, http.StatusInternalServerError, fmt.Errorf("%s: unable to get config: %w", errPrefix, err))
 		}
 
 		var request testkube.Config
 		err = c.BodyParser(&request)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, fmt.Errorf("config request body invalid: %w", err))
+			return s.Error(c, http.StatusBadRequest, fmt.Errorf("%s: config request body invalid: %w", errPrefix, err))
 		}
 		s.Log.Warnw("#######", "request", config)
 		config.EnableTelemetry = request.EnableTelemetry
 		s.Log.Warnw("#######", "request", config)
 		_, err = s.ConfigMap.Upsert(ctx, config)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, fmt.Errorf("unable to update config: %w", err))
+			return s.Error(c, http.StatusInternalServerError, fmt.Errorf("%s: db could not update config: %w", errPrefix, err))
 		}
 		return c.JSON(config)
 	}
