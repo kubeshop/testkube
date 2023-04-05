dbf8999bce16e16b10c137dbbdc5eb50dbc603cf
Lilla Vass <vLia@users.noreply.github.com>
2023-04-05 15:24:30 +0200

    feat: improve errors (#3614)

--- internal/app/api/v1/webhook.go
+++ internal/app/api/v1/webhook.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/kubeshop/testkube/pkg/crd"
@@ -12,10 +14,12 @@ import (
 
 func (s TestkubeAPI) CreateWebhookHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		errPrefix := "failed to create webhook"
+
 		var request testkube.WebhookCreateRequest
 		err := c.BodyParser(&request)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			return s.Error(c, http.StatusBadRequest, fmt.Errorf("%s: could not parse request: %w", errPrefix, err))
 		}
 
 		if c.Accepts(mediaTypeJSON, mediaTypeYAML) == mediaTypeYAML {
@@ -28,7 +32,7 @@ func (s TestkubeAPI) CreateWebhookHandler() fiber.Handler {
 
 		created, err := s.WebhooksClient.Create(&webhook)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			return s.Error(c, http.StatusBadGateway, fmt.Errorf("%s: client could not create webhook: %w", errPrefix, err))
 		}
 
 		c.Status(http.StatusCreated)
@@ -38,9 +42,11 @@ func (s TestkubeAPI) CreateWebhookHandler() fiber.Handler {
 
 func (s TestkubeAPI) ListWebhooksHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		errPrefix := "failed to list webhooks"
+
 		list, err := s.WebhooksClient.List(c.Query("selector"))
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			return s.Error(c, http.StatusBadGateway, fmt.Errorf("%s: client could not list webhooks: %w", errPrefix, err))
 		}
 
 		results := testkube.Webhooks{}
@@ -61,10 +67,14 @@ func (s TestkubeAPI) ListWebhooksHandler() fiber.Handler {
 func (s TestkubeAPI) GetWebhookHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		name := c.Params("name")
+		errPrefix := fmt.Sprintf("failed to get webhook %s", name)
 
 		item, err := s.WebhooksClient.Get(name)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			if errors.IsNotFound(err) {
+				return s.Error(c, http.StatusNotFound, fmt.Errorf("%s: webhook not found: %w", errPrefix, err))
+			}
+			return s.Error(c, http.StatusBadGateway, fmt.Errorf("%s: client could not get webhook: %w", errPrefix, err))
 		}
 
 		result := webhooksmapper.MapCRDToAPI(*item)
@@ -80,10 +90,14 @@ func (s TestkubeAPI) GetWebhookHandler() fiber.Handler {
 func (s TestkubeAPI) DeleteWebhookHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		name := c.Params("name")
+		errPrefix := fmt.Sprintf("failed to delete webhook %s", name)
 
 		err := s.WebhooksClient.Delete(name)
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			if errors.IsNotFound(err) {
+				return s.Error(c, http.StatusNotFound, fmt.Errorf("%s: webhook not found: %w", errPrefix, err))
+			}
+			return s.Error(c, http.StatusBadGateway, fmt.Errorf("%s: client could not delete webhook: %w", errPrefix, err))
 		}
 
 		c.Status(http.StatusNoContent)
@@ -93,9 +107,11 @@ func (s TestkubeAPI) DeleteWebhookHandler() fiber.Handler {
 
 func (s TestkubeAPI) DeleteWebhooksHandler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		errPrefix := "failed to delete webhooks"
+
 		err := s.WebhooksClient.DeleteByLabels(c.Query("selector"))
 		if err != nil {
-			return s.Error(c, http.StatusBadRequest, err)
+			return s.Error(c, http.StatusBadGateway, fmt.Errorf("%s: client could not delete webhooks: %w", errPrefix, err))
 		}
 
 		c.Status(http.StatusNoContent)
