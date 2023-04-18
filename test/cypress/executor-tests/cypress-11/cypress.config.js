2ec4fb246088e0f0263200d39cee0b8d3b9bd1d2
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-04-18 13:02:19 +0200

    cypress - video: false removed from example projects

--- test/cypress/executor-tests/cypress-11/cypress.config.js
+++ test/cypress/executor-tests/cypress-11/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://testkube.kubeshop.io/',
-    video: false
+    baseUrl: 'https://testkube.kubeshop.io/'
   },
 });
