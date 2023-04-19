bca2ca567e50efcf796314fc02157ef55ce61081
nicufk <nicolae@kubeshop.io>
2023-04-19 15:58:57 +0300

    fix: delete execution numbers on test deletion

--- pkg/repository/result/mongo_numbers.go
+++ pkg/repository/result/mongo_numbers.go
@@ -2,6 +2,7 @@ package result
 
 import (
 	"context"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -9,14 +10,27 @@ import (
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 )
 
-type executionNumber struct {
+type oldExecutionNumber struct {
 	TestName string `json:"testName"`
 	Number   int    `json:"number"`
 }
 
-func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, testName string) (number int32, err error) {
+type executionNumber struct {
+	Name        string `json:"name"`
+	Number      int    `json:"number"`
+	IsTestSuite bool   `json:"isTestSuite"`
+}
 
-	execNmbr := executionNumber{TestName: testName}
+func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, name string) (number int32, err error) {
+	err = r.convertFromOldToNew()
+	if err != nil {
+		return 1, err
+	}
+
+	// TODO: modify this when we decide to update the interfaces for OSS and cloud
+	isTestSuite := strings.HasPrefix(name, "ts-")
+
+	execNmbr := executionNumber{Name: name, IsTestSuite: isTestSuite}
 	retry := false
 	retryAttempts := 0
 	maxRetries := 10
@@ -25,10 +39,10 @@ func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, testName s
 	opts.SetUpsert(true)
 	opts.SetReturnDocument(options.After)
 
-	err = r.SequencesColl.FindOne(ctx, bson.M{"testname": testName}).Decode(&execNmbr)
+	err = r.SequencesColl.FindOne(ctx, bson.M{"name": name}).Decode(&execNmbr)
 	if err != nil {
 		var execution testkube.Execution
-		execution, err = r.GetLatestByTest(ctx, testName, "number")
+		execution, err = r.GetLatestByTest(ctx, name, "number")
 		if err != nil {
 			execNmbr.Number = 1
 		} else {
@@ -36,18 +50,66 @@ func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, testName s
 		}
 		_, err = r.SequencesColl.InsertOne(ctx, execNmbr)
 	} else {
-		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"testname": testName}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
+		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"name": name}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
 	}
 
 	retry = err != nil
 
 	for retry {
 		retryAttempts++
-		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"testname": testName}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
+		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"name": name}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
 		if err == nil || retryAttempts >= maxRetries {
 			retry = false
 		}
 	}
 
 	return int32(execNmbr.Number), nil
+}
+
+func (r *MongoRepository) DeleteExecutionNumber(ctx context.Context, name string) (err error) {
+	_, err = r.SequencesColl.DeleteOne(ctx, bson.M{"name": name})
+	return err
+}
+
+func (r *MongoRepository) DeleteExecutionNumbers(ctx context.Context, names []string) (err error) {
+	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"name": bson.M{"$in": names}})
+	return err
+}
+
+func (r *MongoRepository) DeleteAllExecutionNumbers(ctx context.Context, isTestSuite bool) (err error) {
+	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"isTestSuite": isTestSuite})
+	return err
+}
+
+func (r *MongoRepository) convertFromOldToNew() error {
+	filter := bson.M{"testname": bson.M{"$exists": true}}
+
+	cursor, err := r.SequencesColl.Find(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var entry oldExecutionNumber
+		err := cursor.Decode(&entry)
+		if err != nil {
+			return err
+		}
+
+		isTestSuite := strings.HasPrefix(entry.TestName, "ts-")
+
+		newEntry := executionNumber{
+			Name:        entry.TestName,
+			Number:      entry.Number,
+			IsTestSuite: isTestSuite,
+		}
+
+		_, err = r.SequencesColl.InsertOne(context.Background(), newEntry)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = r.SequencesColl.DeleteMany(context.Background(), filter)
+	return err
 }
