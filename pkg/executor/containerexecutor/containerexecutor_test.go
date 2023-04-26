dbebfa1d69bbf7bcad334157d605b134926deabb
ypoplavs <yulia.poplavska@kubeshop.io>
2023-04-26 12:13:41 +0300

    rename init, scraper executors

--- pkg/executor/containerexecutor/containerexecutor_test.go
+++ pkg/executor/containerexecutor/containerexecutor_test.go
@@ -78,7 +78,7 @@ func TestNewExecutorJobSpecEmptyArgs(t *testing.T) {
 	jobOptions := &JobOptions{
 		Name:        "name",
 		Namespace:   "namespace",
-		InitImage:   "kubeshop/testkube-executor-init:0.7.10",
+		InitImage:   "kubeshop/testkube-init-executor:0.7.10",
 		Image:       "ubuntu",
 		JobTemplate: defaultJobTemplate,
 		Args:        []string{},
@@ -94,7 +94,7 @@ func TestNewExecutorJobSpecWithArgs(t *testing.T) {
 	jobOptions := &JobOptions{
 		Name:                  "name",
 		Namespace:             "namespace",
-		InitImage:             "kubeshop/testkube-executor-init:0.7.10",
+		InitImage:             "kubeshop/testkube-init-executor:0.7.10",
 		Image:                 "curl",
 		JobTemplate:           defaultJobTemplate,
 		ImagePullSecrets:      []string{"secret-name"},
