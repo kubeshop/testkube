bacc1d0194ffe5406086fce5b6b1096510fac0da
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-04-28 02:21:37 +0200

    Dashboard E2E tests - scenarios added (.skip)

---
+++ test/dashboard-e2e/tests/test-settings.spec.ts
@@ -0,0 +1,29 @@
+import { test } from '@playwright/test';
+
+test.skip(`Delete test`, async ({ page }) => {
+
+});
+
+test.skip(`Edit test`, async ({ page }) => {
+
+});
+
+test.skip(`Test pre-run script`, async ({ page }) => {
+
+});
+
+test.skip(`Test - variables and secrets`, async ({ page }) => {
+
+});
+
+test.skip(`Test - arguments`, async ({ page }) => {
+
+});
+
+test.skip(`Test - scheduling`, async ({ page }) => {
+
+});
+
+test.skip(`Test - definition`, async ({ page }) => {
+
+});
---
+++ test/dashboard-e2e/tests/testsuite-settings.spec.ts
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+
+test.skip(`Delete test suite`, async ({ page }) => {
+
+});
+
+test.skip(`Test suite - edit tests`, async ({ page }) => {
+
+});
+
+test.skip(`Test suite - edit label`, async ({ page }) => {
+
+});
+
+test.skip(`Test suite - variables`, async ({ page }) => {
+
+});
+
+test.skip(`Test suite - scheduling`, async ({ page }) => {
+
+});
+
+test.skip(`Test suite - definition`, async ({ page }) => {
+
+});
