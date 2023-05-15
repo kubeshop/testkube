99f25056aaca0d0c95be1c9b55f4dfef4265d6fc
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-05-15 22:26:24 +0200

    E2E tests moved to testkube-dashboard repo, test CRD updated

--- test/dashboard-e2e/tests/tests-dashboard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test.skip(`Tests dashboard - test "item" `, async ({ page }) => {
-
-});
-
-test.skip(`Tests dashboard - filters`, async ({ page }) => {
-
-});
--- test/dashboard-e2e/tests/testsuites-dashboard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test.skip(`Test Suites dashboard - test "item" `, async ({ page }) => {
-
-});
-
-test.skip(`Test Suites dashboard - filters`, async ({ page }) => {
-
-});
