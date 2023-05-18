49c6869d561c8202c2d9fce41776a83ce436f1ff
Povilas Versockas <p.versockas@gmail.com>
2023-05-18 16:06:21 +0300

    feat: add clusterid to agent

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -394,7 +394,7 @@ func main() {
 	if mode == common.ModeAgent {
 		log.DefaultLogger.Info("starting agent service")
 
-		agentHandle, err := agent.NewAgent(log.DefaultLogger, api.Mux.Handler(), cfg.TestkubeCloudAPIKey, grpcClient, cfg.TestkubeCloudWorkerCount, cfg.TestkubeCloudLogStreamWorkerCount, api.GetLogsStream)
+		agentHandle, err := agent.NewAgent(log.DefaultLogger, api.Mux.Handler(), cfg.TestkubeCloudAPIKey, grpcClient, cfg.TestkubeCloudWorkerCount, cfg.TestkubeCloudLogStreamWorkerCount, api.GetLogsStream, clusterId)
 		if err != nil {
 			ui.ExitOnError("Starting agent", err)
 		}
