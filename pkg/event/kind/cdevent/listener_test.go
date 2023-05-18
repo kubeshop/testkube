5c16ec64ee3c8c5905d089723a9a03976279ab3d
Vladislav Sukhin <vladislav@kubeshop.io>
2023-05-18 14:13:44 +0300

    feat: unit test for event listener

---
+++ pkg/event/kind/cdevent/listener_test.go
@@ -0,0 +1,96 @@
+package cdevent
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+)
+
+const executionID = "id-1"
+
+var testEventTypes = []testkube.EventType{*testkube.EventStartTest}
+
+func TestCDEventListener_Notify(t *testing.T) {
+
+	t.Run("send event success response", func(t *testing.T) {
+		// given
+		testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := cloudevents.NewEventFromHTTPRequest(r)
+			// then
+			assert.NoError(t, err)
+		})
+
+		svr := httptest.NewServer(testHandler)
+		defer svr.Close()
+
+		client, err := cloudevents.NewClientHTTP(cloudevents.WithTarget(svr.URL))
+		assert.NoError(t, err)
+
+		l := NewCDEventListener("cdeli", "", "clusterID", "", testEventTypes, client)
+
+		// when
+		r := l.Notify(testkube.Event{
+			Type_:         testkube.EventStartTest,
+			TestExecution: exampleExecution(),
+		})
+
+		assert.Equal(t, "", r.Error())
+	})
+
+	t.Run("send event failed response", func(t *testing.T) {
+		// given
+		testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadGateway)
+		})
+
+		svr := httptest.NewServer(testHandler)
+		defer svr.Close()
+
+		client, err := cloudevents.NewClientHTTP(cloudevents.WithTarget(svr.URL))
+		assert.NoError(t, err)
+
+		l := NewCDEventListener("cdeli", "", "clusterID", "", testEventTypes, client)
+
+		// when
+		r := l.Notify(testkube.Event{
+			Type_:         testkube.EventStartTest,
+			TestExecution: exampleExecution(),
+		})
+
+		// then
+		assert.NotEqual(t, "", r.Error())
+
+	})
+
+	t.Run("send event bad uri", func(t *testing.T) {
+		// given
+
+		client, err := cloudevents.NewClientHTTP(cloudevents.WithTarget("abcdef"))
+		assert.NoError(t, err)
+
+		l := NewCDEventListener("cdeli", "", "clusterID", "", testEventTypes, client)
+
+		// when
+		r := l.Notify(testkube.Event{
+			Type_:         testkube.EventStartTest,
+			TestExecution: exampleExecution(),
+		})
+
+		// then
+		assert.NotEqual(t, "", r.Error())
+	})
+
+}
+
+func exampleExecution() *testkube.Execution {
+	execution := testkube.NewQueuedExecution()
+	execution.Name = executionID
+	execution.TestName = "id"
+	execution.TestNamespace = "testkube"
+	return execution
+}
