fc13017733464101af06364fd0338a519298848a
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-05-21 23:52:54 +0200

    playwright artifacts tests

--- test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
+++ test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
@@ -4,5 +4,5 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('https://testkube.io/');
 
-  await expect(page).toHaveTitle(/Testkube/);
+  await expect(page).toHaveTitle(/ASDFASDF/);
 });
