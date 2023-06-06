cea0231d3a0d731cb139db8fb1ee5bfe001cb84c
Vladislav Sukhin <vladislav@kubeshop.io>
2023-06-06 20:19:25 +0300

    fix: unit test

--- pkg/crd/crd_test.go
+++ pkg/crd/crd_test.go
@@ -12,7 +12,7 @@ func TestGenerateYAML(t *testing.T) {
 
 	t.Run("generate single CRD yaml", func(t *testing.T) {
 		// given
-		expected := "apiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name1\n  namespace: namespace1\n  labels:\n    key1: value1\nspec:\n  events:\n  - start-test\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n"
+		expected := "apiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name1\n  namespace: namespace1\n  labels:\n    key1: value1\nspec:\n  events:\n  - start-test\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n  payloadTemplate: {{ .Id }}\n  headers:\n    Content-Type: appication/xml\n"
 		webhooks := []testkube.Webhook{
 			{
 				Name:               "name1",
@@ -37,7 +37,7 @@ func TestGenerateYAML(t *testing.T) {
 
 	t.Run("generate multiple CRDs yaml", func(t *testing.T) {
 		// given
-		expected := "apiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name1\n  namespace: namespace1\n  labels:\n    key1: value1\nspec:\n  events:\n  - start-test\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n\n---\napiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name2\n  namespace: namespace2\n  labels:\n    key2: value2\nspec:\n  events:\n  - end-test-success\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n"
+		expected := "apiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name1\n  namespace: namespace1\n  labels:\n    key1: value1\nspec:\n  events:\n  - start-test\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n  payloadTemplate: {{ .Id }}\n  headers:\n    Content-Type: appication/xml\n\n---\napiVersion: executor.testkube.io/v1\nkind: Webhook\nmetadata:\n  name: name2\n  namespace: namespace2\n  labels:\n    key2: value2\nspec:\n  events:\n  - end-test-success\n  uri: http://localhost\n  selector: app=backend\n  payloadObjectField: text\n  payloadTemplate: {{ .Id }}\n  headers:\n    Content-Type: appication/xml\n"
 		webhooks := []testkube.Webhook{
 			{
 				Name:               "name1",
