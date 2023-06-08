9fdcb8a88625c0730142e151222f32532b0f5b07
Abdallah <aabedraba@gmail.com>
2023-06-08 19:30:33 +0200

    docs: add support to executor kubent

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -156,6 +156,7 @@ const sidebars = {
         "test-types/executor-jmeter",
         "test-types/executor-k6",
         "test-types/executor-kubepug",
+        "test-types/executor-kubent",
         "test-types/executor-maven",
         "test-types/executor-playwright",
         "test-types/executor-postman",
