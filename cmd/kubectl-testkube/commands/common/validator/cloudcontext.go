0b0014905f8d46e794ef207a59834503c694da33
Jacek Wysocki <jacek.wysocki@gmail.com>
2023-06-14 14:36:25 +0200

    chore: organize

--- cmd/kubectl-testkube/commands/common/validator/cloudcontext.go
+++ cmd/kubectl-testkube/commands/common/validator/cloudcontext.go
@@ -1,16 +1,10 @@
 package validator
 
 import (
+	"errors"
+
 	"github.com/kubeshop/testkube/cmd/kubectl-testkube/config"
 )
-
-type ContextErr struct {
-	Msg string
-}
-
-func (e ContextErr) Error() string {
-	return e.Msg
-}
 
 func ValidateCloudContext(cfg config.Data) error {
 	if cfg.ContextType != config.ContextTypeCloud {
@@ -18,19 +12,19 @@ func ValidateCloudContext(cfg config.Data) error {
 	}
 
 	if cfg.CloudContext.ApiUri == "" {
-		return ContextErr{"please provide Testkube Cloud URI"}
+		return errors.New("please provide Testkube Cloud URI")
 	}
 
 	if cfg.CloudContext.ApiKey == "" {
-		return ContextErr{"please provide Testkube Cloud API token"}
+		return errors.New("please provide Testkube Cloud API token")
 	}
 
 	if cfg.CloudContext.Environment == "" {
-		return ContextErr{"please provide environment"}
+		return errors.New("please provide environment")
 	}
 
 	if cfg.CloudContext.Organization == "" {
-		return ContextErr{"please provide organization"}
+		return errors.New("please provide organization")
 	}
 
 	return nil
