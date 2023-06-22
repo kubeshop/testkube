d50f906ef559d84c3dc48c4a36513c7ff31e4fd9
ypoplavs <45286051+ypoplavs@users.noreply.github.com>
2023-06-22 12:53:33 +0300

    test: update install.sh script (#4071)
    * update install script to include beta arg

--- install.sh
+++ install.sh
@@ -30,20 +30,30 @@ _detect_os(){
      esac    
 }
 
-_download_url() { 
-        local arch="$(_detect_arch)" 
-        local os="$(_detect_os)"
-        if [ -z "$TESTKUBE_VERSION" ]
-        then
-                local version=`curl -s https://api.github.com/repos/kubeshop/testkube/releases/latest 2>/dev/null | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
-                echo "https://github.com/kubeshop/testkube/releases/download/${version}/testkube_${version:1}_${os}_$arch.tar.gz"       
-        else   
-                echo "https://github.com/kubeshop/testkube/releases/download/v${TESTKUBE_VERSION}/testkube_${TESTKUBE_VERSION}_${os}_$arch.tar.gz" 
-        fi
+_download_url() {
+  local arch="$(_detect_arch)"
+  local os="$(_detect_os)"
+  if [ -z "$TESTKUBE_VERSION" ]; then
+    if [ "$1" = "beta" ]; then
+      local version=$(curl -s "https://api.github.com/repos/kubeshop/testkube/releases" 2>/dev/null | jq -r '.[].tag_name | select(test("beta"))' | head -n 1)
+    else
+      local version=$(curl -s "https://api.github.com/repos/kubeshop/testkube/releases/latest" 2>/dev/null | jq -r '.tag_name')
+    fi
+  else
+    local version="$TESTKUBE_VERSION"
+  fi
+
+  echo "https://github.com/kubeshop/testkube/releases/download/${version}/testkube_${version:1}_${os}_$arch.tar.gz"
 }
 
-echo "Downloading testkube from URL: $(_download_url)" 
-curl -sSLf $(_download_url) > testkube.tar.gz 
+if [ "$1" = "beta" ]; then
+  echo "Downloading testkube from URL: $(_download_url "beta")"
+  curl -sSLf $(_download_url "beta") > testkube.tar.gz
+else
+  echo "Downloading testkube from URL: $(_download_url)"
+  curl -sSLf $(_download_url) > testkube.tar.gz
+fi
+
 tar -xzf testkube.tar.gz kubectl-testkube 
 rm testkube.tar.gz
 mv kubectl-testkube /usr/local/bin/kubectl-testkube
