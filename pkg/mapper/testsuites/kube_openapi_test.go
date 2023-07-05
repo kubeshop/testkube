385c8886b90aeba181cbff9c8f54a1f96c5148c1
Dawid Rusnak <dawid@drcode.pl>
2023-07-05 11:16:09 +0200

    fix: map test suite YAML to proper update structure (#4129)

--- pkg/mapper/testsuites/kube_openapi_test.go
+++ pkg/mapper/testsuites/kube_openapi_test.go
@@ -19,7 +19,7 @@ func TestMapTestSuiteListKubeToAPI(t *testing.T) {
 					{
 						Execute: []testsuitesv3.TestSuiteStepSpec{
 							{
-								Delay: metav1.Duration{Duration: time.Second},
+								Delay: metav1.Duration{Duration: 2 * time.Second},
 							},
 						},
 					},
@@ -50,7 +50,58 @@ func TestMapTestSuiteListKubeToAPI(t *testing.T) {
 		},
 	)
 
-	assert.Equal(t, 1, len(openAPITest.Steps))
 	assert.Equal(t, 1, len(openAPITest.Before))
+	assert.Equal(t, "2s", openAPITest.Before[0].Execute[0].Delay)
+	assert.Equal(t, 1, len(openAPITest.Steps))
+	assert.Equal(t, "some-test-name", openAPITest.Steps[0].Execute[0].Test)
 	assert.Equal(t, 1, len(openAPITest.After))
+	assert.Equal(t, "1s", openAPITest.After[0].Execute[0].Delay)
+}
+
+func TestMapTestSuiteTestCRDToUpdateRequest(t *testing.T) {
+
+	openAPITest := MapTestSuiteTestCRDToUpdateRequest(
+		&testsuitesv3.TestSuite{
+			Spec: testsuitesv3.TestSuiteSpec{
+				Before: []testsuitesv3.TestSuiteBatchStep{
+					{
+						Execute: []testsuitesv3.TestSuiteStepSpec{
+							{
+								Delay: metav1.Duration{Duration: 2 * time.Second},
+							},
+						},
+					},
+				},
+
+				Steps: []testsuitesv3.TestSuiteBatchStep{
+					{
+						Execute: []testsuitesv3.TestSuiteStepSpec{
+							{
+								Test: "some-test-name",
+							},
+						},
+					},
+				},
+
+				After: []testsuitesv3.TestSuiteBatchStep{
+					{
+						Execute: []testsuitesv3.TestSuiteStepSpec{
+							{
+								Delay: metav1.Duration{Duration: time.Second},
+							},
+						},
+					},
+				},
+
+				Repeats: 2,
+			},
+		},
+	)
+
+	assert.Equal(t, 1, len(*openAPITest.Before))
+	assert.Equal(t, "2s", (*openAPITest.Before)[0].Execute[0].Delay)
+	assert.Equal(t, 1, len(*openAPITest.Steps))
+	assert.Equal(t, "some-test-name", (*openAPITest.Steps)[0].Execute[0].Test)
+	assert.Equal(t, 1, len(*openAPITest.After))
+	assert.Equal(t, "1s", (*openAPITest.After)[0].Execute[0].Delay)
 }
