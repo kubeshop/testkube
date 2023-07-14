ea8a230f7805500bb79f6efac68caffa373af0a7
Vladislav Sukhin <vladislav@kubeshop.io>
2023-07-14 20:32:13 +0300

    fix: pass registry parameter

--- pkg/executor/containerexecutor/tmpl.go
+++ pkg/executor/containerexecutor/tmpl.go
@@ -218,7 +218,7 @@ func InspectDockerImage(namespace, registry, image string, imageSecrets []string
 			secrets = append(secrets, *object)
 		}
 
-		inspector, err = skopeo.NewClientFromSecrets(secrets, "")
+		inspector, err = skopeo.NewClientFromSecrets(secrets, registry)
 		if err != nil {
 			return nil, "", err
 		}
