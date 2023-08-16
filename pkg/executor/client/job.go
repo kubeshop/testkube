74501d94fe32e163000aad600cf015d135391eff
nicufk <89570185+nicufk@users.noreply.github.com>
2023-08-16 10:47:38 +0300

    fix: streaming logs not working (#4212)
    * fix: streaming logs not working
    
    * fix: use only one condition wait when waiting for logs

--- pkg/executor/client/job.go
+++ pkg/executor/client/job.go
@@ -330,11 +330,9 @@ func (c *JobExecutor) updateResultsFromPod(ctx context.Context, pod corev1.Pod, 
 	// wait for pod
 	l.Debug("poll immediate waiting for pod")
 	if err = wait.PollImmediate(pollInterval, c.podStartTimeout, executor.IsPodLoggable(ctx, c.ClientSet, pod.Name, c.Namespace)); err != nil {
-		l.Errorw("waiting for pod started error", "error", err)
-	} else if err = wait.PollImmediate(pollInterval, pollTimeout, executor.IsPodReady(ctx, c.ClientSet, pod.Name, c.Namespace)); err != nil {
-		// continue on poll err and try to get logs later
-		l.Errorw("waiting for pod complete error", "error", err)
+		l.Errorw("waiting for pod logs to be available", "error", err)
 	}
+
 	if err != nil {
 		execution.ExecutionResult.Err(err)
 	}
@@ -559,9 +557,6 @@ func (c *JobExecutor) TailJobLogs(ctx context.Context, id string, logs chan []by
 				if err = wait.PollImmediate(pollInterval, c.podStartTimeout, executor.IsPodLoggable(ctx, c.ClientSet, pod.Name, c.Namespace)); err != nil {
 					l.Errorw("poll immediate error when tailing logs", "error", err)
 					return err
-				} else if err = wait.PollImmediate(pollInterval, pollTimeout, executor.IsPodReady(ctx, c.ClientSet, pod.Name, c.Namespace)); err != nil {
-					l.Errorw("poll immediate error when tailing logs", "error", err)
-					return c.GetLastLogLineError(ctx, pod)
 				}
 
 				l.Debug("tailing pod logs")
