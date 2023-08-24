ad02af017612f3f3d0f0d162a0e917ac52fc7412
hiteshwani <hitesh.wani@infracloud.io>
2023-08-24 10:08:56 +0530

    Added jmeter slaves script and Dockerfile
    Signed-off-by: hiteshwani <hitesh.wani@infracloud.io>

---
+++ contrib/executor/jmeterd/scripts/jmeter-slaves.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+echo "********************************************************"
+echo "*              Installing JMeter Plugins               *"
+echo "********************************************************"
+echo
+
+if [ -d $JMETER_CUSTOM_PLUGINS_FOLDER ]
+then
+  echo "Installing custom plugins from ${JMETER_CUSTOM_PLUGINS_FOLDER}"
+  for plugin in ${JMETER_CUSTOM_PLUGINS_FOLDER}/*.jar; do
+      echo "Copying plugin $plugin to ${JMETER_HOME}/lib/ext/${plugin}"
+      cp $plugin ${JMETER_HOME}/lib/ext
+  done;
+else
+  echo "No custom plugins found in ${JMETER_CUSTOM_PLUGINS_FOLDER}"
+fi
+echo
+
+echo "********************************************************"
+echo "*            Initializing JMeter Master                *"
+echo "********************************************************"
+echo
+
+freeMem=`awk '/MemAvailable/ { print int($2/1024) }' /proc/meminfo`
+
+[[ -z ${JVM_XMN} ]] && JVM_XMN=$(($freeMem/10*2))
+[[ -z ${JVM_XMS} ]] && JVM_XMS=$(($freeMem/10*8))
+[[ -z ${JVM_XMX} ]] && JVM_XMX=$(($freeMem/10*8))
+
+echo "Setting default JVM_ARGS=-Xmn${JVM_XMN}m -Xms${JVM_XMS}m -Xmx${JVM_XMX}m"
+export JVM_ARGS="-Xmn${JVM_XMN}m -Xms${JVM_XMS}m -Xmx${JVM_XMX}m"
+
+if [ -n "$OVERRIDE_JVM_ARGS" ]; then
+  echo "Overriding JVM_ARGS=${OVERRIDE_JVM_ARGS}"
+  export JVM_ARGS="${OVERRIDE_JVM_ARGS}"
+fi
+
+if [ -n "$ADDITIONAL_JVM_ARGS" ]; then
+  echo "Appending additional JVM args: ${ADDITIONAL_JVM_ARGS}"
+  export JVM_ARGS="${JVM_ARGS} ${ADDITIONAL_JVM_ARGS}"
+fi
+
+echo "Available memory: ${freeMem} MB"
+echo "Configured JVM_ARGS=${JVM_ARGS}"
+echo
+
+echo "********************************************************"
+echo "*              Starting JMeter Server                  *"
+echo "********************************************************"
+echo
+
+SERVER_ARGS="-Dserver.rmi.localport=60001 -Dserver_port=1099 -Jserver.rmi.ssl.disable=${SSL_DISABLED}"
+echo "Running command: jmeter-server ${SERVER_ARGS} ${SLAVES_ADDITIONAL_JMETER_ARGS}"
+echo
+
+jmeter-server ${SERVER_ARGS} ${SLAVES_ADDITIONAL_JMETER_ARGS}
