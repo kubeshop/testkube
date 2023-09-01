86743f96cbdf97671fa369f2cea5e583c9c1cae1
Vladislav Sukhin <vladislav@kubeshop.io>
2023-09-01 13:52:55 +0300

    fixL list templates

--- pkg/api/v1/testkube/model_template_extended.go
+++ pkg/api/v1/testkube/model_template_extended.go
@@ -10,3 +10,22 @@
 package testkube
 
 type Templates []Template
+
+func (list Templates) Table() (header []string, output [][]string) {
+	header = []string{"Name", "Type", "Labels"}
+
+	for _, e := range list {
+		templateType := ""
+		if e.Type_ != nil {
+			templateType = string(*e.Type_)
+		}
+
+		output = append(output, []string{
+			e.Name,
+			templateType,
+			MapToString(e.Labels),
+		})
+	}
+
+	return
+}
