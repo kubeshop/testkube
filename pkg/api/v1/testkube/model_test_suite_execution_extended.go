fc924ab09f695e8d06b1d814a599b7839026bdef
nicufk <89570185+nicufk@users.noreply.github.com>
2023-10-02 14:05:08 +0300

    fix: do not store output in test suite execution result (#4409) (#4422)
    * fix: do not store output in test suite execution result (#4409)
    
    * fix: do not store output in test suite execution result
    
    * fix: clean execution result in the BatchResults as well
    
    * fix: add apis spec note and check for nil
    
    * fix: batch output clean (#4423)

--- pkg/api/v1/testkube/model_test_suite_execution_extended.go
+++ pkg/api/v1/testkube/model_test_suite_execution_extended.go
@@ -243,3 +243,20 @@ func (e *TestSuiteExecution) EscapeDots() *TestSuiteExecution {
 func (e *TestSuiteExecution) UnscapeDots() *TestSuiteExecution {
 	return e.convertDots(utils.UnescapeDots)
 }
+
+func (e *TestSuiteExecution) CleanStepsOutput() *TestSuiteExecution {
+	for i := range e.StepResults {
+		if e.StepResults[i].Execution != nil && e.StepResults[i].Execution.ExecutionResult != nil {
+			e.StepResults[i].Execution.ExecutionResult.Output = ""
+		}
+	}
+
+	for i := range e.ExecuteStepResults {
+		for j := range e.ExecuteStepResults[i].Execute {
+			if e.ExecuteStepResults[i].Execute[j].Execution != nil && e.ExecuteStepResults[i].Execute[j].Execution.ExecutionResult != nil {
+				e.ExecuteStepResults[i].Execute[j].Execution.ExecutionResult.Output = ""
+			}
+		}
+	}
+	return e
+}
