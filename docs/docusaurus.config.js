f25eef573bd691e99b84c9afccd1da1ac9d18b9a
Dawid Rusnak <dawid@drcode.pl>
2023-10-19 14:16:45 +0200

    feat: add announcement bar for Testkube Cloud

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -100,6 +100,13 @@ const config = {
         ],
       },
 
+      announcementBar: {
+        id: "join-testkube-cloud",
+        content: '<strong>New to Testkube?</strong> Start leveraging Kubernetes to automate any type of test. <a id="docs_sign_cloud" href="https://cloud.testkube.io/?utm_source=docs" target="_blank">Get Started ></a>',
+        backgroundColor: "#8951ff",
+        textColor: "#fff",
+      },
+
       footer: {
         style: "dark",
         links: [
