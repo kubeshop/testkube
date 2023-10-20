113c986601b3593e2f03831c322429a3e1268b6a
Dawid Rusnak <dawid@drcode.pl>
2023-10-20 09:00:49 +0200

    fix: change the TK Cloud announcement

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
 
       announcementBar: {
         id: "join-testkube-cloud",
-        content: '<strong>New to Testkube?</strong> Start leveraging Kubernetes to automate any type of test. <a id="sign_cloud_docs" href="https://cloud.testkube.io/?utm_source=docs" target="_blank">Get Started ></a>',
+        content: '<strong>New to Testkube?</strong> Unleash the power of cloud native testing in Kubernetes with Testkube. <a id="sign_cloud_docs" href="https://cloud.testkube.io/?utm_source=docs" target="_blank">Get Started ></a>',
         backgroundColor: "#8951ff",
         textColor: "#fff",
       },
