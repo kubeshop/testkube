d9aa1e73c086ecad268d5d11d16e39e86c718d10
Vladislav Sukhin <vladislav@kubeshop.io>
2023-10-26 17:59:00 +0300

    fix: ignore post script error

--- contrib/executor/soapui/pkg/runner/runner.go
+++ contrib/executor/soapui/pkg/runner/runner.go
@@ -99,8 +99,8 @@ func (r *SoapUIRunner) Run(ctx context.Context, execution testkube.Execution) (r
 	if execution.PostRunScript != "" && execution.ExecutePostRunScriptBeforeScraping {
 		output.PrintLog(fmt.Sprintf("%s Running post run script...", ui.IconCheckMark))
 
-		if err = agent.RunScript(execution.PostRunScript, r.Params.WorkingDir); err != nil {
-			output.PrintLogf("%s Failed to execute post run script %s", ui.IconWarning, err)
+		if rerr := agent.RunScript(execution.PostRunScript, r.Params.WorkingDir); rerr != nil {
+			output.PrintLogf("%s Failed to execute post run script %s", ui.IconWarning, rerr)
 		}
 	}
 
