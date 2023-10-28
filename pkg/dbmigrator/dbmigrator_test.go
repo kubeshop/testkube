020bfcd7763dd24cfcb0341243010d2966e9e8bb
Dawid Rusnak <dawid@drcode.pl>
2023-10-28 12:57:40 +0200

    chore: add unit tests for getting MongoDB migrations from FS

--- pkg/dbmigrator/dbmigrator_test.go
+++ pkg/dbmigrator/dbmigrator_test.go
@@ -2,6 +2,7 @@ package dbmigrator
 
 import (
 	"context"
+	"github.com/spf13/afero"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -213,4 +214,26 @@ func TestDbMigrator_Apply_Downgrade_On_Apply_Error(t *testing.T) {
 	err := migrator.Apply(ctx)
 
 	assert.Error(t, err, "test-failed")
+}
+
+func TestGetDbMigrationsFromFs_Empty(t *testing.T) {
+	fsys := &afero.IOFS{Fs: afero.NewMemMapFs()}
+	migrations, err := GetDbMigrationsFromFs(fsys)
+	assert.Equal(t, []DbMigration(nil), migrations)
+	assert.NoError(t, err)
+}
+
+func TestGetDbMigrationsFromFs_Files(t *testing.T) {
+	fsys := &afero.IOFS{Fs: afero.NewMemMapFs()}
+	_ = afero.WriteFile(fsys.Fs, "02_file.up.json", []byte(`[{"a": "2"}]`), 0644)
+	_ = afero.WriteFile(fsys.Fs, "01_file.up.json", []byte(`[{"a": "1"}]`), 0644)
+	_ = afero.WriteFile(fsys.Fs, "02_file.down.json", []byte(`[{"b": "2"}]`), 0644)
+	_ = afero.WriteFile(fsys.Fs, "no_suffix.json", []byte(`[{"b": "2"}]`), 0644)
+	_ = afero.WriteFile(fsys.Fs, "different_ext.up.js", []byte(`[{"b": "2"}]`), 0644)
+	migrations, err := GetDbMigrationsFromFs(fsys)
+	assert.Equal(t, []DbMigration{
+		{Name: "01_file", UpScript: []bson.D{{{Key: "a", Value: "1"}}}, DownScript: []bson.D{}},
+		{Name: "02_file", UpScript: []bson.D{{{Key: "a", Value: "2"}}}, DownScript: []bson.D{{{Key: "b", Value: "2"}}}},
+	}, migrations)
+	assert.NoError(t, err)
 }
