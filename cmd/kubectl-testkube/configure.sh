0c12fbcee3e5a8c6a8360634c98d4b8313f7729b
Hamid Emamian <emami.he@gmail.com>
2023-12-01 13:40:47 +0300

    condition changed in configure.sh

--- cmd/kubectl-testkube/configure.sh
+++ cmd/kubectl-testkube/configure.sh
@@ -9,9 +9,7 @@ fi
 if [ -n "$NAMESPACE" ]; then
 	echo "setting the context for testkube to namespace $NAESPACE"
 	testkube set context --kubeconfig --namespace $NAMESPACE
-fi
-
-if [ -n "$TESTKUBE_API_KEY" ] && [ -n "$TESTKUBE_ORG_ID" ] && [ -n "TESTKUBE_ENV_ID" ]; then
+elif [ -n "$TESTKUBE_API_KEY" ] && [ -n "$TESTKUBE_ORG_ID" ] && [ -n "TESTKUBE_ENV_ID" ]; then
 	echo "setting the context for testkube pro"
 	testkube set context --api-key $TESTKUBE_API_KEY --org $TESTKUBE_ORG_ID --env $TESTKUBE_ENV_ID
 fi
