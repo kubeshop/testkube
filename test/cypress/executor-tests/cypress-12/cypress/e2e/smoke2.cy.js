86aef44fd1a287931826857ae80b3c03fe6bb2eb
Dawid Rusnak <dawid@drcode.pl>
2023-12-01 12:37:06 +0100

    debug: long lines

--- test/cypress/executor-tests/cypress-12/cypress/e2e/smoke2.cy.js
+++ test/cypress/executor-tests/cypress-12/cypress/e2e/smoke2.cy.js
@@ -1,5 +1,9 @@
 describe('Smoke test 2', () => {
   it(`expect 1=1`, () => {
-    expect('1').to.equal('1')
+    let value = '';
+    for (let i = 0; i < 17000; i++) {
+      value += 'a';
+    }
+    expect(value).to.equal('1');
   })
 })
