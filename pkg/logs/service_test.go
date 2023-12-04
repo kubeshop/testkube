db2e06d3c84538eb2116d287ad955dc71ae5b399
Jacek Wysocki <jacek.wysocki@gmail.com>
2023-12-04 13:47:13 +0100

    fix: addadapter test

--- pkg/logs/service_test.go
+++ pkg/logs/service_test.go
@@ -1,1 +1,24 @@
 package logs
+
+import (
+	"testing"
+
+	"github.com/kubeshop/testkube/pkg/logs/consumer"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogsService_AddAdapter(t *testing.T) {
+
+	t.Run("should add adapter", func(t *testing.T) {
+		svc := LogsService{}
+
+		svc.AddAdapter(consumer.NewDummyAdapter())
+		svc.AddAdapter(consumer.NewDummyAdapter())
+		svc.AddAdapter(consumer.NewDummyAdapter())
+		svc.AddAdapter(consumer.NewDummyAdapter())
+
+		assert.Equal(t, 4, len(svc.adapters))
+	})
+
+}
