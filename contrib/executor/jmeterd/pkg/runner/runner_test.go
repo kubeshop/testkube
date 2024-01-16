ea557920c610fd90db0842bcfec5937ed6405772
Vladislav Sukhin <vladislav@kubeshop.io>
2024-01-16 20:55:25 +0300

    fix: unit tests

--- contrib/executor/jmeterd/pkg/runner/runner_test.go
+++ contrib/executor/jmeterd/pkg/runner/runner_test.go
@@ -57,7 +57,7 @@ func TestReplaceArgs(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			hasJunit, hasReport := replacePlaceholderArgs(tt.args, tt.path, tt.jtlPath, tt.reportPath, tt.jmeterLogPath)
+			hasJunit, hasReport := prepareArgs(tt.args, tt.path, tt.jtlPath, tt.reportPath, tt.jmeterLogPath)
 
 			for i, arg := range tt.args {
 				assert.Equal(t, tt.expectedArgs[i], arg)
