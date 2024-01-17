53f07abe274112705a7f1652de29e5238c73f419
Vladislav Sukhin <vladislav@kubeshop.io>
2024-01-17 13:38:57 +0300

    fix: one more test case

--- contrib/executor/jmeterd/pkg/runner/runner_test.go
+++ contrib/executor/jmeterd/pkg/runner/runner_test.go
@@ -178,6 +178,13 @@ func TestRemoveDuplicatedArgs(t *testing.T) {
 			args:         []string{"-t", "first", "-t", "second", "-l"},
 			expectedArgs: []string{"-t", "first", "-t", "second", "-l"},
 		},
+		{
+			name:           "Duplicated not template args",
+			args:           []string{"-t", "first", "-t", "second", "-l"},
+			expectedArgs:   []string{"-t", "first", "-t", "second", "-l"},
+			expectedJunit:  true,
+			expectedReport: false,
+		},
 	}
 
 	for i := range tests {
