8011aa88ecd3c094fffe0f451a9c859a0fa05bc6
nicufk <nicolae@kubeshop.io>
2024-01-18 13:26:41 +0200

    feat: add minio log consumer with opts (#4867)
    * feat: add minio log consumer with opts
    
    * fix: tests
    
    * fix: skip test
    
    * fix: improve code and error handling
    
    * fix: handle chunk too big
    
    * fix: execute but return errors
    
    * fix: minor improvements
    
    * fix: contexts

---
+++ pkg/logs/adapter/minio.go
@@ -0,0 +1,236 @@
+package adapter
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"sync"
+
+	"github.com/minio/minio-go/v7"
+	"go.uber.org/zap"
+
+	"github.com/kubeshop/testkube/pkg/log"
+	"github.com/kubeshop/testkube/pkg/logs/events"
+	minioconnecter "github.com/kubeshop/testkube/pkg/storage/minio"
+)
+
+const (
+	defaultBufferSize = 1024 * 100 // 100KB
+	defaultWriteSize  = 1024 * 80  // 80KB
+)
+
+var _ Adapter = &MinioConsumer{}
+
+type ErrMinioConsumerDisconnected struct {
+}
+
+func (e ErrMinioConsumerDisconnected) Error() string {
+	return "minio consumer disconnected"
+}
+
+type ErrIdNotFound struct {
+	Id string
+}
+
+func (e ErrIdNotFound) Error() string {
+	return fmt.Sprintf("id %s not found", e.Id)
+}
+
+type ErrChucnkTooBig struct {
+	Length int
+}
+
+func (e ErrChucnkTooBig) Error() string {
+	return fmt.Sprintf("chunk too big: %d", e.Length)
+}
+
+type BufferInfo struct {
+	Buffer *bytes.Buffer
+	Part   int
+}
+
+// MinioConsumer creates new MinioSubscriber which will send data to local MinIO bucket
+func NewMinioConsumer(endpoint, accessKeyID, secretAccessKey, region, token, bucket string, opts ...minioconnecter.Option) (*MinioConsumer, error) {
+	ctx := context.TODO()
+	c := &MinioConsumer{
+		minioConnecter: minioconnecter.NewConnecter(endpoint, accessKeyID, secretAccessKey, region, token, bucket, log.DefaultLogger, opts...),
+		Log:            log.DefaultLogger,
+		bucket:         bucket,
+		region:         region,
+		disconnected:   false,
+		buffInfos:      make(map[string]BufferInfo),
+	}
+	minioClient, err := c.minioConnecter.GetClient()
+	if err != nil {
+		c.Log.Errorw("error connecting to minio", "err", err)
+		return c, err
+	}
+
+	c.minioClient = minioClient
+	exists, err := c.minioClient.BucketExists(ctx, c.bucket)
+	if err != nil {
+		c.Log.Errorw("error checking if bucket exists", "err", err)
+		return c, err
+	}
+
+	if !exists {
+		err = c.minioClient.MakeBucket(ctx, c.bucket,
+			minio.MakeBucketOptions{Region: c.region})
+		if err != nil {
+			c.Log.Errorw("error creating bucket", "err", err)
+			return c, err
+		}
+	}
+	return c, nil
+}
+
+type MinioConsumer struct {
+	minioConnecter *minioconnecter.Connecter
+	minioClient    *minio.Client
+	bucket         string
+	region         string
+	Log            *zap.SugaredLogger
+	disconnected   bool
+	buffInfos      map[string]BufferInfo
+	mapLock        sync.RWMutex
+}
+
+func (s *MinioConsumer) Notify(id string, e events.Log) error {
+	if s.disconnected {
+		s.Log.Debugw("minio consumer disconnected", "id", id)
+		return ErrMinioConsumerDisconnected{}
+	}
+
+	buffInfo, ok := s.GetBuffInfo(id)
+	if !ok {
+		buffInfo = BufferInfo{Buffer: bytes.NewBuffer(make([]byte, 0, defaultBufferSize)), Part: 0}
+		s.UpdateBuffInfo(id, buffInfo)
+	}
+
+	chunckToAdd, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+
+	if len(chunckToAdd) > defaultWriteSize {
+		s.Log.Warnw("chunck too big", "length", len(chunckToAdd))
+		return ErrChucnkTooBig{len(chunckToAdd)}
+	}
+
+	chunckToAdd = append(chunckToAdd, []byte("\n")...)
+
+	writer := buffInfo.Buffer
+	_, err = writer.Write(chunckToAdd)
+	if err != nil {
+		return err
+	}
+
+	if writer.Len() > defaultWriteSize {
+		buffInfo.Buffer = bytes.NewBuffer(make([]byte, 0, defaultBufferSize))
+		name := id + "-" + strconv.Itoa(buffInfo.Part)
+		buffInfo.Part++
+		s.UpdateBuffInfo(id, buffInfo)
+		go s.putData(context.TODO(), name, writer)
+	}
+
+	return nil
+}
+
+func (s *MinioConsumer) putData(ctx context.Context, name string, buffer *bytes.Buffer) {
+	if buffer != nil && buffer.Len() != 0 {
+		_, err := s.minioClient.PutObject(ctx, s.bucket, name, buffer, int64(buffer.Len()), minio.PutObjectOptions{ContentType: "application/octet-stream"})
+		if err != nil {
+			s.Log.Errorw("error putting object", "err", err)
+		}
+	} else {
+		s.Log.Warn("empty buffer for name: ", name)
+	}
+
+}
+
+func (s *MinioConsumer) combineData(ctxt context.Context, minioClient *minio.Client, id string, parts int, deleteIntermediaryData bool) error {
+	var returnedError []error
+	returnedError = nil
+	buffer := bytes.NewBuffer(make([]byte, 0, parts*defaultBufferSize))
+	for i := 0; i < parts; i++ {
+		objectName := fmt.Sprintf("%s-%d", id, i)
+		if s.objectExists(objectName) {
+			objInfo, err := minioClient.GetObject(ctxt, s.bucket, objectName, minio.GetObjectOptions{})
+			if err != nil {
+				s.Log.Errorw("error getting object", "err", err)
+				returnedError = append(returnedError, err)
+			}
+			_, err = buffer.ReadFrom(objInfo)
+			if err != nil {
+				s.Log.Errorw("error reading object", "err", err)
+				returnedError = append(returnedError, err)
+			}
+		}
+	}
+	_, err := minioClient.PutObject(ctxt, s.bucket, id, buffer, int64(buffer.Len()), minio.PutObjectOptions{ContentType: "application/octet-stream"})
+	if err != nil {
+		s.Log.Errorw("error putting object", "err", err)
+		return err
+	}
+
+	if deleteIntermediaryData {
+		for i := 0; i < parts; i++ {
+			objectName := fmt.Sprintf("%s-%d", id, i)
+			if s.objectExists(objectName) {
+				err = minioClient.RemoveObject(ctxt, s.bucket, objectName, minio.RemoveObjectOptions{})
+				if err != nil {
+					s.Log.Errorw("error removing object", "err", err)
+					returnedError = append(returnedError, err)
+				}
+			}
+		}
+	}
+	buffer.Reset()
+	if len(returnedError) == 0 {
+		return nil
+	}
+	return fmt.Errorf("executed with errors: %v", returnedError)
+}
+
+func (s *MinioConsumer) objectExists(objectName string) bool {
+	_, err := s.minioClient.StatObject(context.Background(), s.bucket, objectName, minio.StatObjectOptions{})
+	return err == nil
+}
+
+func (s *MinioConsumer) Stop(id string) error {
+	ctx := context.TODO()
+	buffInfo, ok := s.GetBuffInfo(id)
+	if !ok {
+		return ErrIdNotFound{id}
+	}
+	name := id + "-" + strconv.Itoa(buffInfo.Part)
+	s.putData(ctx, name, buffInfo.Buffer)
+	parts := buffInfo.Part + 1
+	s.DeleteBuffInfo(id)
+	return s.combineData(ctx, s.minioClient, id, parts, true)
+}
+
+func (s *MinioConsumer) Name() string {
+	return "minio"
+}
+
+func (s *MinioConsumer) GetBuffInfo(id string) (BufferInfo, bool) {
+	s.mapLock.RLock()
+	defer s.mapLock.RUnlock()
+	buffInfo, ok := s.buffInfos[id]
+	return buffInfo, ok
+}
+
+func (s *MinioConsumer) UpdateBuffInfo(id string, buffInfo BufferInfo) {
+	s.mapLock.Lock()
+	defer s.mapLock.Unlock()
+	s.buffInfos[id] = buffInfo
+}
+
+func (s *MinioConsumer) DeleteBuffInfo(id string) {
+	s.mapLock.Lock()
+	defer s.mapLock.Unlock()
+	delete(s.buffInfos, id)
+}
---
+++ pkg/storage/minio/minio_connecter.go
@@ -0,0 +1,147 @@
+package minio
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type Option func(*Connecter) error
+
+// Insecure is an Option to enable TLS secure connections that skip server verification.
+func Insecure() Option {
+	return func(o *Connecter) error {
+		if o.TlsConfig == nil {
+			o.TlsConfig = &tls.Config{MinVersion: tls.VersionTLS12}
+		}
+		o.TlsConfig.InsecureSkipVerify = true
+		o.Ssl = true
+		return nil
+	}
+}
+
+// RootCAs is a helper option to provide the RootCAs pool from a list of filenames.
+// If Secure is not already set this will set it as well.
+func RootCAs(file ...string) Option {
+	return func(o *Connecter) error {
+		pool := x509.NewCertPool()
+		for _, f := range file {
+			rootPEM, err := os.ReadFile(f)
+			if err != nil || rootPEM == nil {
+				return fmt.Errorf("nats: error loading or parsing rootCA file: %v", err)
+			}
+			ok := pool.AppendCertsFromPEM(rootPEM)
+			if !ok {
+				return fmt.Errorf("nats: failed to parse root certificate from %q", f)
+			}
+		}
+		if o.TlsConfig == nil {
+			o.TlsConfig = &tls.Config{MinVersion: tls.VersionTLS12}
+		}
+		o.TlsConfig.RootCAs = pool
+		o.Ssl = true
+		return nil
+	}
+}
+
+// ClientCert is a helper option to provide the client certificate from a file.
+// If Secure is not already set this will set it as well.
+func ClientCert(certFile, keyFile string) Option {
+	return func(o *Connecter) error {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return fmt.Errorf("nats: error loading client certificate: %v", err)
+		}
+		cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+		if err != nil {
+			return fmt.Errorf("nats: error parsing client certificate: %v", err)
+		}
+		if o.TlsConfig == nil {
+			o.TlsConfig = &tls.Config{MinVersion: tls.VersionTLS12}
+		}
+		o.TlsConfig.Certificates = []tls.Certificate{cert}
+		o.Ssl = true
+		return nil
+	}
+}
+
+type Connecter struct {
+	Endpoint        string
+	AccessKeyID     string
+	SecretAccessKey string
+	Region          string
+	Token           string
+	Bucket          string
+	Ssl             bool
+	TlsConfig       *tls.Config
+	Opts            []Option
+	Log             *zap.SugaredLogger
+	client          *minio.Client
+}
+
+// NewConnecter creates a new Connecter
+func NewConnecter(endpoint, accessKeyID, secretAccessKey, region, token, bucket string, log *zap.SugaredLogger, opts ...Option) *Connecter {
+	c := &Connecter{
+		Endpoint:        endpoint,
+		AccessKeyID:     accessKeyID,
+		SecretAccessKey: secretAccessKey,
+		Region:          region,
+		Token:           token,
+		Bucket:          bucket,
+		Opts:            opts,
+		Log:             log,
+	}
+	return c
+}
+
+// GetClient() connects to MinIO
+func (c *Connecter) GetClient() (*minio.Client, error) {
+	if c.client != nil {
+		return c.client, nil
+	}
+
+	for _, opt := range c.Opts {
+		if err := opt(c); err != nil {
+			return nil, errors.Wrapf(err, "error connecting to server")
+		}
+	}
+	creds := credentials.NewIAM("")
+	c.Log.Debugw("connecting to server",
+		"endpoint", c.Endpoint,
+		"accessKeyID", c.AccessKeyID,
+		"region", c.Region,
+		"token", c.Token,
+		"bucket", c.Bucket,
+		"ssl", c.Ssl)
+	if c.AccessKeyID != "" && c.SecretAccessKey != "" {
+		creds = credentials.NewStaticV4(c.AccessKeyID, c.SecretAccessKey, c.Token)
+	}
+	transport, err := minio.DefaultTransport(c.Ssl)
+	if err != nil {
+		c.Log.Errorw("error creating minio transport", "error", err)
+		return nil, err
+	}
+	transport.TLSClientConfig = c.TlsConfig
+	opts := &minio.Options{
+		Creds:     creds,
+		Secure:    c.Ssl,
+		Transport: transport,
+	}
+	if c.Region != "" {
+		opts.Region = c.Region
+	}
+	mclient, err := minio.New(c.Endpoint, opts)
+	if err != nil {
+		c.Log.Errorw("error connecting to minio", "error", err)
+		return nil, err
+	}
+
+	c.client = mclient
+	return mclient, nil
+}
