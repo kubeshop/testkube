6a3b9af8685ba34d38b7f1ffe71db7c2026df9a2
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-01-24 15:16:05 +0100

    fix: tests

--- pkg/logs/events_test.go
+++ pkg/logs/events_test.go
@@ -85,6 +85,9 @@ func TestLogs_EventsFlow(t *testing.T) {
 		_, err = stream.Stop(ctx, "stop-test")
 		assert.NoError(t, err)
 
+		// cooldown stop time
+		time.Sleep(waitTime * 2)
+
 		// then all adapters should be gracefully stopped
 		assert.Equal(t, 0, log.GetConsumersStats(ctx).Count)
 	})
@@ -224,8 +227,8 @@ func TestLogs_EventsFlow(t *testing.T) {
 		_, err = stream.Stop(ctx, "consumer-stats")
 		assert.NoError(t, err)
 
-		// wait for mess
-		time.Sleep(time.Millisecond * 100)
+		// there will be wait timwait for mess
+		time.Sleep(waitTime * 2)
 
 		// then all adapters should be gracefully stopped
 		assert.Equal(t, 0, log.GetConsumersStats(ctx).Count)
