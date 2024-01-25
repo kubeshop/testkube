f3acd41a32ada69acc17704baa1a0473aff2e098
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-01-25 16:59:00 +0100

    fix: adapter stop call after consumer stop (#4950)

--- pkg/logs/events.go
+++ pkg/logs/events.go
@@ -229,7 +229,7 @@ func (ls *LogsService) handleStop(ctx context.Context, group string) func(msg *n
 	}
 }
 
-func (ls *LogsService) stopConsumer(ctx context.Context, wg *sync.WaitGroup, consumer Consumer) {
+func (ls *LogsService) stopConsumer(ctx context.Context, wg *sync.WaitGroup, consumer Consumer, adapter adapter.Adapter, id string) {
 	defer wg.Done()
 
 	var (
@@ -261,6 +261,14 @@ func (ls *LogsService) stopConsumer(ctx context.Context, wg *sync.WaitGroup, con
 			// delete nats consumer instance from memory
 			ls.consumerInstances.Delete(consumer.Name)
 			l.Infow("stopping and removing consumer", "name", consumer.Name, "consumerSeq", info.Delivered.Consumer, "streamSeq", info.Delivered.Stream, "last", info.Delivered.Last)
+
+			// call adapter stop to handle given id
+			err := adapter.Stop(id)
+			if err != nil {
+				l.Errorw("stop error", "adapter", adapter.Name(), "error", err)
+				continue
+			}
+
 			return
 		}
 
