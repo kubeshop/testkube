7499aadf806942e06fd822133c145e18c8e0696b
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-01-30 14:51:04 +0100

    fix: dummy adapter use structured logging (#4957)
    * fix: dummy adapter use structured logging
    
    * fix: golang ci fixes

---
+++ pkg/logs/adapter/dummy.go
@@ -0,0 +1,35 @@
+package adapter
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/kubeshop/testkube/pkg/log"
+	"github.com/kubeshop/testkube/pkg/logs/events"
+)
+
+var _ Adapter = &DebugAdapter{}
+
+// NewDebugAdapter creates new DebugAdapter which will write logs to stdout
+func NewDebugAdapter() *DebugAdapter {
+	return &DebugAdapter{
+		l: log.DefaultLogger,
+	}
+}
+
+type DebugAdapter struct {
+	l *zap.SugaredLogger
+}
+
+func (s *DebugAdapter) Notify(id string, e events.Log) error {
+	s.l.Debugw("got event", "id", id, "event", e)
+	return nil
+}
+
+func (s *DebugAdapter) Stop(id string) error {
+	s.l.Debugw("Stopping", "id", id)
+	return nil
+}
+
+func (s *DebugAdapter) Name() string {
+	return "dummy"
+}
