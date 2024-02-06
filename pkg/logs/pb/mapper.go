3643c8f4d15334250c80797fe5c8972f23e31646
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-02-06 08:38:38 +0100

    fix: renamed mapper

--- pkg/logs/pb/mapper.go
+++ pkg/logs/pb/mapper.go
@@ -8,32 +8,32 @@ import (
 
 // TODO figure out how to pass errors better
 func MapResponseToPB(r events.LogResponse) *Log {
-	chunk := r.Log
-	content := chunk.Content
+	log := r.Log
+	content := log.Content
 	isError := false
 	if r.Error != nil {
 		content = r.Error.Error()
 		isError = true
 	}
 	return &Log{
-		Time:     timestamppb.New(chunk.Time),
+		Time:     timestamppb.New(log.Time),
 		Content:  content,
 		Error:    isError,
-		Type:     chunk.Type,
-		Source:   chunk.Source,
-		Metadata: chunk.Metadata,
-		Version:  string(chunk.Version),
+		Type:     log.Type,
+		Source:   log.Source,
+		Metadata: log.Metadata,
+		Version:  string(log.Version),
 	}
 }
 
-func MapFromPB(l *Log) events.Log {
+func MapFromPB(log *Log) events.Log {
 	return events.Log{
-		Time:     l.Time.AsTime(),
-		Content:  l.Content,
-		Error:    l.Error,
-		Type:     l.Type,
-		Source:   l.Source,
-		Metadata: l.Metadata,
-		Version:  events.LogVersion(l.Version),
+		Time:     log.Time.AsTime(),
+		Content:  log.Content,
+		Error:    log.Error,
+		Type:     log.Type,
+		Source:   log.Source,
+		Metadata: log.Metadata,
+		Version:  events.LogVersion(log.Version),
 	}
 }
