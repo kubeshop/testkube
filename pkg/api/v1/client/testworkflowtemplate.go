23f282a18c7b12d8f0000c73f07668f2a8c13f6f
Dawid Rusnak <dawid@drcode.pl>
2024-02-21 13:41:07 +0100

    feat(TKC-1458): adjust TestWorkflowTemplate client to reformat input template name

--- pkg/api/v1/client/testworkflowtemplate.go
+++ pkg/api/v1/client/testworkflowtemplate.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 )
@@ -24,14 +25,15 @@ type TestWorkflowTemplateClient struct {
 
 // GetTestWorkflowTemplate returns single test by id
 func (c TestWorkflowTemplateClient) GetTestWorkflowTemplate(id string) (testkube.TestWorkflowTemplate, error) {
+	id = strings.ReplaceAll(id, "/", "--")
 	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates/%s", id)
 	return c.testWorkflowTemplateTransport.Execute(http.MethodGet, uri, nil, nil)
 }
 
 // ListTestWorkflowTemplates list all tests
 func (c TestWorkflowTemplateClient) ListTestWorkflowTemplates(selector string) (testkube.TestWorkflowTemplates, error) {
-	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates")
 	params := map[string]string{"selector": selector}
+	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates")
 	return c.testWorkflowTemplateTransport.ExecuteMultiple(http.MethodGet, uri, nil, params)
 }
 
@@ -43,13 +45,14 @@ func (c TestWorkflowTemplateClient) DeleteTestWorkflowTemplates(selector string)
 
 // CreateTestWorkflowTemplate creates new TestWorkflowTemplate Custom Resource
 func (c TestWorkflowTemplateClient) CreateTestWorkflowTemplate(template testkube.TestWorkflowTemplate) (result testkube.TestWorkflowTemplate, err error) {
-	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates")
+	template.Name = strings.ReplaceAll(template.Name, "/", "--")
 
 	body, err := json.Marshal(template)
 	if err != nil {
 		return result, err
 	}
 
+	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates")
 	return c.testWorkflowTemplateTransport.Execute(http.MethodPost, uri, body, nil)
 }
 
@@ -58,14 +61,14 @@ func (c TestWorkflowTemplateClient) UpdateTestWorkflowTemplate(template testkube
 	if template.Name == "" {
 		return result, fmt.Errorf("test workflow template name '%s' is not valid", template.Name)
 	}
-
-	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates/%s", template.Name)
+	template.Name = strings.ReplaceAll(template.Name, "/", "--")
 
 	body, err := json.Marshal(template)
 	if err != nil {
 		return result, err
 	}
 
+	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates/%s", template.Name)
 	return c.testWorkflowTemplateTransport.Execute(http.MethodPut, uri, body, nil)
 }
 
@@ -74,6 +77,7 @@ func (c TestWorkflowTemplateClient) DeleteTestWorkflowTemplate(name string) erro
 	if name == "" {
 		return fmt.Errorf("test workflow template name '%s' is not valid", name)
 	}
+	name = strings.ReplaceAll(name, "/", "--")
 
 	uri := c.testWorkflowTemplateTransport.GetURI("/test-workflow-templates/%s", name)
 	return c.testWorkflowTemplateTransport.Delete(uri, "", true)
