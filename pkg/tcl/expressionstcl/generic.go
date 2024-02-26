2dbeb5220d2a5eb9eba2ba2ce0a075d011642723
Dawid Rusnak <dawid@drcode.pl>
2024-02-26 18:18:19 +0100

    feat(TKC-1465): add mechanism to allow to resolve expressions in the objects via tags

---
+++ pkg/tcl/expressionstcl/generic.go
@@ -0,0 +1,167 @@
+// Copyright 2024 Testkube.
+//
+// Licensed as a Testkube Pro file under the Testkube Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/kubeshop/testkube/blob/main/licenses/TCL.txt
+
+package expressionstcl
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+type tagData struct {
+	key   string
+	value string
+}
+
+func parseTag(tag string) tagData {
+	s := strings.Split(tag, ",")
+	if len(s) > 1 {
+		return tagData{key: s[0], value: s[1]}
+	}
+	return tagData{value: s[0]}
+}
+
+var unrecognizedErr = errors.New("unsupported value passed for resolving expressions")
+
+func clone(v reflect.Value) reflect.Value {
+	if v.Kind() == reflect.String {
+		s := v.String()
+		return reflect.ValueOf(&s).Elem()
+	} else if v.Kind() == reflect.Struct {
+		r := reflect.New(v.Type()).Elem()
+		for i := 0; i < r.NumField(); i++ {
+			r.Field(i).Set(v.Field(i))
+		}
+		return r
+	}
+	return v
+}
+
+func resolve(v reflect.Value, t tagData, m []MachineCore) (err error) {
+	if t.key == "" && t.value == "" {
+		return
+	}
+
+	ptr := v
+	for v.Kind() == reflect.Pointer || v.Kind() == reflect.Interface {
+		if v.IsNil() {
+			return
+		}
+		ptr = v
+		v = v.Elem()
+	}
+
+	if v.IsZero() || !v.IsValid() || (v.Kind() == reflect.Slice || v.Kind() == reflect.Map) && v.IsNil() {
+		return
+	}
+
+	switch v.Kind() {
+	case reflect.Struct:
+		// TODO: Cache the tags for structs for better performance
+		vv, ok := v.Interface().(intstr.IntOrString)
+		if ok {
+			if vv.Type == intstr.String {
+				return resolve(v.FieldByName("StrVal"), t, m)
+			}
+		} else if t.value == "include" {
+			tt := v.Type()
+			for i := 0; i < tt.NumField(); i++ {
+				f := tt.Field(i)
+				tag := parseTag(f.Tag.Get("expr"))
+				value := v.FieldByName(f.Name)
+				err = resolve(value, tag, m)
+				if err != nil {
+					return errors.Wrap(err, f.Name)
+				}
+			}
+		}
+		return
+	case reflect.Slice:
+		if t.value == "" {
+			return nil
+		}
+		for i := 0; i < v.Len(); i++ {
+			err := resolve(v.Index(i), t, m)
+			if err != nil {
+				return errors.Wrap(err, fmt.Sprintf("%d", i))
+			}
+		}
+		return
+	case reflect.Map:
+		if t.value == "" && t.key == "" {
+			return nil
+		}
+		for _, k := range v.MapKeys() {
+			if t.value != "" {
+				// It's not possible to get a pointer to map element,
+				// so we need to copy it and reassign
+				item := clone(v.MapIndex(k))
+				err = resolve(item, t, m)
+				v.SetMapIndex(k, item)
+				if err != nil {
+					return errors.Wrap(err, k.String())
+				}
+			}
+			if t.key != "" {
+				key := clone(k)
+				err = resolve(key, tagData{value: t.key}, m)
+				if !key.Equal(k) {
+					item := clone(v.MapIndex(k))
+					v.SetMapIndex(k, reflect.Value{})
+					v.SetMapIndex(key, item)
+				}
+				if err != nil {
+					return errors.Wrap(err, "key("+k.String()+")")
+				}
+			}
+		}
+		return
+	case reflect.String:
+		if t.value == "expression" {
+			var expr Expression
+			expr, err = CompileAndResolve(v.String(), m...)
+			if err != nil {
+				return err
+			}
+			vv := expr.String()
+			if ptr.Kind() == reflect.String {
+				v.SetString(vv)
+			} else {
+				ptr.Set(reflect.ValueOf(&vv))
+			}
+		} else if t.value == "template" {
+			var expr Expression
+			expr, err = CompileAndResolveTemplate(v.String(), m...)
+			if err != nil {
+				return err
+			}
+			vv := expr.Template()
+			if ptr.Kind() == reflect.String {
+				v.SetString(vv)
+			} else {
+				ptr.Set(reflect.ValueOf(&vv))
+			}
+		}
+		return
+	}
+
+	// Fail for unrecognized values
+	return unrecognizedErr
+}
+
+func Resolve(t interface{}, m ...MachineCore) error {
+	v := reflect.ValueOf(t)
+	if v.Kind() != reflect.Pointer {
+		return errors.New("pointer needs to be passed to Resolve function")
+	}
+	return resolve(v, tagData{value: "include"}, m)
+}
---
+++ pkg/tcl/expressionstcl/generic_test.go
@@ -0,0 +1,150 @@
+// Copyright 2024 Testkube.
+//
+// Licensed as a Testkube Pro file under the Testkube Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     https://github.com/kubeshop/testkube/blob/main/licenses/TCL.txt
+
+package expressionstcl
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"github.com/kubeshop/testkube/internal/common"
+)
+
+type testObj2 struct {
+	Expr  string `expr:"expression"`
+	Dummy string
+}
+
+type testObj struct {
+	Expr            string                        `expr:"expression"`
+	Tmpl            string                        `expr:"template"`
+	ExprPtr         *string                       `expr:"expression"`
+	TmplPtr         *string                       `expr:"template"`
+	IntExpr         intstr.IntOrString            `expr:"expression"`
+	IntTmpl         intstr.IntOrString            `expr:"template"`
+	IntExprPtr      *intstr.IntOrString           `expr:"expression"`
+	IntTmplPtr      *intstr.IntOrString           `expr:"template"`
+	Obj             testObj2                      `expr:"include"`
+	ObjPtr          *testObj2                     `expr:"include"`
+	SliceExprStr    []string                      `expr:"expression"`
+	SliceExprStrPtr *[]string                     `expr:"expression"`
+	SliceExprObj    []testObj2                    `expr:"include"`
+	MapKeyVal       map[string]string             `expr:"template,template"`
+	MapValIntTmpl   map[string]intstr.IntOrString `expr:"template"`
+	MapKeyTmpl      map[string]string             `expr:"template,"`
+	MapValTmpl      map[string]string             `expr:"template"`
+	MapTmplExpr     map[string]string             `expr:"template,expression"`
+	Dummy           string
+	DummyPtr        *string
+	DummyObj        testObj2
+	DummyObjPtr     *testObj2
+}
+
+var testMachine = NewMachine().
+	Register("dummy", "test").
+	Register("ten", 10)
+
+func TestGenericString(t *testing.T) {
+	obj := testObj{
+		Expr:     "5 + 3 + ten",
+		Tmpl:     "{{ 10 + 3 }}{{ ten }}",
+		ExprPtr:  common.Ptr("1 + 2 + ten"),
+		TmplPtr:  common.Ptr("{{ 4 + 3 }}{{ ten }}"),
+		Dummy:    "5 + 3 + ten",
+		DummyPtr: common.Ptr("5 + 3 + ten"),
+	}
+	err := Resolve(&obj, testMachine)
+	assert.NoError(t, err)
+	assert.Equal(t, "18", obj.Expr)
+	assert.Equal(t, "1310", obj.Tmpl)
+	assert.Equal(t, common.Ptr("13"), obj.ExprPtr)
+	assert.Equal(t, common.Ptr("710"), obj.TmplPtr)
+	assert.Equal(t, "5 + 3 + ten", obj.Dummy)
+	assert.Equal(t, common.Ptr("5 + 3 + ten"), obj.DummyPtr)
+}
+
+func TestGenericIntOrString(t *testing.T) {
+	obj := testObj{
+		IntExpr:    intstr.IntOrString{Type: intstr.String, StrVal: "5 + 3 + ten"},
+		IntTmpl:    intstr.IntOrString{Type: intstr.String, StrVal: "{{ 10 + 3 }}{{ ten }}"},
+		IntExprPtr: &intstr.IntOrString{Type: intstr.String, StrVal: "1 + 2 + ten"},
+		IntTmplPtr: &intstr.IntOrString{Type: intstr.String, StrVal: "{{ 4 + 3 }}{{ ten }}"},
+	}
+	err := Resolve(&obj, testMachine)
+	assert.NoError(t, err)
+	assert.Equal(t, "18", obj.IntExpr.String())
+	assert.Equal(t, "1310", obj.IntTmpl.String())
+	assert.Equal(t, "13", obj.IntExprPtr.String())
+	assert.Equal(t, "710", obj.IntTmplPtr.String())
+}
+
+func TestGenericSlice(t *testing.T) {
+	obj := testObj{
+		SliceExprStr:    []string{"200 + 100", "100 + 200", "ten", "abc"},
+		SliceExprStrPtr: &[]string{"200 + 100", "100 + 200", "ten", "abc"},
+		SliceExprObj:    []testObj2{{Expr: "10 + 5", Dummy: "3 + 2"}},
+	}
+	err := Resolve(&obj, testMachine)
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"300", "300", "10", "abc"}, obj.SliceExprStr)
+	assert.Equal(t, &[]string{"300", "300", "10", "abc"}, obj.SliceExprStrPtr)
+	assert.Equal(t, []testObj2{{Expr: "15", Dummy: "3 + 2"}}, obj.SliceExprObj)
+}
+
+func TestGenericMap(t *testing.T) {
+	obj := testObj{
+		MapKeyVal:     map[string]string{"{{ 10 + 3 }}2": "{{ 3 + 5 }}"},
+		MapKeyTmpl:    map[string]string{"{{ 10 + 3 }}2": "{{ 3 + 5 }}"},
+		MapValTmpl:    map[string]string{"{{ 10 + 3 }}2": "{{ 3 + 5 }}"},
+		MapValIntTmpl: map[string]intstr.IntOrString{"{{ 10 + 3 }}2": {Type: intstr.String, StrVal: "{{ 3 + 5 }}"}},
+		MapTmplExpr:   map[string]string{"{{ 10 + 3 }}2": "3 + 5"},
+	}
+	err := Resolve(&obj, testMachine)
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]string{"132": "8"}, obj.MapKeyVal)
+	assert.Equal(t, map[string]string{"132": "{{ 3 + 5 }}"}, obj.MapKeyTmpl)
+	assert.Equal(t, map[string]string{"{{ 10 + 3 }}2": "8"}, obj.MapValTmpl)
+	assert.Equal(t, map[string]intstr.IntOrString{"{{ 10 + 3 }}2": {Type: intstr.String, StrVal: "8"}}, obj.MapValIntTmpl)
+	assert.Equal(t, map[string]string{"132": "8"}, obj.MapTmplExpr)
+}
+
+func TestNestedObject(t *testing.T) {
+	obj := testObj{
+		Obj:         testObj2{Expr: "10 + 5", Dummy: "3 + 2"},
+		ObjPtr:      &testObj2{Expr: "10 + 8", Dummy: "33 + 2"},
+		DummyObj:    testObj2{Expr: "10 + 8", Dummy: "333 + 2"},
+		DummyObjPtr: &testObj2{Expr: "10 + 8", Dummy: "3333 + 2"},
+	}
+	err := Resolve(&obj, testMachine)
+	assert.NoError(t, err)
+	assert.Equal(t, testObj2{Expr: "15", Dummy: "3 + 2"}, obj.Obj)
+	assert.Equal(t, &testObj2{Expr: "18", Dummy: "33 + 2"}, obj.ObjPtr)
+	assert.Equal(t, testObj2{Expr: "10 + 8", Dummy: "333 + 2"}, obj.DummyObj)
+	assert.Equal(t, &testObj2{Expr: "10 + 8", Dummy: "3333 + 2"}, obj.DummyObjPtr)
+}
+
+func TestGenericNotMutateStringPointer(t *testing.T) {
+	ptr := common.Ptr("200 + 10")
+	obj := testObj{
+		ExprPtr: ptr,
+	}
+	_ = Resolve(&obj, testMachine)
+	assert.Equal(t, common.Ptr("200 + 10"), ptr)
+}
+
+func TestGenericCompileError(t *testing.T) {
+	got := testObj{
+		Tmpl: "{{ 1 + 2 }}{{ 3",
+	}
+	err := Resolve(&got)
+
+	assert.Contains(t, fmt.Sprintf("%v", err), "Tmpl: template error")
+}
