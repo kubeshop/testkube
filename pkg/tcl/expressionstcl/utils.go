855086ab7087367770af232ee4706663ff4e80a6
Dawid Rusnak <dawid@drcode.pl>
2024-02-27 16:04:44 +0100

    feat(TKC-1465): add helpers to immediately evaluate Template/Expression

--- pkg/tcl/expressionstcl/utils.go
+++ pkg/tcl/expressionstcl/utils.go
@@ -10,6 +10,8 @@ package expressionstcl
 
 import (
 	"fmt"
+
+	"github.com/pkg/errors"
 )
 
 const maxCallStack = 10_000
@@ -25,4 +27,34 @@ func deepResolve(expr Expression, machines ...Machine) (Expression, error) {
 		i++
 	}
 	return expr, err
+}
+
+func EvalTemplate(tpl string, machines ...Machine) (string, error) {
+	expr, err := CompileTemplate(tpl)
+	if err != nil {
+		return "", errors.Wrap(err, "compiling")
+	}
+	expr, err = expr.Resolve(machines...)
+	if err != nil {
+		return "", errors.Wrap(err, "resolving")
+	}
+	if expr.Static() != nil {
+		return "", fmt.Errorf("template should be static: %s", expr.Template())
+	}
+	return expr.String(), nil
+}
+
+func EvalExpression(str string, machines ...Machine) (StaticValue, error) {
+	expr, err := Compile(str)
+	if err != nil {
+		return nil, errors.Wrap(err, "compiling")
+	}
+	expr, err = expr.Resolve(machines...)
+	if err != nil {
+		return nil, errors.Wrap(err, "resolving")
+	}
+	if expr.Static() != nil {
+		return nil, fmt.Errorf("expression should be static: %s", expr.String())
+	}
+	return expr.Static(), nil
 }
