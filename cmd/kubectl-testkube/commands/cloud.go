be290542abd340acad16e51a599d4e85ee99caa2
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-03-13 17:03:52 +0100

    chore: removed duplicated cloud-pro code

--- cmd/kubectl-testkube/commands/cloud.go
+++ cmd/kubectl-testkube/commands/cloud.go
@@ -3,7 +3,7 @@ package commands
 import (
 	"github.com/spf13/cobra"
 
-	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/cloud"
+	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/pro"
 	"github.com/kubeshop/testkube/pkg/ui"
 )
 
@@ -15,15 +15,15 @@ func NewCloudCmd() *cobra.Command {
 		Hidden:     true,
 		Short:      "[Deprecated] Testkube Cloud commands",
 		Aliases:    []string{"cl"},
-		Run: func(cmd *cobra.Command, args []string) {
-			ui.Warn("You are using a deprecated command, please switch to `testkube pro`.")
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			ui.Errf("You are using a deprecated command, please switch to `testkube pro` prefix.\n\n")
 		},
 	}
 
-	cmd.AddCommand(cloud.NewConnectCmd())
-	cmd.AddCommand(cloud.NewDisconnectCmd())
-	cmd.AddCommand(cloud.NewInitCmd())
-	cmd.AddCommand(cloud.NewLoginCmd())
+	cmd.AddCommand(pro.NewConnectCmd())
+	cmd.AddCommand(pro.NewDisconnectCmd())
+	cmd.AddCommand(pro.NewInitCmd())
+	cmd.AddCommand(pro.NewLoginCmd())
 
 	return cmd
 }
