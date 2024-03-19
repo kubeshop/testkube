24d6da238d44961d5db28b936e8728c5164184cc
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-03-19 08:56:37 +0100

    feat: added logs service params for cloud (#5208)
    * feat: added logs service params for cloud
    
    * fix added feature flag for logs v2

--- cmd/kubectl-testkube/config/master.go
+++ cmd/kubectl-testkube/config/master.go
@@ -1,15 +1,19 @@
 package config
 
+import "github.com/kubeshop/testkube/pkg/featureflags"
+
 type Master struct {
-	AgentToken     string `json:"agentToken,omitempty"`
-	IdToken        string `json:"idToken,omitempty"`
-	OrgId          string `json:"orgId,omitempty"`
-	EnvId          string `json:"envId,omitempty"`
-	Insecure       bool   `json:"insecure,omitempty"`
-	UiUrlPrefix    string `json:"uiUrlPrefix,omitempty"`
-	AgentUrlPrefix string `json:"agentUrlPrefix,omitempty"`
-	ApiUrlPrefix   string `json:"apiUrlPrefix,omitempty"`
-	RootDomain     string `json:"rootDomain,omitempty"`
+	AgentToken     string                    `json:"agentToken,omitempty"`
+	IdToken        string                    `json:"idToken,omitempty"`
+	OrgId          string                    `json:"orgId,omitempty"`
+	EnvId          string                    `json:"envId,omitempty"`
+	Insecure       bool                      `json:"insecure,omitempty"`
+	UiUrlPrefix    string                    `json:"uiUrlPrefix,omitempty"`
+	AgentUrlPrefix string                    `json:"agentUrlPrefix,omitempty"`
+	LogsUrlPrefix  string                    `json:"logsUrlPrefix,omitempty"`
+	ApiUrlPrefix   string                    `json:"apiUrlPrefix,omitempty"`
+	RootDomain     string                    `json:"rootDomain,omitempty"`
+	Features       featureflags.FeatureFlags `json:"features,omitempty"`
 
 	URIs MasterURIs `json:"uris,omitempty"`
 }
@@ -20,6 +24,7 @@ type MasterURIs struct {
 	RootDomain string `json:"rootDomain,omitempty"`
 	Api        string `json:"api,omitempty"`
 	Agent      string `json:"agent,omitempty"`
+	Logs       string `json:"logs,omitempty"`
 	Ui         string `json:"ui,omitempty"`
 	Auth       string `json:"auth,omitempty"`
 }
