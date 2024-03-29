e450525e2deaa2266aa3015a59d06a9020203efb
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-03-29 10:11:15 +0100

    fix: new MinIO adapter file based

--- pkg/logs/config/logs_config.go
+++ pkg/logs/config/logs_config.go
@@ -56,6 +56,7 @@ type Config struct {
 	StorageCertFile        string `envconfig:"STORAGE_CERT_FILE" default:""`
 	StorageKeyFile         string `envconfig:"STORAGE_KEY_FILE" default:""`
 	StorageCAFile          string `envconfig:"STORAGE_CA_FILE" default:""`
+	StorageFilePath        string `envconfig:"STORAGE_FILE_PATH" default:"/data"`
 }
 
 func Get() (*Config, error) {
