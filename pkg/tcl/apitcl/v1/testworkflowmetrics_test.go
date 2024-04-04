e9a038c2ee367311d05b8af40bc45e678ad5a6db
Lilla Vass <lilla@kubeshop.io>
2024-04-04 14:52:05 +0300

    ci: fix linter

--- pkg/tcl/apitcl/v1/testworkflowmetrics_test.go
+++ pkg/tcl/apitcl/v1/testworkflowmetrics_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+
 	testworkflowsv1 "github.com/kubeshop/testkube-operator/api/testworkflows/v1"
 	configRepo "github.com/kubeshop/testkube/pkg/repository/config"
 )
