2e25e1941be2c771ddf755e1cfa58e64adbae5dd
Vladislav Sukhin <vladislav@kubeshop.io>
2024-04-17 20:20:25 +0400

    feat: [TKC-1841] test workflow cron job (#5296)
    * feat: test workflow event model
    
    * fix: dep update
    
    * feat: mapping fo workflow con job
    
    * docs: workflow schedule

--- pkg/tcl/mapperstcl/testworkflows/kube_openapi.go
+++ pkg/tcl/mapperstcl/testworkflows/kube_openapi.go
@@ -444,6 +444,20 @@ func MapJobConfigKubeToAPI(v testworkflowsv1.JobConfig) testkube.TestWorkflowJob
 	}
 }
 
+func MapEventKubeToAPI(v testworkflowsv1.Event) testkube.TestWorkflowEvent {
+	return testkube.TestWorkflowEvent{
+		Cronjob: common.MapPtr(v.Cronjob, MapCronJobConfigKubeToAPI),
+	}
+}
+
+func MapCronJobConfigKubeToAPI(v testworkflowsv1.CronJobConfig) testkube.TestWorkflowCronJobConfig {
+	return testkube.TestWorkflowCronJobConfig{
+		Cron:        v.Cron,
+		Labels:      v.Labels,
+		Annotations: v.Annotations,
+	}
+}
+
 func MapPodConfigKubeToAPI(v testworkflowsv1.PodConfig) testkube.TestWorkflowPodConfig {
 	return testkube.TestWorkflowPodConfig{
 		ServiceAccountName: v.ServiceAccountName,
@@ -684,6 +698,7 @@ func MapSpecKubeToAPI(v testworkflowsv1.TestWorkflowSpec) testkube.TestWorkflowS
 		Setup:     common.MapSlice(v.Setup, MapStepKubeToAPI),
 		Steps:     common.MapSlice(v.Steps, MapStepKubeToAPI),
 		After:     common.MapSlice(v.After, MapStepKubeToAPI),
+		Events:    common.MapSlice(v.Events, MapEventKubeToAPI),
 	}
 }
 
@@ -697,6 +712,7 @@ func MapTemplateSpecKubeToAPI(v testworkflowsv1.TestWorkflowTemplateSpec) testku
 		Setup:     common.MapSlice(v.Setup, MapIndependentStepKubeToAPI),
 		Steps:     common.MapSlice(v.Steps, MapIndependentStepKubeToAPI),
 		After:     common.MapSlice(v.After, MapIndependentStepKubeToAPI),
+		Events:    common.MapSlice(v.Events, MapEventKubeToAPI),
 	}
 }
 
