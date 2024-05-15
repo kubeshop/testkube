98b8f4aebb9e3922044a1f171caf9f12e4c429e3
Emil Davtyan <emil@kubeshop.io>
2024-05-15 13:32:08 +0200

    docs: hold application

--- internal/config/config.go
+++ internal/config/config.go
@@ -106,9 +106,15 @@ type Config struct {
 	GlobalWorkflowTemplateName                  string        `envconfig:"TESTKUBE_GLOBAL_WORKFLOW_TEMPLATE_NAME" default:""`
 	// IstioProxyWait triggers logic in the test runner jobs to wait for Istio'
 	// Envoy proxy to become ready.
-	// This should only be necessary with Istio versions before 1.7, after
-	// which one should use either the global (1.7) or pod-level (1.8)
-	// holdApplicationUntilProxyStarts setting.
+	// This should not be necessery in environments that support native
+	// sidecars (Kubernetes 1.28+ and Istio 1.19+):
+	// https://istio.io/latest/blog/2023/native-sidecars/
+	// This is necessary with Istio versions before 1.7, after which one could
+	// choose to use either the global (1.7) or pod-level (1.8)
+	// holdApplicationUntilProxyStarts setting. However, when using this
+	// setting the default container for kubectl commands such as logs
+	// and exec changes to the Envoy proxy which could be undesirable for some
+	// installations especially when set globally.
 	IstioProxyWait bool `envconfig:"ISTIO_PROXY_WAIT" default:"false"`
 	// IstioProxyExit triggers logic in the test runner jobs that sends an exit
 	// signal to Istio's Envoy proxy sidecar, otherwise, the proxy never exits.
