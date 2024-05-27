3aa25eb577402fb898a8df67fb516ef2dbea5898
Vladislav Sukhin <vladislav@kubeshop.io>
2024-05-27 16:42:39 +0300

    fix: dep update

--- pkg/tcl/mapperstcl/testworkflows/openapi_kube.go
+++ pkg/tcl/mapperstcl/testworkflows/openapi_kube.go
@@ -1384,8 +1384,8 @@ func MapTestWorkflowExecutionAPIToKube(v *testkube.TestWorkflowExecution) *testw
 	}
 }
 
-func MapTestWorkflowExecutionStatusAPIToKube(v *testkube.TestWorkflowExecution, generation int64) *testworkflowsv1.TestWorkflowExecutionStatus {
-	return &testworkflowsv1.TestWorkflowExecutionStatus{
+func MapTestWorkflowExecutionStatusAPIToKube(v *testkube.TestWorkflowExecution, generation int64) testworkflowsv1.TestWorkflowExecutionStatus {
+	return testworkflowsv1.TestWorkflowExecutionStatus{
 		LatestExecution: MapTestWorkflowExecutionAPIToKube(v),
 		Generation:      generation,
 	}
