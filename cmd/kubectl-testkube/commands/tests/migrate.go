bb55fbe38cfecf5f1ea2a11f941d7c368244da08
Vladislav Sukhin <vladislav@kubeshop.io>
2024-06-05 20:02:38 +0300

    fix: print test workflow and template crds

--- cmd/kubectl-testkube/commands/tests/migrate.go
+++ cmd/kubectl-testkube/commands/tests/migrate.go
@@ -1,17 +1,25 @@
 package tests
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 
-	testworkflowsv1 "github.com/kubeshop/testkube-operator/api/testworkflows/v1"
 	"github.com/kubeshop/testkube/cmd/kubectl-testkube/commands/common"
-	internalcommon "github.com/kubeshop/testkube/internal/common"
-	testsmapper "github.com/kubeshop/testkube/pkg/mapper/tests"
-	testworkflowmappers "github.com/kubeshop/testkube/pkg/tcl/mapperstcl/testworkflows"
+	"github.com/kubeshop/testkube/pkg/api/v1/client"
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/kubeshop/testkube/pkg/ui"
 )
 
+var (
+	printedExecutors = make(map[string]struct{})
+)
+
 func NewMigrateTestsCmd() *cobra.Command {
+	var (
+		migrateExecutors bool
+	)
+
 	cmd := &cobra.Command{
 		Use:     "test <testName>",
 		Aliases: []string{"tests", "t"},
@@ -22,43 +30,56 @@ func NewMigrateTestsCmd() *cobra.Command {
 			client, _, err := common.GetClient(cmd)
 			ui.ExitOnError("getting client", err)
 
-			var name string
 			if len(args) > 0 {
-				name = args[0]
-				test, err := client.GetTest(name)
+				test, err := client.GetTest(args[0])
 				ui.ExitOnError("getting test in namespace "+namespace, err)
 
-				ui.NL()
-				ui.Info("Test workflow:")
-				testCR := testsmapper.MapTestAPIToCR(test)
-				testWorkflow := testworkflowmappers.MapTestKubeToTestWorkflowKube(testCR, "")
-				b, err := internalcommon.SerializeCRDs([]testworkflowsv1.TestWorkflow{testWorkflow}, internalcommon.SerializeOptions{
-					OmitCreationTimestamp: true,
-					CleanMeta:             true,
-					Kind:                  testworkflowsv1.Resource,
-					GroupVersion:          &testworkflowsv1.GroupVersion,
-				})
-				ui.ExitOnError("serializing obj", err)
-				ui.Info(string(b))
+				templateName := printExecutors(client, namespace, test, migrateExecutors)
+				common.PrintTestWorkflowCRDForTest(test, templateName)
 			} else {
 				tests, err := client.ListTests("")
 				ui.ExitOnError("getting all tests in namespace "+namespace, err)
 
-				for _, test := range tests {
-					testCR := testsmapper.MapTestAPIToCR(test)
-					testWorkflow := testworkflowmappers.MapTestKubeToTestWorkflowKube(testCR, "")
-					b, err := internalcommon.SerializeCRDs([]testworkflowsv1.TestWorkflow{testWorkflow}, internalcommon.SerializeOptions{
-						OmitCreationTimestamp: true,
-						CleanMeta:             true,
-						Kind:                  testworkflowsv1.Resource,
-						GroupVersion:          &testworkflowsv1.GroupVersion,
-					})
-					ui.ExitOnError("serializing obj", err)
-					ui.Info(string(b))
+				for i, test := range tests {
+					templateName := printExecutors(client, namespace, test, migrateExecutors)
+					common.PrintTestWorkflowCRDForTest(test, templateName)
+					if i != len(tests)-1 {
+						fmt.Printf("\n---\n\n")
+					}
 				}
 			}
 		},
 	}
 
+	cmd.Flags().BoolVar(&migrateExecutors, "migrate-executors", true, "migrate executors for tests")
+
 	return cmd
+}
+
+func printExecutors(client client.Client, namespace string, test testkube.Test, migrateExecutors bool) string {
+	executors, err := client.ListExecutors("")
+	ui.ExitOnError("getting all tests in namespace "+namespace, err)
+
+	executorTypes := make(map[string]testkube.ExecutorDetails)
+	for _, executor := range executors {
+		for _, executorType := range executor.Executor.Types {
+			executorTypes[executorType] = executor
+		}
+	}
+
+	templateName := ""
+	if executor, ok := executorTypes[test.Type_]; ok {
+		templateName = executor.Name
+		if official, ok := common.OfficialTestWorkflowTemplates[templateName]; !ok {
+			if _, ok = printedExecutors[templateName]; !ok && migrateExecutors {
+				common.PrintTestWorkflowTemplateCRDForExecutor(executor)
+				fmt.Printf("\n---\n\n")
+				printedExecutors[templateName] = struct{}{}
+			}
+		} else {
+			templateName = official
+		}
+	}
+
+	return templateName
 }
