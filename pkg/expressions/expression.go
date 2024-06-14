63c2b2793598cebdc36abfa4f81ce19104d9f70e
Dawid Rusnak <dawid@drcode.pl>
2024-06-14 12:49:28 +0200

    feat(expressions): delete TCL header from TestWorkflow expressions

--- pkg/expressions/expression.go
+++ pkg/expressions/expression.go
@@ -1,11 +1,3 @@
-// Copyright 2024 Testkube.
-//
-// Licensed as a Testkube Pro file under the Testkube Community
-// License (the "License"); you may not use this file except in compliance with
-// the License. You may obtain a copy of the License at
-//
-//     https://github.com/kubeshop/testkube/blob/main/licenses/TCL.txt
-
 package expressions
 
 //go:generate mockgen -destination=./mock_expression.go -package=expressions "github.com/kubeshop/testkube/pkg/expressions" Expression
