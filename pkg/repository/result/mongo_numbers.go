03d0755e011592e884bf0242eac8d8a077cd5342
Vladislav Sukhin <vladislav@kubeshop.io>
2024-07-22 21:38:36 +0300

    fix: change sequence for numbers
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/repository/result/mongo_numbers.go
+++ pkg/repository/result/mongo_numbers.go
@@ -2,126 +2,128 @@ package result
 
 import (
 	"context"
-	"strings"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 )
 
 type oldExecutionNumber struct {
-	TestName string `json:"testName"`
-	Number   int    `json:"number"`
-}
-
-type executionNumber struct {
 	Name        string `json:"name"`
 	Number      int    `json:"number"`
 	IsTestSuite bool   `json:"isTestSuite"`
 }
 
-func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, name string) (number int32, err error) {
-	err = r.convertFromOldToNew()
+type executionNumber struct {
+	Id            string `bson:"_id"`
+	Number        int    `bson:"number"`
+	ExecutionType string `bson:"executionType"`
+}
+
+func (r *MongoRepository) GetNextExecutionNumber(ctx context.Context, name string, executionType string) (number int32, err error) {
+	oldName := getOldName(name, executionType)
+	number, err = r.getOldNumber(ctx, oldName)
 	if err != nil {
-		return 1, err
+		return 0, err
 	}
 
-	// TODO: modify this when we decide to update the interfaces for OSS and cloud
-	isTestSuite := strings.HasPrefix(name, "ts-")
-
-	execNmbr := executionNumber{Name: name, IsTestSuite: isTestSuite}
-	retry := false
-	retryAttempts := 0
-	maxRetries := 10
-
+	id := getMongoId(name, executionType)
+	executionNumber := executionNumber{
+		Id:            id,
+		Number:        int(number),
+		ExecutionType: executionType,
+	}
 	opts := options.FindOneAndUpdate()
 	opts.SetUpsert(true)
+
+	err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"_id": id}, bson.M{"$set": executionNumber}, opts).Err()
+	if err != nil && !mongo.IsDuplicateKeyError(err) {
+		return 0, err
+	}
+
+	if number != 0 {
+		_, err = r.SequencesColl.DeleteOne(ctx, bson.M{"name": oldName})
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	opts.SetUpsert(false)
 	opts.SetReturnDocument(options.After)
 
-	err = r.SequencesColl.FindOne(ctx, bson.M{"name": name}).Decode(&execNmbr)
-	if err != nil {
-		var execution testkube.Execution
-		number, _ = r.GetLatestTestNumber(ctx, name)
-		if number == 0 {
-			execNmbr.Number = 1
-		} else {
-			execNmbr.Number = int(execution.Number) + 1
-		}
-		_, err = r.SequencesColl.InsertOne(ctx, execNmbr)
-	} else {
-		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"name": name}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
+	err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"_id": id}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&executionNumber)
+	if err == nil {
+		return 0, err
 	}
 
-	retry = err != nil
-
-	for retry {
-		retryAttempts++
-		err = r.SequencesColl.FindOneAndUpdate(ctx, bson.M{"name": name}, bson.M{"$inc": bson.M{"number": 1}}, opts).Decode(&execNmbr)
-		if err == nil || retryAttempts >= maxRetries {
-			retry = false
-		}
-	}
-
-	return int32(execNmbr.Number), nil
+	return int32(executionNumber.Number), nil
 }
 
-func (r *MongoRepository) DeleteExecutionNumber(ctx context.Context, name string) (err error) {
-	err = r.convertFromOldToNew()
+func (r *MongoRepository) DeleteExecutionNumber(ctx context.Context, name string, executionType string) (err error) {
+	_, err = r.SequencesColl.DeleteOne(ctx, bson.M{"name": getOldName(name, executionType)})
 	if err != nil {
 		return err
 	}
-	_, err = r.SequencesColl.DeleteOne(ctx, bson.M{"name": name})
+
+	_, err = r.SequencesColl.DeleteOne(ctx, bson.M{"_id": getMongoId(name, executionType)})
 	return err
 }
 
-func (r *MongoRepository) DeleteExecutionNumbers(ctx context.Context, names []string) (err error) {
-	err = r.convertFromOldToNew()
+func (r *MongoRepository) DeleteExecutionNumbers(ctx context.Context, names []string, executionType string) (err error) {
+	ids := make([]string, len(names))
+	for i := range names {
+		ids[i] = getOldName(names[i], executionType)
+	}
+
+	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"name": bson.M{"$in": ids}})
 	if err != nil {
 		return err
 	}
-	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"name": bson.M{"$in": names}})
+
+	for i := range names {
+		ids[i] = getMongoId(names[i], executionType)
+	}
+
+	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"_id": bson.M{"$in": ids}})
 	return err
 }
 
-func (r *MongoRepository) DeleteAllExecutionNumbers(ctx context.Context, isTestSuite bool) (err error) {
-	err = r.convertFromOldToNew()
-	if err != nil {
-		return err
+func (r *MongoRepository) DeleteAllExecutionNumbers(ctx context.Context, executionType string) (err error) {
+	isTestSuite := false
+	if executionType == "testsuite" {
+		isTestSuite = true
 	}
+
 	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"istestsuite": isTestSuite})
-	return err
-}
-
-func (r *MongoRepository) convertFromOldToNew() error {
-	filter := bson.M{"testname": bson.M{"$exists": true}}
-
-	cursor, err := r.SequencesColl.Find(context.Background(), filter)
 	if err != nil {
 		return err
 	}
-	defer cursor.Close(context.Background())
 
-	for cursor.Next(context.Background()) {
-		var entry oldExecutionNumber
-		err := cursor.Decode(&entry)
-		if err != nil {
-			return err
-		}
-
-		isTestSuite := strings.HasPrefix(entry.TestName, "ts-")
-
-		newEntry := executionNumber{
-			Name:        entry.TestName,
-			Number:      entry.Number,
-			IsTestSuite: isTestSuite,
-		}
-
-		_, err = r.SequencesColl.InsertOne(context.Background(), newEntry)
-		if err != nil {
-			return err
-		}
-	}
-	_, err = r.SequencesColl.DeleteMany(context.Background(), filter)
+	_, err = r.SequencesColl.DeleteMany(ctx, bson.M{"executionType": executionType})
 	return err
+}
+
+func (r *MongoRepository) getOldNumber(ctx context.Context, name string) (int32, error) {
+	var executionNumber oldExecutionNumber
+
+	err := r.SequencesColl.FindOne(ctx, bson.M{"name": name}).Decode(&executionNumber)
+	if err != nil && err != mongo.ErrNoDocuments {
+		return 0, err
+	}
+
+	return int32(executionNumber.Number), nil
+}
+
+func getMongoId(name string, executionType string) string {
+	return fmt.Sprintf("%s-%s", name, executionType)
+}
+
+func getOldName(name string, executionType string) string {
+	oldPrefix := ""
+	if executionType == "testsuite" {
+		oldPrefix = "ts-"
+	}
+
+	return fmt.Sprintf("%s%s", oldPrefix, name)
 }
