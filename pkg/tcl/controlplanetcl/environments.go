83e0dc8be973d7a042054034cec6183e57386239
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-08-05 12:47:59 +0200

    feat: added env org details

---
+++ pkg/tcl/controlplanetcl/environments.go
@@ -0,0 +1,44 @@
+package controlplanetcl
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/kubeshop/testkube/internal/config"
+	"github.com/kubeshop/testkube/pkg/cloud"
+	cloudconfig "github.com/kubeshop/testkube/pkg/cloud/data/config"
+	"github.com/kubeshop/testkube/pkg/cloud/data/executor"
+	"google.golang.org/grpc"
+)
+
+func GetEnvironment(ctx context.Context, proContext config.ProContext, cloudClient cloud.TestKubeCloudAPIClient, grpcConn *grpc.ClientConn) (resp GetEnvironmentResponse, err error) {
+	executor := executor.NewCloudGRPCExecutor(cloudClient, grpcConn, proContext.APIKey, proContext.RunnerId)
+
+	req := GetEnvironmentRequest{}
+	respBytes, err := executor.Execute(ctx, cloudconfig.CmdConfigGetEnvironment, req)
+	if err != nil {
+		return GetEnvironmentResponse{}, err
+	}
+
+	if err := json.Unmarshal(respBytes, &resp); err != nil {
+		return GetEnvironmentResponse{}, err
+	}
+
+	return resp, nil
+}
+
+// GetEnvironmentRequest represents a request to get an environment by the token
+type GetEnvironmentRequest struct {
+	Token string
+}
+
+// GetEnvironmentResponse represents a response with env org data of connected runner
+type GetEnvironmentResponse struct {
+	Id   string
+	Name string
+	Slug string
+
+	OrganizationId   string
+	OrganizationSlug string
+	OrganizationName string
+}
