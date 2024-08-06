a07bdfd3b2e84b0cb8f1345420f55b9bdadf44c3
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-08-06 07:25:32 +0200

    feat: added executor v2

--- pkg/cloud/data/controlplane/commands.go
+++ pkg/cloud/data/controlplane/commands.go
@@ -1,6 +1,6 @@
 package controlplane
 
-import "github.com/kubeshop/testkube/pkg/cloud/data/executor"
+import "github.com/kubeshop/testkube/pkg/cloud/data/executor/v2"
 
 const (
 	CmdControlPlaneGetEnvironment executor.Command = "get_environment"
