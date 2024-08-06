b1111dd8efcc840946a646cc28cf85937c06e0bf
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-08-06 08:18:16 +0200

    feat: undeprecate executor

--- pkg/tcl/controlplanetcl/environments.go
+++ pkg/tcl/controlplanetcl/environments.go
@@ -6,17 +6,17 @@ import (
 	"github.com/kubeshop/testkube/internal/config"
 	"github.com/kubeshop/testkube/pkg/cloud"
 	"github.com/kubeshop/testkube/pkg/cloud/data/controlplane"
-	"github.com/kubeshop/testkube/pkg/cloud/data/executor/v2"
+	"github.com/kubeshop/testkube/pkg/cloud/data/executor"
 	"google.golang.org/grpc"
 )
 
 func GetEnvironment(ctx context.Context, proContext config.ProContext, cloudClient cloud.TestKubeCloudAPIClient, grpcConn *grpc.ClientConn) (resp GetEnvironmentResponse, err error) {
-	ex := executor.NewCloudGRPCExecutor[GetEnvironmentResponse](cloudClient, grpcConn, proContext.APIKey, proContext.RunnerId)
-
-	return ex.Execute(ctx, executor.ExecuteParams{
-		Command: controlplane.CmdControlPlaneGetEnvironment,
-		Payload: GetEnvironmentRequest{},
-	})
+	ex := executor.NewCloudGRPCExecutor(cloudClient, grpcConn, proContext.APIKey, proContext.RunnerId)
+	bytes, err := ex.Execute(ctx, controlplane.CmdControlPlaneGetEnvironment, GetEnvironmentRequest{})
+	if err != nil {
+		return resp, err
+	}
+	return executor.ToResponse[GetEnvironmentResponse](bytes)
 }
 
 // GetEnvironmentRequest represents a request to get an environment by the token
