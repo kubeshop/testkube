08aa00ebffc7e2dbb674d71d7ff84a51d7112f5a
Jacek Wysocki <jacek.wysocki@gmail.com>
2024-09-05 13:48:23 +0200

    fix: sensitive data in events (#5821)
    * fix: remove sensitive envs from events passed to websockets
    
    * fix: remove envs from agent
    
    * fix: moved envs to webhooks loader
    
    * removed variable
    
    * fix: passed envs to listener event
    
    * fix: not needed map in tests
    
    * Update pkg/event/kind/webhook/listener.go
    
    Co-authored-by: Dawid Rusnak <dawid@drcode.pl>
    
    * fix: flaky parallel test
    
    * fix: clean envs passed to the default payload
    
    ---------
    
    Co-authored-by: Dawid Rusnak <dawid@drcode.pl>

--- pkg/triggers/service_test.go
+++ pkg/triggers/service_test.go
@@ -67,7 +67,7 @@ func TestService_Run(t *testing.T) {
 
 	mockExecutor := client.NewMockExecutor(mockCtrl)
 
-	mockEventEmitter := event.NewEmitter(bus.NewEventBusMock(), "", nil)
+	mockEventEmitter := event.NewEmitter(bus.NewEventBusMock(), "")
 
 	mockTest := testsv3.Test{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "testkube", Name: "some-test"},
