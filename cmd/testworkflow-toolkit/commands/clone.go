8d90700c7be3956e84af838b9fe196070cf80267
Bogdan Hanea <bogdan.hanea@yahoo.com>
2024-09-26 17:32:23 +0300

    attempt

--- cmd/testworkflow-toolkit/commands/clone.go
+++ cmd/testworkflow-toolkit/commands/clone.go
@@ -150,7 +150,7 @@ func NewCloneCmd() *cobra.Command {
 
 				// bold the folder name
 				if info.IsDir() {
-					fmt.Printf("📥\033[1m%s\033[0m\n", name)
+					fmt.Printf("📦\x1b[1m%s\x1b[0m\n", name)
 				} else {
 					fmt.Println(name)
 				}
