5c42889f0ab93cc3146cb263adcecaba7988e990
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-09-27 15:15:18 +0200

    workflow tests - junit5 tests added

---
+++ test/junit/junit5-project/scr/test/java/TestSmoke1.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+public class TestSmoke1 {
+    @BeforeEach
+    public void beforeEach() {
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test4() {
+        TimeUnit.SECONDS.sleep(3);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test6() {
+        TimeUnit.SECONDS.sleep(6);
+        assertEquals(1, 1);
+    }
+}
