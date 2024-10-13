01ca493acd11e20326576ff323cb35c900693ac1
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-10-13 16:22:12 +0200

    k6 example perf test

---
+++ test/k6/executor-tests/k6-perf-test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '1m', target: 20 },
+    { duration: '30s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://testkube-test-page-lipsum.pages.dev/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  check(res, {
+    'verify partial text': (r) =>
+      r.body.includes('Testkube test page - Lipsum'),
+  });
+}
