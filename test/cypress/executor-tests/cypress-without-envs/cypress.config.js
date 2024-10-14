c4bb824ab0fe1898c5439e6b6f70474f40f44c38
Bogdan Hanea <bogdan.hanea@yahoo.com>
2024-10-14 10:38:07 +0300

    change paths for testkube.kubeshop.io

--- test/cypress/executor-tests/cypress-without-envs/cypress.config.js
+++ test/cypress/executor-tests/cypress-without-envs/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://testkube.kubeshop.io/',
-    video: false
+    baseUrl: "https://testkube-test-page-lipsum.pages.dev/",
+    video: false,
   },
 });
