e35bb69a93ac39afb82d2b8f7f68578fc1e48521
Bogdan Hanea <bogdan.hanea@yahoo.com>
2024-10-15 15:42:26 +0300

    fix: change default test website (#5920)

--- test/k6/executor-tests/k6-smoke-test-without-envs.js
+++ test/k6/executor-tests/k6-smoke-test-without-envs.js
@@ -1,5 +1,5 @@
-import http from 'k6/http';
+import http from "k6/http";
 
 export default function () {
-  http.get('https://testkube.kubeshop.io/');
+  http.get("https://testkube-test-page-lipsum.pages.dev/");
 }
--- test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
+++ test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
@@ -1,8 +1,8 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('Smoke 1 - has title', async ({ page }) => {
-  await page.goto('https://testkube.io/');
+test("Smoke 1 - has title", async ({ page }) => {
+  await page.goto("https://testkube-test-page-lipsum.pages.dev/");
 
   await expect(page).toHaveTitle(/Testkube/);
 });
