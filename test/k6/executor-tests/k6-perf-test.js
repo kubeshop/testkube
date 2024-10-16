0a6a959bb163e48f6aa911616657a3e91f320c4c
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-10-16 12:16:10 +0200

    k6 perf test - additinal requests

--- test/k6/executor-tests/k6-perf-test.js
+++ test/k6/executor-tests/k6-perf-test.js
@@ -16,4 +16,18 @@ export default function () {
     'verify partial text': (r) =>
       r.body.includes('Testkube test page - Lipsum'),
   });
+
+  const res2 = http.get('https://testkube-test-page-lipsum.pages.dev/');
+  check(res2, { 'status was 200': (r) => r.status == 200 });
+  check(res2, {
+    'verify partial text': (r) =>
+      r.body.includes('Testkube test page'),
+  });
+
+  const res3 = http.get('https://testkube-test-page-lipsum.pages.dev/');
+  check(res3, { 'status was 200': (r) => r.status == 200 });
+  check(res3, {
+    'verify partial text': (r) =>
+      r.body.includes('Lorem ipsum'),
+  });
 }
