859718b268ffd9457a77e0da13520953cb5f8a1a
Vladislav Sukhin <vladislav@kubeshop.io>
2024-10-16 12:21:35 +0300

    fix: test docker image version
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- internal/config/config.go
+++ internal/config/config.go
@@ -116,7 +116,7 @@ type Config struct {
 	TestkubeDefaultStorageClassName  string        `envconfig:"TESTKUBE_DEFAULT_STORAGE_CLASS_NAME" default:""`
 	GlobalWorkflowTemplateName       string        `envconfig:"TESTKUBE_GLOBAL_WORKFLOW_TEMPLATE_NAME" default:""`
 	EnableK8sEvents                  bool          `envconfig:"ENABLE_K8S_EVENTS" default:"true"`
-	TestkubeDockerImageVersion       string        `envconfig:"TESTKUBE_DOCKER_IMAGE_VERSION" default:""`
+	TestkubeDockerImageVersion       string        `envconfig:"TESTKUBE_DOCKER_IMAGE_VERSION" default:"1.2.3"`
 
 	// DEPRECATED: Use TestkubeProAPIKey instead
 	TestkubeCloudAPIKey string `envconfig:"TESTKUBE_CLOUD_API_KEY" default:""`
