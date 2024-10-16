6f2ac47bdde96961d5d7ab4854ce45cdb91e7366
Vladislav Sukhin <vladislav@kubeshop.io>
2024-10-16 18:36:09 +0300

    feat: move runnng context to tcl
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/mapper/cdevents/mapper.go
+++ pkg/mapper/cdevents/mapper.go
@@ -423,21 +423,6 @@ func MapTestkubeRunningContextTypeToCDEventTiggerType(contextType string) string
 	return "other"
 }
 
-// MapTestkubeTestWorkflowRunningContextActorToCDEventTiggerType maps OpenAPI spec Test Workflow Running Context Actor to CDEvent Trigger Type
-func MapTestkubeTestWorkflowRunningContextActorToCDEventTiggerType(actor testkube.TestWorkflowRunningContextActorType) string {
-	switch actor {
-	case testkube.USER_TestWorkflowRunningContextActorType, testkube.PROGRAM_TestWorkflowRunningContextActorType:
-		return "manual"
-	case testkube.TESTWORKFLOW_TestWorkflowRunningContextActorType, testkube.TESTWORKFLOWEXECUTION_TestWorkflowRunningContextActorType,
-		testkube.TESTRIGGER_TestWorkflowRunningContextActorType:
-		return "event"
-	case testkube.CRON_TestWorkflowRunningContextActorType:
-		return "schedule"
-	}
-
-	return "other"
-}
-
 // MapTestkubeTestTypeToCDEventTestCaseType maps OpenAPI spec Test Type to CDEvent Test Case Type
 func MapTestkubeTestTypeToCDEventTestCaseType(testType string) string {
 	var types = map[string]string{
