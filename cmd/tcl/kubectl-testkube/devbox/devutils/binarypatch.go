a86ac21a3aa3bf6c32de901197afbf3e2a5ed4a8
Dawid Rusnak <dawid@drcode.pl>
2024-10-30 18:27:59 +0100

    fix: make binary patch more stable

--- cmd/tcl/kubectl-testkube/devbox/devutils/binarypatch.go
+++ cmd/tcl/kubectl-testkube/devbox/devutils/binarypatch.go
@@ -20,6 +20,7 @@ const (
 	BinaryPatchAddOp      = 1
 	BinaryPatchOriginalOp = 2
 	BinaryPatchDeleteOp   = 3
+	BinaryPatchRepeatOp   = 4
 
 	BinaryBatchBlockSize      = 100
 	BinaryBatchBlockFactor    = 8
@@ -69,15 +70,20 @@ func (p *BinaryPatch) Load(data []byte) {
 }
 
 func (p *BinaryPatch) Read(originalFile, currentFile []byte, maxDuration time.Duration) {
-	size := int32(30)
-	step := int32(10)
+	skew := int32(60)
+	minReuse := int32(20)
+	step := skew / 2
 
 	ts := time.Now()
 
 	originalMarkers := make([][]int32, math.MaxUint16+1)
-	originalIterations := int32(len(originalFile)) - size
-	for i := int32(0); i < originalIterations; i++ {
-		marker := uint16(originalFile[i]) | uint16(originalFile[i+size])<<8
+	originalIterations := int32(len(originalFile)) - skew
+	for i := skew; i < originalIterations; i++ {
+		if originalFile[i] == 0 {
+			continue
+		}
+		// Approximate the marker
+		marker := uint16((int(originalFile[i-(skew/4)])+int(originalFile[i-(skew/2)]))/2) | uint16((int(originalFile[i+(skew/4)])+int(originalFile[i+(skew/2)]))/2)<<8
 		originalMarkers[marker] = append(originalMarkers[marker], i)
 	}
 
@@ -106,30 +112,36 @@ func (p *BinaryPatch) Read(originalFile, currentFile []byte, maxDuration time.Du
 	totalSaved := 0
 	iterations := 0
 
-	maxIndex := int32(len(currentFile)) - size
+	maxIndex := int32(len(currentFile)) - skew
 loop:
-	for ci := int32(0); ci < maxIndex; ci += step {
-		marker := uint16(currentFile[ci]) | uint16(currentFile[ci+size])<<8
-		if len(originalMarkers[marker]) == 0 {
-			ci = ci - step/2
+	for ci := skew; ci < maxIndex; {
+		iterations++
+		if currentFile[ci] == 0 {
+			ci += step
 			continue
 		}
-		iterations++
+		marker := uint16((int(currentFile[ci-(skew/4)])+int(currentFile[ci-(skew/2)]))/2) | uint16((int(currentFile[ci+(skew/4)])+int(currentFile[ci+(skew/2)]))/2)<<8
 
 		if maxDuration != 0 && iterations%1000 == 0 && time.Since(ts) > maxDuration {
 			break
 		}
 
-		if iterations%20000 == 0 && 100*ci/maxIndex < 50 {
-			step = step * 4 / 3
-			ci -= step
+		if iterations%50000 == 0 {
+			step = step * 7 / 6
 			continue
 		}
+
+		nextCL := 0
+		nextCR := 0
+		nextOL := 0
 		for _, oi := range originalMarkers[marker] {
-			if (oi < step || ci < step || originalFile[oi-step] != currentFile[ci-step]) && (oi+step > oiMax || ci+step > ciMax || originalFile[oi+step] != currentFile[ci+step]) {
+			if currentFile[ci] != originalFile[oi] ||
+				currentFile[ci+1] != originalFile[oi+1] ||
+				currentFile[ci-1] != originalFile[oi-1] ||
+				currentFile[ci-skew/2] != originalFile[oi-skew/2] ||
+				currentFile[ci+skew/2] != originalFile[oi+skew/2] {
 				continue
 			}
-
 			// Validate exact range
 			l, r := int32(0), int32(0)
 			for ; ci-l > lastCi && oi-l > lastOi && originalFile[oi-l-1] == currentFile[ci-l-1]; l++ {
@@ -137,15 +149,26 @@ loop:
 			for ; oi+r < oiMax && ci+r < ciMax && originalFile[oi+r+1] == currentFile[ci+r+1]; r++ {
 			}
 			// Determine if it's nice
-			if l+r > 14 {
-				totalSaved += int(r + l + 1)
-				p.Add(currentFile[lastCi : ci-l])
-				lastCi = ci + r + 1
-				ci = lastCi + 1 - step
-				p.Original(int(oi-l), int(r+l+1))
-				continue loop
+			if l+r+1 >= minReuse && nextCR-nextCL < int(r+l) {
+				nextCL = int(ci - l)
+				nextCR = int(ci + r)
+				nextOL = int(oi - l)
+			}
+			if l+r > skew {
+				break
 			}
 		}
+
+		if nextCL != 0 || nextCR != 0 {
+			totalSaved += (nextCR - nextCL) + 1
+			p.Add(currentFile[lastCi:nextCL])
+			lastCi = int32(nextCR + 1)
+			p.Original(nextOL, (nextCR-nextCL)+1)
+			ci = lastCi + 1
+			continue loop
+		}
+
+		ci += step
 	}
 
 	p.Add(currentFile[lastCi:])
@@ -207,6 +230,18 @@ func (p *BinaryPatch) Add(bytesArr []byte) {
 	p.buf.Write(bytesArr)
 }
 
+func (p *BinaryPatch) Repeat(count int, b byte) {
+	if count == 0 {
+		return
+	}
+	p.lastOp = BinaryPatchRepeatOp
+	p.buf.WriteByte(BinaryPatchRepeatOp)
+	num := make([]byte, 4)
+	binary.LittleEndian.PutUint32(num, uint32(count))
+	p.buf.Write(num)
+	p.buf.WriteByte(b)
+}
+
 func (p *BinaryPatch) Apply(original []byte) []byte {
 	result := make([]byte, 0)
 	patch := p.buf.Bytes()
@@ -217,10 +252,14 @@ func (p *BinaryPatch) Apply(original []byte) []byte {
 			count := binary.LittleEndian.Uint32(patch[i+5 : i+9])
 			result = append(result, original[index:index+count]...)
 			i += 9
-		//case BinaryPatchDeleteOp:
-		//	count := binary.LittleEndian.Uint32(patch[i+1 : i+5])
-		//	original = original[count:]
-		//	i += 5
+		case BinaryPatchRepeatOp:
+			count := binary.LittleEndian.Uint32(patch[i+1 : i+5])
+			buf := make([]byte, count)
+			for j := 0; j < int(count); j++ {
+				buf[j] = patch[i+5]
+			}
+			result = append(result, buf...)
+			i += 6
 		case BinaryPatchAddOp:
 			count := binary.LittleEndian.Uint32(patch[i+1 : i+5])
 			result = append(result, patch[i+5:i+5+int(count)]...)
