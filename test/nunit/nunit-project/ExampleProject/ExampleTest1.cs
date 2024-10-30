695c08926a38decd6d4f30199342cab15fb444e4
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-10-30 11:55:08 +0100

    NUnit project - additional tests and delays

--- test/nunit/nunit-project/ExampleProject/ExampleTest1.cs
+++ test/nunit/nunit-project/ExampleProject/ExampleTest1.cs
@@ -14,17 +14,54 @@ public class Tests
     }
 
     [Test]
+    public void Test2()
+    {
+        System.Threading.Thread.Sleep(4000);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        System.Threading.Thread.Sleep(500);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test4()
+    {
+        System.Threading.Thread.Sleep(1000);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test5()
+    {
+        System.Threading.Thread.Sleep(2000);
+        Assert.Pass();
+    }
+
+    [Test]
     public void TestA() {
-        Assert.That("A", Is.EqualTo("A"));
+        System.Threading.Thread.Sleep(2000);
+        var variable1 = "A";
+        var variable2 = "A";
+        Assert.That(variable1, Is.EqualTo(variable2));
     }
 
     [Test]
     public void TestB() {
-        Assert.That("B", Is.EqualTo("B"));
+        System.Threading.Thread.Sleep(500);
+        var variable1 = "B";
+        var variable2 = "B";
+        Assert.That(variable1, Is.EqualTo(variable2));
     }
 
     [Test]
     public void TestC() {
-        Assert.That("C", Is.EqualTo("C"));
+        System.Threading.Thread.Sleep(1000);
+        var variable1 = "C";
+        var variable2 = "C";
+        Assert.That(variable1, Is.EqualTo(variable2));
     }
 }
