9e029d8e16000335e67bf0bc2e49c5f020d157c2
Vladislav Sukhin <vladislav@kubeshop.io>
2024-11-12 14:45:26 +0300

    fix: update help
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- cmd/kubectl-testkube/commands/testworkflows/run.go
+++ cmd/kubectl-testkube/commands/testworkflows/run.go
@@ -181,7 +181,7 @@ func NewRunTestWorkflowCmd() *cobra.Command {
 	cmd.Flags().StringVar(&format, "format", "folder", "data format for storing files, one of folder|archive")
 	cmd.Flags().StringArrayVarP(&masks, "mask", "", []string{}, "regexp to filter downloaded files, single or comma separated, like report/.* or .*\\.json,.*\\.js$")
 	cmd.Flags().StringToStringVarP(&tags, "tag", "", map[string]string{}, "execution tags in a form of name1=val1 passed to executor")
-	cmd.Flags().StringSliceVarP(&selectors, "label", "l", nil, "label key value pair: --label key1=value1")
+	cmd.Flags().StringSliceVarP(&selectors, "label", "l", nil, "label key value pair: --label key1=value1 or label expression")
 	cmd.Flags().IntVar(&parallelism, "parallelism", 10, "parallelism for multiple test workflow execution")
 
 	return cmd
