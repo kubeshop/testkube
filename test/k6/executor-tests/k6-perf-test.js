36e1f97857b422fb016c26992e0a74d7bd38caf0
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-11-18 17:29:23 +0100

    k6 test updated

--- test/k6/executor-tests/k6-perf-test.js
+++ test/k6/executor-tests/k6-perf-test.js
@@ -1,14 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-// export const options = {
-//   stages: [
-//     { duration: '30s', target: 1000 },
-//     { duration: '1m', target: 1000 },
-//     { duration: '30s', target: 0 },
-//   ],
-// };
-
 export default function () {
   const res = http.get('https://testkube-test-page-lipsum.pages.dev/');
   check(res, { 'status was 200': (r) => r.status == 200 });
