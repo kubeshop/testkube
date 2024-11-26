b84552df668527fa421ce2b772002a3a8c431153
Ole Lensmar <ole@lensmar.com>
2024-11-26 08:00:52 +0100

    enabled webkit browser

--- test/playwright/executor-tests/playwright-project/playwright.config.js
+++ test/playwright/executor-tests/playwright-project/playwright.config.js
@@ -54,11 +54,10 @@ module.exports = defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+       name: 'webkit',
+       use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
