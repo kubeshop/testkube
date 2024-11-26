06a04b40ede9075c3c63cda0e4ff7ed35321c9bd
Ole Lensmar <ole@lensmar.com>
2024-11-26 07:53:19 +0100

    added video recording to playwright test

--- test/playwright/executor-tests/playwright-project/playwright.config.js
+++ test/playwright/executor-tests/playwright-project/playwright.config.js
@@ -40,6 +40,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
