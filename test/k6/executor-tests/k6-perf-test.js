92b42b9e53f30bdd4e220bfe334bda7f93c4724d
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-11-26 21:57:57 +0100

    discardResponseBodies

--- test/k6/executor-tests/k6-perf-test.js
+++ test/k6/executor-tests/k6-perf-test.js
@@ -1,11 +1,15 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+export const options = {
+  discardResponseBodies: true,
+};
+
 export default function () {
   const res = http.get('https://testkube-test-page-lipsum.pages.dev/');
   check(res, { 'status was 200': (r) => r.status == 200 });
-  check(res, {
-    'verify partial text': (r) =>
-      r.body.includes('Testkube test page - Lipsum'),
-  });
+  // check(res, {
+  //   'verify partial text': (r) =>
+  //     r.body.includes('Testkube test page - Lipsum'),
+  // });
 }
