0ac779effbe567823391ea7e369c8c58342ab7e3
Tomasz Konieczny <tkonieczny91@gmail.com>
2024-11-27 14:36:30 +0100

    k6 perf test - gcp-discard-response-bodies

--- test/k6/executor-tests/k6-perf-test.js
+++ test/k6/executor-tests/k6-perf-test.js
@@ -1,10 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-// export const options = {
-//   discardResponseBodies: true,
-// };
-
 export default function () {
   const res = http.get('https://testkube-test-page-lipsum.pages.dev/');
   check(res, { 'status was 200': (r) => r.status == 200 });
