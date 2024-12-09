b3dd564e82ea6129b6fbdfd1bcff53fe177061d9
Dawid Rusnak <dawid@drcode.pl>
2024-12-09 10:38:36 +0100

    feat: retry multiple times getting credentials from the Control Plane

--- pkg/credentials/repository.go
+++ pkg/credentials/repository.go
@@ -3,6 +3,7 @@ package credentials
 import (
 	"context"
 	"math"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding/gzip"
@@ -10,6 +11,19 @@ import (
 	agentclient "github.com/kubeshop/testkube/pkg/agent/client"
 	"github.com/kubeshop/testkube/pkg/cloud"
 )
+
+const (
+	GetCredentialRetryCount = 30
+)
+
+func getIterationDelay(iteration int) time.Duration {
+	if iteration < 5 {
+		return 500 * time.Millisecond
+	} else if iteration < 100 {
+		return 1 * time.Second
+	}
+	return 5 * time.Second
+}
 
 //go:generate mockgen -destination=./mock_repository.go -package=credentials "github.com/kubeshop/testkube/pkg/credentials" CredentialRepository
 type CredentialRepository interface {
@@ -29,9 +43,14 @@ func NewCredentialRepository(getClient func() cloud.TestKubeCloudAPIClient, apiK
 func (c *credentialRepository) Get(ctx context.Context, name string) ([]byte, error) {
 	ctx = agentclient.AddAPIKeyMeta(ctx, c.apiKey)
 	opts := []grpc.CallOption{grpc.UseCompressor(gzip.Name), grpc.MaxCallRecvMsgSize(math.MaxInt32)}
-	result, err := c.getClient().GetCredential(ctx, &cloud.CredentialRequest{Name: name, ExecutionId: c.executionId}, opts...)
-	if err != nil {
-		return nil, err
+	var err error
+	var result *cloud.CredentialResponse
+	for i := 0; i < GetCredentialRetryCount; i++ {
+		result, err = c.getClient().GetCredential(ctx, &cloud.CredentialRequest{Name: name, ExecutionId: c.executionId}, opts...)
+		if err == nil {
+			return result.Content, nil
+		}
+		time.Sleep(getIterationDelay(i))
 	}
-	return result.Content, nil
+	return nil, err
 }
