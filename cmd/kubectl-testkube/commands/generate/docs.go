18af8c000673b30166a182f661ac50fb2954fc1d
Ole Lensmar <ole@lensmar.com>
2025-03-14 11:42:16 +0100

    fix: added frontmatter title for nicer doc menus

--- cmd/kubectl-testkube/commands/generate/docs.go
+++ cmd/kubectl-testkube/commands/generate/docs.go
@@ -1,10 +1,31 @@
 package generate
 
 import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
+const fmTemplate = `---
+title: "%s"
+---
+`
+
+var filePrepender = func(filename string) string {
+	name := filepath.Base(filename)
+	base := strings.TrimSuffix(name, path.Ext(name))
+	return fmt.Sprintf(fmTemplate, strings.Replace(base, "_", " ", -1))
+}
+
+var linkHandler = func(name string) string {
+	return name
+}
+
+func NewDocsCmd() *cobra.Command 
 func NewDocsCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "doc",
@@ -14,7 +35,7 @@ func NewDocsCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			root := cmd.Root()
 			root.DisableAutoGenTag = true
-			return doc.GenMarkdownTree(root, "gen/docs/cli")
+			return doc.GenMarkdownTreeCustom(root, "gen/docs/cli", filePrepender, linkHandler)
 		},
 	}
 }
