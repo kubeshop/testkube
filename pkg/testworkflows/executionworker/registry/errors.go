cc6af12421ac8526fbc710193fc7588994b5fbe1
Vladislav Sukhin <vladislav@kubeshop.io>
2025-04-16 19:59:50 +0300

    fix: error message
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/testworkflows/executionworker/registry/errors.go
+++ pkg/testworkflows/executionworker/registry/errors.go
@@ -3,6 +3,6 @@ package registry
 import "errors"
 
 var (
-	ErrResourceNotFound = errors.New("resource not found agent")
+	ErrResourceNotFound = errors.New("resource not found")
 	ErrPodIpNotAssigned = errors.New("selected pod does not have IP assigned")
 )
