d29538898605cbcf505fa612bb17840e8967655f
Vladislav Sukhin <vladislav@kubeshop.io>
2025-06-10 14:33:23 +0300

    fix: unit tests
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

---
+++ pkg/repository/inteface.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/kubeshop/testkube/pkg/repository/config"
+	"github.com/kubeshop/testkube/pkg/repository/leasebackend"
+	"github.com/kubeshop/testkube/pkg/repository/result"
+	"github.com/kubeshop/testkube/pkg/repository/testresult"
+	"github.com/kubeshop/testkube/pkg/repository/testworkflow"
+)
+
+// DatabaseType represents the type of database backend
+type DatabaseType string
+
+const (
+	DatabaseTypeMongoDB    DatabaseType = "mongodb"
+	DatabaseTypePostgreSQL DatabaseType = "postgresql"
+)
+
+// RepositoryFactory defines the interface for creating repository instances
+type RepositoryFactory interface {
+	// Config Repository
+	NewConfigRepository() config.Repository
+
+	// LeaseBackend Repository
+	NewLeaseBackendRepository() leasebackend.Repository
+
+	// Result Repository (Test Executions)
+	NewResultRepository() result.Repository
+
+	// TestResult Repository (Test Suite Executions)
+	NewTestResultRepository() testresult.Repository
+
+	// TestWorkflow Repository (Test Workflow Executions)
+	NewTestWorkflowRepository() testworkflow.Repository
+
+	// Utility methods
+	GetDatabaseType() DatabaseType
+	Close(ctx context.Context) error
+	HealthCheck(ctx context.Context) error
+}
