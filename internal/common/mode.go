b10f39cbbeac41a17c0dfe37fe1af5962ee7f991
Povilas Versockas <p.versockas@gmail.com>
2025-08-08 15:15:38 +0300

    feat: add listener-agent mode; add DISABLE_API_SERVER and DISABLE_EVENT_SYSTEM flags; wire event system & API toggles

--- internal/common/mode.go
+++ internal/common/mode.go
@@ -3,8 +3,10 @@ package common
 import "errors"
 
 const (
-	ModeStandalone = "standalone"
-	ModeAgent      = "agent"
+	ModeStandalone    = "standalone"
+	ModeAgent         = "agent"
+	ModeListenerAgent = "listener-agent"
+	ModeGitOpsAgent   = "gitops-agent"
 )
 
 var ErrNotSupported = errors.New("Feature is not supported in standalone mode")
