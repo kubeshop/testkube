b6bbd1ea46a9ab4856bf02fdf6d1183b6827ffa0
Tomasz Konieczny <tkonieczny91@gmail.com>
2025-08-11 13:08:00 +0200

    java test updated

--- test/selenium/selenium-java/src/test/java/Example2Test.java
+++ test/selenium/selenium-java/src/test/java/Example2Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Example2Test extends BaseTest {
 
     @Test
-    public void example_2_1_Test() {
+    public void example_2_1_Test() throws InterruptedException {
         driver.get("https://testkube-test-page-lipsum.pages.dev/");
         Thread.sleep(10000); // just to make the test longer
         assertEquals("Testkube test page - Lorem Ipsum", driver.getTitle());
