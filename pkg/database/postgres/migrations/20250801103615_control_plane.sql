8c086618817291704e5752b283c8fd0dc8434489
Vladislav Sukhin <vladislav@kubeshop.io>
2025-09-05 21:06:47 +0300

    fix: change pkey
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/database/postgres/migrations/20250801103615_control_plane.sql
+++ pkg/database/postgres/migrations/20250801103615_control_plane.sql
@@ -5,6 +5,8 @@ ALTER TABLE test_workflow_executions ADD COLUMN environment_id VARCHAR(255) DEFA
 
 ALTER TABLE execution_sequences ADD COLUMN organization_id VARCHAR(255) DEFAULT '' NOT NULL;
 ALTER TABLE execution_sequences ADD COLUMN environment_id VARCHAR(255) DEFAULT '' NOT NULL;
+ALTER TABLE execution_sequences DROP CONSTRAINT execution_sequences_pkey;
+ALTER TABLE execution_sequences ADD PRIMARY KEY (name, organization_id, environment_id);
 
 -- Create indexes
 CREATE INDEX idx_test_workflow_executions_organization_id ON test_workflow_executions(organization_id);
@@ -24,6 +26,8 @@ DROP INDEX IF EXISTS idx_test_workflow_executions_environment_id;
 
 ALTER TABLE execution_sequences DROP COLUMN organization_id;
 ALTER TABLE execution_sequences DROP COLUMN environment_id;
+ALTER TABLE execution_sequences DROP CONSTRAINT execution_sequences_pkey;
+ALTER TABLE execution_sequences ADD PRIMARY KEY (name);
 
 ALTER TABLE test_workflow_executions DROP COLUMN organization_id;
 ALTER TABLE test_workflow_executions DROP COLUMN environment_id;
