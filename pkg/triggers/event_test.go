8b1e348ebb9a441e6fb506884013a72c1db70823
Kubeshop <174873053+ed382@users.noreply.github.com>
2025-09-19 15:03:16 +0200

    feat: built in event labels for kind, group, and version

---
+++ pkg/triggers/event_test.go
@@ -0,0 +1,54 @@
+package triggers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestNewWatcherEvent(t *testing.T) {
+	service := &Service{
+		agentName:         "testkube-agent",
+		testkubeNamespace: "testkube-ns",
+	}
+
+	deploymentLabels := map[string]string{"app": "nginx", "env": "prod"}
+	deployment := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nginx-deployment",
+			Namespace: "default",
+			Labels:    deploymentLabels,
+		},
+	}
+	event := service.newWatcherEvent(
+		"created",
+		&deployment.ObjectMeta,
+		deployment,
+		"deployment",
+	)
+
+	gvk := deployment.GetObjectKind().GroupVersionKind()
+	expectedEventLabels := map[string]string{
+		"testkube.io/agent-name":             "testkube-agent",
+		"testkube.io/agent-namespace":        "testkube-ns",
+		"testkube.io/resource-name":          "nginx-deployment",
+		"testkube.io/resource-namespace":     "default",
+		"testkube.io/resource-kind":          gvk.Kind,
+		"testkube.io/resource-group":         gvk.Group,
+		"testkube.io/resource-version":       gvk.Version,
+		"testkube.io/resource-group-version": gvk.Group + "/" + gvk.Version,
+	}
+
+	assert.EqualValues(t, "deployment", event.resource, "resource should be correct")
+	assert.Equal(t, "nginx-deployment", event.name, "name should be correct")
+	assert.Equal(t, "default", event.Namespace, "namespace should be correct")
+	assert.Equal(t, deploymentLabels, event.resourceLabels, "resourceLabels should be correct")
+	assert.EqualValues(t, "created", event.eventType, "eventType should be correct")
+	assert.Equal(t, expectedEventLabels, event.EventLabels, "EventLabels should be correct")
+}
