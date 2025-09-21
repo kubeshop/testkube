13f7d714167bc2df4d075d0f01aa4df646e43cb1
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2025-09-22 01:36:52 +0200

    cleanup and refactor test worfklows run command in testkube cli
    Signed-off-by: Dejan Zele Pejchev <pejcev.dejan@gmail.com>

--- cmd/kubectl-testkube/commands/testworkflows/run_test.go
+++ cmd/kubectl-testkube/commands/testworkflows/run_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
@@ -375,3 +376,4 @@ func TestExtractResults(t *testing.T) {
 		assert.Len(t, results, 1)
 	})
 }
+
