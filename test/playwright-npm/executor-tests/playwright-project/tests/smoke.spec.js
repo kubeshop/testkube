43741b8786b012b6e2d95bca71f0d4298fc9ec06
Jfortunati <jonathan.fortunati@gmail.com>
2025-09-22 14:36:07 -0400

    Update smoke.spec.js

--- test/playwright-npm/executor-tests/playwright-project/tests/smoke.spec.js
+++ test/playwright-npm/executor-tests/playwright-project/tests/smoke.spec.js
@@ -3,6 +3,6 @@ const { test, expect } = require("@playwright/test");
 
 test("Smoke 1 - has title", async ({ page }) => {
   await page.goto("https://testkube-test-page-lipsum.pages.dev/");
-
+  await page.waitForTimeout(10000)
   await expect(page).toHaveTitle(/Testkube/);
 });
