8c67975ec9d8f8f967277a37a37df5eef3f40df7
Wito <WitoDelnat@users.noreply.github.com>
2025-10-03 11:55:28 +0200

    chore: fix test

--- pkg/crd/crd.go
+++ pkg/crd/crd.go
@@ -47,20 +47,20 @@ const (
 // Gettable is an interface of gettable objects
 type Gettable interface {
 	testkube.Test |
-		testkube.TestSuite |
-		testkube.Webhook |
-		testkube.WebhookTemplate |
-		testkube.TestUpsertRequest |
-		testkube.TestSuiteUpsertRequest |
-		testkube.ExecutorUpsertRequest |
-		testkube.WebhookCreateRequest |
-		testkube.WebhookTemplateCreateRequest |
-		testkube.TestTrigger |
-		testkube.TestTriggerUpsertRequest |
-		testkube.TestSource |
-		testkube.TestSourceUpsertRequest |
-		testkube.Template |
-		testkube.TemplateCreateRequest
+	testkube.TestSuite |
+	testkube.Webhook |
+	testkube.WebhookTemplate |
+	testkube.TestUpsertRequest |
+	testkube.TestSuiteUpsertRequest |
+	testkube.ExecutorUpsertRequest |
+	testkube.WebhookCreateRequest |
+	testkube.WebhookTemplateCreateRequest |
+	testkube.TestTrigger |
+	testkube.TestTriggerUpsertRequest |
+	testkube.TestSource |
+	testkube.TestSourceUpsertRequest |
+	testkube.Template |
+	testkube.TemplateCreateRequest
 }
 
 // ExecuteTemplate executes crd template
@@ -99,7 +99,7 @@ func GenerateYAML[G Gettable](tmpl Template, items []G) (string, error) {
 
 func ValidateYAMLAgainstSchema(name opcrd.Schema, dataYAML []byte) error {
 	// Load CRD YAML
-	schemaYAML, err := opcrd.SF.ReadFile(fmt.Sprintf("bases/%s.yaml", name))
+	schemaYAML, err := opcrd.SF.ReadFile(fmt.Sprintf("crd/%s.yaml", name))
 	if err != nil {
 		return err
 	}
