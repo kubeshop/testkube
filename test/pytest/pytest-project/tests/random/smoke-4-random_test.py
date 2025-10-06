e2ba34e9d25b733d0a8729e9e049ecc7938747f7
Tomasz Konieczny <tkonieczny91@gmail.com>
2025-10-06 11:29:48 +0200

    workflow tests - pyteest random failure with junit report updated

--- test/pytest/pytest-project/tests/random/smoke-4-random_test.py
+++ test/pytest/pytest-project/tests/random/smoke-4-random_test.py
@@ -1,15 +1,24 @@
 import time
+import random
 
-def test_AAA():
-    assert 1 == 2
-
-def test_BBB():
+def test_EEE():
     time.sleep(3)
+    if random.randint(1, 4) == 4:
+        assert 0 == 1
+    else:
+        assert 1 == 1
+
+def test_FFF():
+    time.sleep(5)
     assert 1 == 1
 
-def test_CCC():
+def test_GGG():
+    time.sleep(2)
     assert 1 == 2
 
-def test_DDD():
-    time.sleep(2)
-    assert 1 == 1
+def test_HHH():
+    time.sleep(1)
+    if random.randint(1, 10) == 10:
+        assert 0 == 1
+    else:
+        assert 1 == 1
