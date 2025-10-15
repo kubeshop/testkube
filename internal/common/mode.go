f8157e9084e7927f0736afbfb43471ecda906a28
Wito <WitoDelnat@users.noreply.github.com>
2025-10-15 11:30:31 +0200

    chore: add important disclaimer regarding empty string env/orgs

--- internal/common/mode.go
+++ internal/common/mode.go
@@ -9,9 +9,12 @@ const (
 	// note: Testkube Agent should not really have knowledge about organizations and environments.
 	// It appears that over time this has seeped into this codebase. Often empty strings are used
 	// to make function works. These constants make usages explicit to determine if we can and should get rid of this.
+
+	// WARNING: Be extremely cautious renaming this from an empty string to anything else. I've discovered that
+	// these empty strings might down the line be replaced by a defaultEnvironment which is set through ProContext.
 	StandaloneEnvironment      = ""
 	StandaloneEnvironmentSlug  = ""
-	StandaloneOrganization     = "standalone"
+	StandaloneOrganization     = ""
 	StandaloneOrganizationSlug = ""
 	StandaloneRunner           = "default-agent"
 	StandaloneRunnerName       = "Testkube Agent"
