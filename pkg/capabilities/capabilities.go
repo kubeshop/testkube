b02f627a5a1fe389be52c252f778a74a31522864
Wito <WitoDelnat@users.noreply.github.com>
2025-10-20 14:41:29 +0200

    chore: fix commented code

--- pkg/capabilities/capabilities.go
+++ pkg/capabilities/capabilities.go
@@ -8,7 +8,8 @@ const CapabilityJUnitReports Capability = "junit-reports"
 
 // Deprecated: NewArchitecture is always enabled since November 2025.
 // This is kept for backwards compatibility with older agents.
-// Feel free to permanently delete this after 2026Q1.const CapabilityNewArchitecture Capability = "exec"
+// Feel free to permanently delete this after 2026Q1.
+const CapabilityNewArchitecture Capability = "exec"
 const CapabilityCloudStorage Capability = "tw-storage"
 
 func Enabled(capabilities []*cloud.Capability, capability Capability) bool {
