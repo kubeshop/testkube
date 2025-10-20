a6e86ee7bca872a5c357e12ce477b160d1dfc913
Tomasz Konieczny <tomasz.konieczny@kubeshop.io>
2025-10-20 14:03:48 +0200

    fix: [TKC-4406] k6 reports fixed (duration increased to 30s to generate report) (#6772)
    * k6 tests updated
    
    * k6 tests updated
    
    * workflow tests - k6 tests duration increased to 30s to generate reports
    
    * workflow tests - k6 tests duration increased to 30s to generate reports

--- test/k6/k6-smoke-test.js
+++ test/k6/k6-smoke-test.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { check } from "k6";
+import { check, sleep } from "k6";
 
 if (__ENV.K6_ENV_FROM_PARAM != "K6_ENV_FROM_PARAM_value") {
   throw new Error("Incorrect K6_ENV_FROM_PARAM ENV value");
@@ -10,5 +10,9 @@ if (__ENV.K6_SYSTEM_ENV != "K6_SYSTEM_ENV_value") {
 }
 
 export default function () {
-  http.get("https://testkube-test-page-lipsum.pages.dev/");
+  const res = http.get("https://testkube-test-page-lipsum.pages.dev/");
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+  });
+  sleep(1);
 }
