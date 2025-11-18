79726b8dff45eea01fcf3b013f7ccf4daeb33f49
Caio Medeiros Pinto <caio@testkube.io>
2025-11-18 10:49:13 -0300

    feat: update runner policy from CLI

--- cmd/kubectl-testkube/commands/agents/update.go
+++ cmd/kubectl-testkube/commands/agents/update.go
@@ -14,6 +14,8 @@ func NewUpdateAgentCommand() *cobra.Command {
 	var (
 		setLabels    []string
 		deleteLabels []string
+		runnerMode   string
+		groupName    string
 	)
 
 	cmd := &cobra.Command{
@@ -21,17 +23,19 @@ func NewUpdateAgentCommand() *cobra.Command {
 		Aliases: []string{"runner"},
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			UiUpdateAgent(cmd, strings.Join(args, ""), setLabels, deleteLabels)
+			UiUpdateAgent(cmd, strings.Join(args, ""), setLabels, deleteLabels, runnerMode, groupName)
 		},
 	}
 
 	cmd.Flags().StringSliceVarP(&setLabels, "label", "l", nil, "labels to set")
 	cmd.Flags().StringSliceVarP(&deleteLabels, "delete-label", "L", nil, "labels to delete")
+	cmd.Flags().StringVarP(&runnerMode, "runner-mode", "m", "", "runner mode, allowed values: independent, group, global")
+	cmd.Flags().StringVarP(&runnerMode, "group-name", "g", "", "group name used to target executions")
 
 	return cmd
 }
 
-func UiUpdateAgent(cmd *cobra.Command, name string, setLabels, deleteLabels []string) {
+func UiUpdateAgent(cmd *cobra.Command, name string, setLabels, deleteLabels []string, runnerMode string, groupName string) {
 	agent, err := GetControlPlaneAgent(cmd, name)
 	ui.ExitOnError("getting agent", err)
 
@@ -39,9 +43,29 @@ func UiUpdateAgent(cmd *cobra.Command, name string, setLabels, deleteLabels []st
 		agent.Labels = make(map[string]string)
 	}
 
+	if runnerMode == "group" && groupName != "" && agent.Labels["group"] == "" {
+		ui.ExitOnError("group name is mandatory to set runner mode as group, use param --group-name")
+	}
+
+	if runnerMode != "" {
+		if runnerMode == "group" || runnerMode == "global" || runnerMode == "independent" {
+			if runnerMode == "independent" {
+				runnerMode = "name"
+			}
+			agent.RunnerPolicy = &client.RunnerPolicy{
+				RequiredMatch: []string{runnerMode},
+			}
+		} else {
+			ui.ExitOnError("runner mode not valid, allowed values:  independent, group, global")
+		}
+	}
+
 	for _, labelPair := range setLabels {
 		k, v, _ := strings.Cut(labelPair, "=")
 		if v == "" {
+			if agent.RunnerPolicy.RequiredMatch[0] == "group" && k == "group" {
+				ui.ExitOnError("cannot delete group label when runner mode is group")
+			}
 			delete(agent.Labels, k)
 		} else {
 			agent.Labels[k] = v
@@ -49,10 +73,14 @@ func UiUpdateAgent(cmd *cobra.Command, name string, setLabels, deleteLabels []st
 	}
 
 	for _, labelName := range deleteLabels {
+		if agent.RunnerPolicy.RequiredMatch[0] == "group" && labelName == "group" {
+			ui.ExitOnError("cannot delete group label when runner mode is group")
+		}
 		delete(agent.Labels, labelName)
 	}
 
 	agent, err = UpdateAgent(cmd, agent.ID, client.AgentInput{
+		RunnerPolicy: common.Ptr(*agent.RunnerPolicy),
 		Labels: common.Ptr(agent.Labels),
 	})
 	ui.ExitOnError("updating agent", err)
