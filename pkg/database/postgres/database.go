abba9c9a946a1d5c1b93e5b8f4059dec7a43bec7
guillermotestkube <guillermo@testkube.io>
2026-04-29 10:52:25 -0400

    fix(postgres): create missing DB on API startup (#7603)
    * fix(postgres): create database before running migrations
    
    Replicate the cloud-api approach by creating the PostgreSQL database from the DSN during API startup when migrations are enabled, so standalone deployments recover from missing backend DBs.
    
    * fix(postgres): use postgres DB for bootstrap connection
    
    Use the explicit postgres database when creating missing databases so startup does not depend on role-name database fallbacks.

--- pkg/database/postgres/database.go
+++ pkg/database/postgres/database.go
@@ -2,16 +2,55 @@ package database
 
 import (
 	"context"
+	"errors"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	sqlc "github.com/kubeshop/testkube/pkg/controlplane/scheduling/sqlc"
+	"github.com/kubeshop/testkube/pkg/log"
 )
 
 type DB struct {
 	Pool *pgxpool.Pool
 	*sqlc.Queries
+}
+
+func CreateDatabaseIfNotExists(ctx context.Context, connectionString string) error {
+	connConfig, err := pgx.ParseConfig(connectionString)
+	if err != nil {
+		return err
+	}
+	log.DefaultLogger.Infof("Attempting to create database %q on host %s", connConfig.Database, connConfig.Host)
+
+	if connConfig.Database == "" {
+		log.DefaultLogger.Info("No database in connection string")
+		return nil
+	}
+
+	dbName := connConfig.Database
+	connConfig.Database = "postgres"
+
+	conn, err := pgx.ConnectConfig(ctx, connConfig)
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	_, err = conn.Exec(ctx, "CREATE DATABASE "+pgx.Identifier{dbName}.Sanitize())
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "42P04" {
+			log.DefaultLogger.Infof("Database %s already exists", dbName)
+		} else {
+			return err
+		}
+	} else {
+		log.DefaultLogger.Infof("Database %s created successfully", dbName)
+	}
+
+	return nil
 }
 
 func NewForScheduler(pool *pgxpool.Pool) (*DB, error) {
