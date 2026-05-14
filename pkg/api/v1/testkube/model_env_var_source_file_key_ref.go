41aab2a821ebc8aec3ab79e8e3b14a71d581f79e
Vladislav Sukhin <vladislav@kubeshop.io>
2026-05-15 01:27:28 +0300

    fix: removee model
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/api/v1/testkube/model_env_var_source_file_key_ref.go
+++
@@ -1,9 +0,0 @@
-package testkube
-
-// EnvVarSourceFileKeyRef selects a key from an env file mounted in a volume.
-type EnvVarSourceFileKeyRef struct {
-	VolumeName string `json:"volumeName"`
-	Path       string `json:"path"`
-	Key        string `json:"key"`
-	Optional   *bool  `json:"optional,omitempty"`
-}
