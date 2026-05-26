51b1462a5d2449e80b1ebc04f22541afab1a7288
Povilas Versockas <p.versockas@gmail.com>
2026-05-26 11:44:03 +0300

    test: [TKC-5821] drop default k6 scenario thresholds

--- test/k6/k6-smoke-test-without-envs.js
+++ test/k6/k6-smoke-test-without-envs.js
@@ -1,14 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-export const options = {
-  thresholds: {
-    "http_req_duration{scenario:default}": ["p(95)<2000"],
-    "http_reqs{scenario:default}": ["count>0"],
-    "http_req_failed{scenario:default}": ["rate<0.5"],
-  },
-};
-
 export default function () {
   const res = http.get("https://testkube-test-page-lipsum.pages.dev/");
   check(res, {
--- test/k6/k6-smoke-test.js
+++ test/k6/k6-smoke-test.js
@@ -1,14 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-export const options = {
-  thresholds: {
-    "http_req_duration{scenario:default}": ["p(95)<2000"],
-    "http_reqs{scenario:default}": ["count>0"],
-    "http_req_failed{scenario:default}": ["rate<0.5"],
-  },
-};
-
 if (__ENV.K6_ENV_FROM_PARAM != "K6_ENV_FROM_PARAM_value") {
   throw new Error("Incorrect K6_ENV_FROM_PARAM ENV value");
 }
