2885c338ad263b76b873097d6e19a5346a74f81a
Vladislav Sukhin <vladislav@kubeshop.io>
2026-09-07 23:08:29 +0300

    fix: generate openapi
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- pkg/api/v1/testkube/model_test_workflow_step.go
+++ pkg/api/v1/testkube/model_test_workflow_step.go
@@ -40,6 +40,7 @@ type TestWorkflowStep struct {
 	Container  *TestWorkflowContainerConfig `json:"container,omitempty"`
 	Execute    *TestWorkflowStepExecute     `json:"execute,omitempty"`
 	Artifacts  *TestWorkflowStepArtifacts   `json:"artifacts,omitempty"`
+	TestCases  *TestWorkflowStepTestCases   `json:"testCases,omitempty"`
 	Parallel   *TestWorkflowStepParallel    `json:"parallel,omitempty"`
 	// nested setup steps to run
 	Setup []TestWorkflowStep `json:"setup,omitempty"`
